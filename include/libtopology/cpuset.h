86ec853f3de2b4d2bd0b5c64eadc8599e80ce10b
Brice Goglin <brice.goglin@inria.fr>
2009-04-24 12:26:55 +0000

    Remove support for small cpusets. This kind of ...
    Remove support for small cpusets. This kind of optimization is only useful within
    marcel, libtopo doesn't need such complexity.
    We have 1024 bits per cpuset by default and we always use an array of unsigned longs.
    
    This commit was SVN r39.

--- include/libtopology/cpuset.h
+++ include/libtopology/cpuset.h
@@ -10,44 +10,28 @@
 #include <strings.h>
 #include <libtopology/configuration.h>
 
+/* large cpuset using an array of unsigned long subsets */
 
-#if LIBTOPO_NBMAXCPUS <= 32
-typedef uint32_t lt_cpuset_t; /* FIXME: uint_fast32_t if available? */
-/** \brief Set with no or all cpus selected. */
-#    define LT_CPUSET_ZERO		((lt_cpuset_t) 0U)
-#    define LT_CPUSET_FULL		((lt_cpuset_t) ~0U)
-/** \brief Set with only \e cpu in selected. */
-#    define LT_CPUSET_CPU(cpu)		((lt_cpuset_t)(1U<<(cpu)))
-/** \brief Format string snippet suitable for the cpuset datatype */
-#    define LT_PRIxCPUSET			"08x"
-#    define LT_CPUSET_PRINTF_VALUE(x)	((unsigned)(x))
-
-#elif LIBTOPO_NBMAXCPUS <= 64
-typedef uint64_t lt_cpuset_t;
-#    define LT_CPUSET_ZERO		((lt_cpuset_t) 0ULL)
-#    define LT_CPUSET_FULL		((lt_cpuset_t) ~0ULL)
-#    define LT_CPUSET_CPU(cpu)		((lt_cpuset_t)(1ULL<<(cpu)))
-#    define LT_PRIxCPUSET			"016llx"
-#    define LT_CPUSET_PRINTF_VALUE(x)	((unsigned long long)(x))
-
-#else /* large cpuset using an array of unsigned long subsets */
-#    define LT_HAVE_CPUSUBSET	1 /* marker for CPUSUBSET being enabled */
 /* size and count of subsets within a set */
 #    define LT_CPUSUBSET_SIZE		(8*sizeof(long))
 #    define LT_CPUSUBSET_COUNT		((LIBTOPO_NBMAXCPUS+LT_CPUSUBSET_SIZE-1)/LT_CPUSUBSET_SIZE)
 typedef struct { unsigned long s[LT_CPUSUBSET_COUNT]; } lt_cpuset_t;
+
 /* extract a subset from a set using an index or a cpu */
 #    define LT_CPUSUBSET_SUBSET(set,x)	((set).s[x])
 #    define LT_CPUSUBSET_INDEX(cpu)	((cpu)/(LT_CPUSUBSET_SIZE))
 #    define LT_CPUSUBSET_CPUSUBSET(set,cpu)	LT_CPUSUBSET_SUBSET(set,LT_CPUSUBSET_INDEX(cpu))
+
 /* predefined subset values */
 #    define LT_CPUSUBSET_VAL(cpu)	(1UL<<((cpu)%(LT_CPUSUBSET_SIZE)))
 #    define LT_CPUSUBSET_ZERO		0UL
 #    define LT_CPUSUBSET_FULL		~0UL
+
 /* actual whole-cpuset values */
 #    define LT_CPUSET_ZERO		(lt_cpuset_t){ .s[0 ... LT_CPUSUBSET_COUNT-1] = LT_CPUSUBSET_ZERO }
 #    define LT_CPUSET_FULL		(lt_cpuset_t){ .s[0 ... LT_CPUSUBSET_COUNT-1] = LT_CPUSUBSET_FULL }
 #    define LT_CPUSET_CPU(cpu)		({ lt_cpuset_t __set = LT_CPUSET_ZERO; LT_CPUSUBSET_CPUSUBSET(__set,cpu) = LT_CPUSUBSET_VAL(cpu); __set; })
+
 /* displaying cpusets */
 #if LT_BITS_PER_LONG == 32
 #    define LT_PRIxCPUSUBSET		"%08lx"
@@ -66,18 +50,6 @@ typedef struct { unsigned long s[LT_CPUSUBSET_COUNT]; } lt_cpuset_t;
 	*(__tmp-1) = '\0';						\
 	__buf;								\
      })
-#endif /* large cpuset using an array of unsigned long subsets */
-
-
-#ifndef LT_HAVE_CPUSUBSET
-#   define LT_CPUSUBSET_SIZE 		0 /* useless */
-#   define LT_CPUSUBSET_COUNT		1
-#   define LT_CPUSUBSET_SUBSET(set,x)	(set)
-#   define LT_CPUSUBSET_CPUSUBSET(set,cpu)	(set)
-#   define LT_CPUSUBSET_VAL(cpu)	LT_CPUSET_CPU(cpu)
-#   define LT_CPUSUBSET_ZERO		LT_CPUSET_ZERO
-#   define LT_CPUSUBSET_FULL		LT_CPUSET_FULL
-#endif
 
 
 
@@ -111,12 +83,8 @@ static __inline__ void lt_cpuset_cpu(lt_cpuset_t * set,
 static __inline__ void lt_cpuset_cpu(lt_cpuset_t * set,
     unsigned cpu)
 {
-#ifndef LT_HAVE_CPUSUBSET
-	*set = LT_CPUSET_CPU(cpu);
-#else
 	lt_cpuset_zero(set);
 	LT_CPUSUBSET_CPUSUBSET(*set,cpu) |= LT_CPUSUBSET_VAL(cpu);
-#endif
 }
 
 /** \brief Clear CPU set and set all but the CPU \e cpu */
@@ -125,12 +93,8 @@ static __inline__ void lt_cpuset_all_but_cpu(lt_cpuset_t * set,
 static __inline__ void lt_cpuset_all_but_cpu(lt_cpuset_t * set,
     unsigned cpu)
 {
-#ifndef LT_HAVE_CPUSUBSET
-	*set = ~LT_CPUSET_CPU(cpu);
-#else
 	lt_cpuset_fill(set);
 	LT_CPUSUBSET_CPUSUBSET(*set,cpu) &= ~LT_CPUSUBSET_VAL(cpu);
-#endif
 }
 
 /** \brief Add CPU \e cpu in CPU set \e set */
@@ -139,11 +103,7 @@ static __inline__ void lt_cpuset_set(lt_cpuset_t * set,
 static __inline__ void lt_cpuset_set(lt_cpuset_t * set,
     unsigned cpu)
 {
-#ifndef LT_HAVE_CPUSUBSET
-	*set |= LT_CPUSET_CPU(cpu);
-#else
 	LT_CPUSUBSET_CPUSUBSET(*set,cpu) |= LT_CPUSUBSET_VAL(cpu);
-#endif
 }
 
 /** \brief Remove CPU \e cpu from CPU set \e set */
@@ -152,11 +112,7 @@ static __inline__ void lt_cpuset_clr(lt_cpuset_t * set,
 static __inline__ void lt_cpuset_clr(lt_cpuset_t * set,
     unsigned cpu)
 {
-#ifndef LT_HAVE_CPUSUBSET
-	*set &= ~(LT_CPUSET_CPU(cpu));
-#else
 	LT_CPUSUBSET_CPUSUBSET(*set,cpu) &= ~LT_CPUSUBSET_VAL(cpu);
-#endif
 }
 
 /** \brief Test whether CPU \e cpu is part of set \e set */
@@ -165,11 +121,7 @@ static __inline__ int lt_cpuset_isset(const lt_cpuset_t * set,
 static __inline__ int lt_cpuset_isset(const lt_cpuset_t * set,
     unsigned cpu)
 {
-#ifndef LT_HAVE_CPUSUBSET
-	return 1 & (*set >> cpu);
-#else
 	return (LT_CPUSUBSET_CPUSUBSET(*set,cpu) & LT_CPUSUBSET_VAL(cpu)) != 0;
-#endif
 }
 
 /** \brief Test whether set \e set is zero or full */
@@ -256,16 +208,6 @@ static __inline__ void lt_cpuset_clearset (lt_cpuset_t *set,
 static __inline__ int lt_cpuset_first(const lt_cpuset_t * cpuset);
 static __inline__ int lt_cpuset_first(const lt_cpuset_t * cpuset)
 {
-#if (!defined LT_HAVE_CPUSUBSET) && LT_BITS_PER_LONG < LIBTOPO_NBMAXCPUS
-	int i;
-	i = ffs(((unsigned*)cpuset)[0]);
-	if (i>0)
-		return i-1;
-	i = ffs(((unsigned*)cpuset)[1]);
-	if (i>0)
-		return i+32-1;
-	return -1;
-#else
 	int i;
 	for(i=0; i<LT_CPUSUBSET_COUNT; i++) {
 		/* subsets are unsigned longs, use ffsl */
@@ -275,7 +217,6 @@ static __inline__ int lt_cpuset_first(const lt_cpuset_t * cpuset)
 	}
 
 	return -1;
-#endif
 }
 
 /** \brief Loop macro iterating on a cpuset and yielding on each cpu that
