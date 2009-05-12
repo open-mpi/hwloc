68916c08e8a53a6a0501bcfe8c055a9512883f03
Brice Goglin <brice.goglin@inria.fr>
2009-05-12 17:47:35 +0000

    Make topomask useful: it convert <depth:index> into a cpuset depth may be an integer or a type-string
    This commit was SVN r225.

--- utils/topomask.c
+++ utils/topomask.c
@@ -8,36 +8,81 @@
 #include <string.h>
 #include <assert.h>
 
-int main()
+static void usage(void)
+{
+  fprintf(stderr, "Usage: topomask [depth:index] ...\n");
+  fprintf(stderr, "  depth may be machine, node, die, core, proc or a numeric depth\n");
+}
+
+int main(int argc, char *argv[])
 {
   topo_topology_t topology;
   struct topo_topology_info topoinfo;
-  char string[TOPO_CPUSET_STRING_LENGTH+1];
-  topo_obj_t obj;
-  topo_cpuset_t set;
+  int verbose = 0;
 
   topo_topology_init(&topology);
   topo_topology_load(topology);
   topo_topology_get_info(topology, &topoinfo);
 
-  obj = topo_get_machine_object(topology);
-  topo_object_cpuset_snprintf(string, sizeof(string), 1, &obj);
-  printf("machine cpuset is %s\n", string);
-  topo_cpuset_from_string(string, &set);
-  printf("machine cpuset is %s\n", TOPO_CPUSET_PRINTF_VALUE(set));
-  assert(topo_cpuset_isequal(&set, &obj->cpuset));
+  if (argc < 2) {
+    usage();
+    return EXIT_FAILURE;
+  }
 
-  obj = topo_get_object(topology, topoinfo.depth-1, 0);
-  topo_object_cpuset_snprintf(string, sizeof(string), 1, &obj);
-  printf("first cpu cpuset is %s\n", string);
-  topo_cpuset_from_string(string, &set);
-  assert(topo_cpuset_isequal(&set, &obj->cpuset));
+  while (argc >= 2) {
+    char string[TOPO_CPUSET_STRING_LENGTH+1];
+    topo_obj_t obj;
+    unsigned depth;
+    char *colon;
+    unsigned index;
 
-  obj = topo_get_object(topology, topoinfo.depth-1, topo_get_depth_nbitems(topology, topoinfo.depth-1) - 1);
-  topo_object_cpuset_snprintf(string, sizeof(string), 1, &obj);
-  printf("last cpu cpuset is %s\n", string);
-  topo_cpuset_from_string(string, &set);
-  assert(topo_cpuset_isequal(&set, &obj->cpuset));
+    if (!strcmp(argv[1], "-v")) {
+      verbose = 1;
+      goto next;
+    }
+
+    colon = strchr(argv[1], ':');
+    if (!colon) {
+      usage();
+      return EXIT_FAILURE;
+    }
+
+    if (!strncmp(argv[1], "machine", 1))
+      depth = topo_get_type_depth(topology, TOPO_OBJ_NODE);
+    else if (!strncmp(argv[1], "node", 1))
+      depth = topo_get_type_depth(topology, TOPO_OBJ_NODE);
+    else if (!strncmp(argv[1], "die", 1))
+      depth = topo_get_type_depth(topology, TOPO_OBJ_DIE);
+    else if (!strncmp(argv[1], "core", 1))
+      depth = topo_get_type_depth(topology, TOPO_OBJ_CORE);
+    else if (!strncmp(argv[1], "proc", 1))
+      depth = topo_get_type_depth(topology, TOPO_OBJ_PROC);
+    else
+      depth = atoi(argv[1]);
+
+    index = atoi(colon+1);
+
+    obj = topo_get_object(topology, depth, index);
+    if (obj) {
+      topo_object_cpuset_snprintf(string, sizeof(string), 1, &obj);
+    } else {
+      topo_cpuset_t empty;
+      if (verbose)
+        printf("object (%d,%d) does not exist\n", depth, index);
+      topo_cpuset_zero(&empty);
+      snprintf(string, sizeof(string), "%" TOPO_PRIxCPUSET, TOPO_CPUSET_PRINTF_VALUE(empty));
+    }
+    if (verbose)
+      printf("object (%d,%d) has cpuset %s\n", depth, index, string);
+    else
+      printf("%s\n", string);
+
+ next:
+    argc--;
+    argv++;
+  }
+
+  topo_topology_destroy(topology);
 
   return 0;
 }
