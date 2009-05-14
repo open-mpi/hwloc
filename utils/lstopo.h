c92f12420f4fdfb18a2880f0e614759115308137
Brice Goglin <brice.goglin@inria.fr>
2009-05-14 12:16:02 +0000

    Add redundant include guard
    This commit was SVN r268.

--- utils/lstopo.h
+++ utils/lstopo.h
@@ -1,5 +1,8 @@
 /* Copyright 2009 INRIA, Université Bordeaux 1  */
 
+#ifndef UTILS_LSTOPO_H
+#define UTILS_LSTOPO_H
+
 #include <topology.h>
 
 typedef void output_method (struct topo_topology *topology, FILE *output, int verbose_mode);
@@ -15,3 +18,5 @@ struct draw_methods {
 
 extern void *output_draw_start(struct draw_methods *draw_methods, struct topo_topology *topology, void *output);
 extern void output_draw(struct draw_methods *draw_methods, struct topo_topology *topology, void *output);
+
+#endif /* UTILS_LSTOPO_H */
