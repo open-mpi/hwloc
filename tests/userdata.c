856cb21745ec505cd6bd572cc7bc43df184668ef
Brice Goglin <brice.goglin@inria.fr>
2009-05-14 10:07:27 +0000

    Add tests/userdata checking that the userdata p...
    Add tests/userdata checking that the userdata pointer is NULL
    
    This commit was SVN r266.

---
+++ tests/userdata.c
@@ -0,0 +1,45 @@
+/* Copyright 2009 INRIA, Université Bordeaux 1  */
+
+#include <config.h>
+#include <libtopology.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+/* make sure userdata is properly initialized */
+
+static void check(topo_topology_t topology)
+{
+  struct topo_topology_info topoinfo;
+  topo_obj_t obj;
+  unsigned i,j;
+
+  topo_topology_get_info(topology, &topoinfo);
+  for(i=0; i<topoinfo.depth; i++) {
+    for(j=0; j<topo_get_depth_nbitems(topology, i); j++) {
+      assert(topo_get_object(topology, i, j)->userdata == NULL);
+    }
+  }
+}
+
+int main(void)
+{
+  topo_topology_t topology;
+
+  /* check the real topology */
+  topo_topology_init(&topology);
+  topo_topology_load(topology);
+  check(topology);
+  topo_topology_destroy(topology);
+
+  /* check a synthetic topology */
+  topo_topology_init(&topology);
+  topo_topology_set_synthetic(topology, "6 5 4 3 2");
+  topo_topology_load(topology);
+  check(topology);
+  topo_topology_destroy(topology);
+
+  return 0;
+}
