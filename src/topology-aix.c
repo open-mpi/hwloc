814346f13d8a9c18ae4c5b0b2fc4400bf5cd7c1e
Samuel Thibault <samuel.thibault@labri.fr>
2009-05-27 13:43:34 +0000

    Simplify AIX detection a bit, extend it to repo...
    Simplify AIX detection a bit, extend it to report all levels, even unknown ones
    
    This commit was SVN r367.

--- src/topology-aix.c
+++ src/topology-aix.c
@@ -50,12 +50,12 @@
 #include <sys/rset.h>
 
 static void
-look_rset(int sdl, enum topo_obj_type_e type, struct topo_topology *topology)
+look_rset(int sdl, enum topo_obj_type_e type, struct topo_topology *topology, int level)
 {
   rsethandle_t rset, rad;
-  int r,i,maxcpus,j;
+  int i,maxcpus,j;
   unsigned nbnodes;
-  struct topo_obj **rad_level;
+  struct topo_obj *obj;
 
   if ((topology->flags & TOPO_FLAGS_IGNORE_ADMIN_DISABLE))
     rset = rs_alloc(RS_ALL);
@@ -68,9 +68,7 @@ look_rset(int sdl, enum topo_obj_type_e type, struct topo_topology *topology)
     return;
   }
 
-  rad_level = calloc(nbnodes+1, sizeof(*rad_level));
-
-  for (r = 0, i = 0; i < nbnodes; i++) {
+  for (i = 0; i < nbnodes; i++) {
     if (rs_getrad(rset, rad, sdl, i, 0)) {
       fprintf(stderr,"rs_getrad(%d) failed: %s\n", i, strerror(errno));
       continue;
@@ -78,32 +76,36 @@ look_rset(int sdl, enum topo_obj_type_e type, struct topo_topology *topology)
     if (!rs_getinfo(rad, R_NUMPROCS, 0))
       continue;
 
-    rad_level[r] = malloc(sizeof(struct topo_obj));
-    assert(rad_level[r]);
-    topo_setup_object(rad_level[r], type, r);
+    obj = malloc(sizeof(struct topo_obj));
+    assert(obj);
+    /* It seems logical processors are numbered from 1 here, while the
+     * bindprocessor functions numbers them from 0... */
+    topo_setup_object(obj, type, i - (type == TOPO_OBJ_PROC));
     switch(type) {
       case TOPO_OBJ_NODE:
-	rad_level[r]->memory_kB = 0; /* TODO */
-	rad_level[r]->huge_page_free = 0;
+	obj->memory_kB = 0; /* TODO */
+	obj->huge_page_free = 0;
 	break;
       case TOPO_OBJ_CACHE:
-	rad_level[r]->memory_kB = 0; /* TODO */
-	rad_level[r]->cache_depth = 2;
+	obj->memory_kB = 0; /* TODO */
+	obj->cache_depth = 2;
 	break;
+      case TOPO_OBJ_FAKE:
+	obj->fake_depth = level;
       default:
 	break;
     }
     maxcpus = rs_getinfo(rad, R_MAXPROCS, 0);
     for (j = 0; j < maxcpus; j++) {
       if (rs_op(RS_TESTRESOURCE, rad, NULL, R_PROCS, j))
-	topo_cpuset_set(&rad_level[r]->cpuset,j);
+	topo_cpuset_set(&obj->cpuset,j);
     }
-    topo_debug("node %d has cpuset %"TOPO_PRIxCPUSET"\n",
-	       r, TOPO_CPUSET_PRINTF_VALUE(rad_level[r]->cpuset));
-    r++;
+    topo_debug("%s %d has cpuset %"TOPO_PRIxCPUSET"\n",
+	       topo_object_type_string(type),
+	       i, TOPO_CPUSET_PRINTF_VALUE(obj->cpuset));
+    topo_add_object(topology, obj);
   }
 
-  topo_add_level(topology, rad_level, nbnodes);
   rs_free(rset);
   rs_free(rad);
 }
@@ -116,36 +118,53 @@ topo_look_aix(struct topo_topology *topology)
 
   for (i=0; i<=rs_getinfo(NULL, R_MAXSDL, 0); i++)
     {
+      int known = 0;
+#if 0
+      if (i == rs_getinfo(NULL, R_SMPSDL, 0))
+	/* Not enabled for now because I'm not sure what it corresponds to. On
+	 * decrypthon it contains all the cpus. Is it a "machine" or a "system"
+	 * level ?
+	 */
+	{
+	  topo_debug("looking AIX \"SMP\" sdl %d\n", i);
+	  look_rset(i, TOPO_OBJ_MACHINE, topology, i);
+	  known = 1;
+	}
+#endif
       if (i == rs_getinfo(NULL, R_MCMSDL, 0))
 	{
 	  topo_debug("looking AIX node sdl %d\n", i);
-	  look_rset(i, TOPO_OBJ_NODE, topology);
+	  look_rset(i, TOPO_OBJ_NODE, topology, i);
+	  known = 1;
 	}
 #      ifdef R_L2CSDL
       if (i == rs_getinfo(NULL, R_L2CSDL, 0))
 	{
 	  topo_debug("looking AIX L2 sdl %d\n", i);
-	  look_rset(i, TOPO_OBJ_CACHE, topology);
+	  look_rset(i, TOPO_OBJ_CACHE, topology, i);
+	  known = 1;
 	}
 #      endif
 #      ifdef R_PCORESDL
       if (i == rs_getinfo(NULL, R_PCORESDL, 0))
 	{
 	  topo_debug("looking AIX core sdl %d\n", i);
-	  look_rset(i, TOPO_OBJ_CORE, topology);
+	  look_rset(i, TOPO_OBJ_CORE, topology, i);
+	  known = 1;
 	}
 #      endif
-      if (i == rs_getinfo(NULL, R_SMPSDL, 0))
-	topo_debug("not looking AIX \"SMP\" sdl %d\n", i);
       if (i == rs_getinfo(NULL, R_MAXSDL, 0))
-	topo_debug("not looking AIX max sdl %d\n", i);
-#if 0
-      /* Disabled for now until the generic code accepts it.
-       */
 	{
 	  topo_debug("looking AIX max sdl %d\n", i);
-	  look_rset(i, TOPO_OBJ_PROC, topology);
+	  look_rset(i, TOPO_OBJ_PROC, topology, i);
+	  known = 1;
 	}
-#endif
+
+      /* Don't know how it should be rendered, make a fake object for it.  */
+      if (!known)
+	{
+	  topo_debug("looking AIX unknown sdl %d\n", i);
+	  look_rset(i, TOPO_OBJ_FAKE, topology, i);
+	}
     }
 }
