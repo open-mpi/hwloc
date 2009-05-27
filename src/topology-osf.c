c80ed4019d09dfcd57fdd975ec2adb5b867824f8
Samuel Thibault <samuel.thibault@labri.fr>
2009-05-27 13:45:05 +0000

    simplify OSF port a bit
    This commit was SVN r368.

--- src/topology-osf.c
+++ src/topology-osf.c
@@ -53,16 +53,13 @@ void
 topo_look_osf(struct topo_topology *topology)
 {
   cpu_cursor_t cursor;
-  unsigned i = 0;
   unsigned nbnodes;
   radid_t radid;
   cpuid_t cpuid;
   cpuset_t cpuset;
-  struct topo_obj **node_level;
+  struct topo_obj *obj;
 
   nbnodes=rad_get_num();
-
-  node_level = calloc(nbnodes+1, sizeof(*node_level));
 
   cpusetcreate(&cpuset);
   for (radid = 0; radid < nbnodes; radid++) {
@@ -72,20 +69,17 @@ topo_look_osf(struct topo_topology *topology)
       continue;
     }
 
-    node_level[i] = malloc(sizeof(struct topo_obj));
-    assert(node_level[i]);
-    topo_setup_object(node_level[i], TOPO_OBJ_NODE, radid);
-    node_level[i]->memory_kB = 0; /* TODO */
-    node_level[i]->huge_page_free = 0;
+    obj = malloc(sizeof(struct topo_obj));
+    assert(obj);
+    topo_setup_object(obj, TOPO_OBJ_NODE, radid);
+    obj->memory_kB = 0; /* TODO */
+    obj->huge_page_free = 0;
 
     cursor = SET_CURSOR_INIT;
     while((cpuid = cpu_foreach(cpuset, 0, &cursor)) != CPU_NONE)
-      topo_cpuset_set(&node_level[i]->cpuset,cpuid);
+      topo_cpuset_set(&obj->cpuset,cpuid);
 
     topo_debug("node %d has cpuset %"TOPO_PRIxCPUSET"\n",
-	       i, TOPO_CPUSET_PRINTF_VALUE(node_level[i]->cpuset));
-    i++;
+	       radid, TOPO_CPUSET_PRINTF_VALUE(obj->cpuset));
   }
-
-  topo_add_level(topology, node_level, nbnodes);
 }
