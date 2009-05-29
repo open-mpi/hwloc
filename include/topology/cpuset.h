e18a1f6149aedb0045475b0588274c8c2b363f89
Samuel Thibault <samuel.thibault@labri.fr>
2009-05-29 16:22:14 +0000

    properly get alloca according to the system
    This commit was SVN r412.

--- include/topology/cpuset.h
+++ include/topology/cpuset.h
@@ -43,7 +43,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <alloca.h>
+
+#ifdef HAVE_ALLOCA_H
+# include <alloca.h>
+#elif defined __GNUC__
+# define alloca __builtin_alloca
+#elif defined _AIX
+# define alloca __alloca
+#elif defined _MSC_VER
+# include <malloc.h>
+# define alloca _alloca
+#else
+# include <stddef.h>
+# ifdef  __cplusplus
+extern "C"
+# endif
+void *alloca (size_t);
+#endif
 
 /* large cpuset using an array of unsigned long subsets */
 
