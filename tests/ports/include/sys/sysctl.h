24c6319d8e360e2f8a26015f3c855e6b3227e9cd
Samuel Thibault <samuel.thibault@labri.fr>
2009-06-08 15:43:17 +0000

    add Darwin port check
    This commit was SVN r516.

---
+++ tests/ports/include/sys/sysctl.h
@@ -0,0 +1,8 @@
+/* Copyright 2009 INRIA, Université Bordeaux 1  */
+
+#ifndef LIBTOPOLOGY_SYS_SYSCTL_H
+#define LIBTOPOLOGY_SYS_SYSCTL_H
+
+extern int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
+
+#endif /* LIBTOPOLOGY_SYS_SYSCTL_H */
