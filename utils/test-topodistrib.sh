456f63f076060e4b23685b6b052330cb0ffca4d8
Samuel Thibault <samuel.thibault@labri.fr>
2009-06-12 12:17:41 +0000

    add tests for topodistrib
    This commit was SVN r624.

---
+++ utils/test-topodistrib.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+printenv > /tmp/foo
+set -e
+file="`mktemp`"
+(
+  ./topodistrib --synthetic "2 2 2" 2
+  echo
+  ./topodistrib --synthetic "2 2 2" 4
+  echo
+  ./topodistrib --synthetic "2 2 2" 8
+  echo
+  ./topodistrib --synthetic "2 2 2" 13
+  echo
+  ./topodistrib --synthetic "2 2 2" 16
+  echo
+  ./topodistrib --synthetic "3 3 3" 4
+) > "$file"
+diff -u $srcdir/test-topodistrib.output "$file"
+rm -f "$file"
