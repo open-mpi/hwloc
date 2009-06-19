e1ceb054c401d8f7d95e3a0d3fb941828c3ca53a
Samuel Thibault <samuel.thibault@labri.fr>
2009-06-19 12:08:34 +0000

    really fix usage of HAVE_DECL_PTHREAD_SETAFFINI...
    really fix usage of HAVE_DECL_PTHREAD_SETAFFINITY_NP
    
    This commit was SVN r702.

--- src/topology-linux.c
+++ src/topology-linux.c
@@ -181,7 +181,7 @@ topo_linux_set_proc_cpubind(topo_topology_t topology, pid_t pid, const topo_cpus
   return topo_linux_set_tid_cpubind(topology, pid, topo_set, strict);
 }
 
-#ifdef HAVE_DECL_PTHREAD_SETAFFINITY_NP
+#if HAVE_DECL_PTHREAD_SETAFFINITY_NP
 #pragma weak pthread_setaffinity_np
 
 static int
@@ -920,7 +920,7 @@ topo_look_linux(struct topo_topology *topology)
 
   if (!topology->is_fake) {
     topology->set_cpubind = topo_linux_set_cpubind;
-#ifdef HAVE_DECL_PTHREAD_SETAFFINITY_NP
+#if HAVE_DECL_PTHREAD_SETAFFINITY_NP
     topology->set_thread_cpubind = topo_linux_set_thread_cpubind;
 #endif /* HAVE_DECL_PTHREAD_SETAFFINITY_NP */
     topology->set_thisthread_cpubind = topo_linux_set_thisthread_cpubind;
