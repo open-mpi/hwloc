c8438d5341f5638ff90f748f44e43e83e45c9a46
Samuel Thibault <samuel.thibault@labri.fr>
2009-06-30 12:33:44 +0000

    remove obj type prefixes in xml attributes
    This commit was SVN r735.

--- src/topology-xml.c
+++ src/topology-xml.c
@@ -89,9 +89,8 @@ topo__process_object_attr(struct topo_topology *topology, struct topo_obj *obj,
   const char *value = (const char *) _value;
 
   if (!strcmp(name, "type")) {
-    obj->type = topo_obj_type_of_string(value);
-    if (obj->type == TOPO_OBJ_TYPE_MAX)
-      fprintf(stderr, "ignoring unknown object type %s\n", value);
+    /* already handled */
+    return;
   }
 
   else if (!strcmp(name, "os_index"))
@@ -99,40 +98,102 @@ topo__process_object_attr(struct topo_topology *topology, struct topo_obj *obj,
   else if (!strcmp(name, "cpuset"))
     topo_cpuset_from_string(value, &obj->cpuset);
 
-  else if (!strcmp(name, "cache_memory_kB"))
-    obj->attr->cache.memory_kB = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "cache_depth"))
-    obj->attr->cache.depth = strtoul(value, NULL, 10);
+  else if (!strcmp(name, "memory_kB")) {
+    unsigned long lvalue = strtoul(value, NULL, 10);
+    switch (obj->type) {
+      case TOPO_OBJ_CACHE:
+	obj->attr->cache.memory_kB = lvalue;
+	break;
+      case TOPO_OBJ_NODE:
+	obj->attr->node.memory_kB = lvalue;
+	break;
+      case TOPO_OBJ_MACHINE:
+	obj->attr->machine.memory_kB = lvalue;
+	break;
+      case TOPO_OBJ_SYSTEM:
+	obj->attr->system.memory_kB = lvalue;
+	break;
+      default:
+	fprintf(stderr, "ignoring memory_kB attribute for object type without memory\n");
+	break;
+    }
+  }
 
-  else if (!strcmp(name, "node_memory_kB"))
-    obj->attr->node.memory_kB = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "node_huge_page_free"))
-    obj->attr->node.huge_page_free = strtoul(value, NULL, 10);
+  else if (!strcmp(name, "depth")) {
+    unsigned long lvalue = strtoul(value, NULL, 10);
+    switch (obj->type) {
+      case TOPO_OBJ_CACHE:
+	obj->attr->cache.depth = lvalue;
+	break;
+      case TOPO_OBJ_MISC:
+	obj->attr->misc.depth = lvalue;
+	break;
+      default:
+	fprintf(stderr, "ignoring depth attribute for object type without depth\n");
+	break;
+    }
+  }
 
-  else if (!strcmp(name, "dmi_board_vendor"))
-    obj->attr->machine.dmi_board_vendor = strdup(value);
-  else if (!strcmp(name, "dmi_board_name"))
-    obj->attr->machine.dmi_board_name = strdup(value);
-  else if (!strcmp(name, "machine_memory_kB"))
-    obj->attr->machine.memory_kB = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "machine_huge_page_free"))
-    obj->attr->machine.huge_page_free = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "huge_page_size_kB"))
-    obj->attr->machine.huge_page_size_kB = strtoul(value, NULL, 10);
+  else if (!strcmp(name, "huge_page_free")) {
+    unsigned long lvalue = strtoul(value, NULL, 10);
+    switch (obj->type) {
+      case TOPO_OBJ_NODE:
+	obj->attr->node.huge_page_free = lvalue;
+	break;
+      case TOPO_OBJ_MACHINE:
+	obj->attr->machine.huge_page_free = lvalue;
+	break;
+      case TOPO_OBJ_SYSTEM:
+	obj->attr->system.huge_page_free = lvalue;
+	break;
+      default:
+	fprintf(stderr, "ignoring huge_page_free attribute for object type without huge pages\n");
+	break;
+    }
+  }
 
-  else if (!strcmp(name, "system_dmi_board_vendor"))
-    obj->attr->system.dmi_board_vendor = strdup(value);
-  else if (!strcmp(name, "system_dmi_board_name"))
-    obj->attr->system.dmi_board_name = strdup(value);
-  else if (!strcmp(name, "system_memory_kB"))
-    obj->attr->system.memory_kB = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "system_huge_page_free"))
-    obj->attr->system.huge_page_free = strtoul(value, NULL, 10);
-  else if (!strcmp(name, "huge_page_size_kB"))
-    obj->attr->system.huge_page_size_kB = strtoul(value, NULL, 10);
+  else if (!strcmp(name, "huge_page_size_kB")) {
+    unsigned long lvalue = strtoul(value, NULL, 10);
+    switch (obj->type) {
+      case TOPO_OBJ_MACHINE:
+	obj->attr->machine.huge_page_size_kB = lvalue;
+	break;
+      case TOPO_OBJ_SYSTEM:
+	obj->attr->system.huge_page_size_kB = strtoul(value, NULL, 10);
+	break;
+      default:
+	fprintf(stderr, "ignoring huge_page_size_kB attribute for object type without huge pages\n");
+	break;
+    }
+  }
 
-  else if (!strcmp(name, "misc_depth"))
-    obj->attr->misc.depth = strtoul(value, NULL, 10);
+  else if (!strcmp(name, "dmi_board_vendor")) {
+    switch (obj->type) {
+      case TOPO_OBJ_MACHINE:
+	obj->attr->machine.dmi_board_vendor = strdup(value);
+	break;
+      case TOPO_OBJ_SYSTEM:
+	obj->attr->system.dmi_board_vendor = strdup(value);
+	break;
+      default:
+	fprintf(stderr, "ignoring dmi_board_vendor attribute for object type without DMI board\n");
+	break;
+    }
+  }
+
+  else if (!strcmp(name, "dmi_board_name")) {
+    switch (obj->type) {
+      case TOPO_OBJ_MACHINE:
+	obj->attr->machine.dmi_board_name = strdup(value);
+	break;
+      case TOPO_OBJ_SYSTEM:
+	obj->attr->system.dmi_board_name = strdup(value);
+	break;
+      default:
+	fprintf(stderr, "ignoring dmi_board_name attribute for object type without DMI board\n");
+	break;
+    }
+  }
 
   else
     fprintf(stderr, "ignoring unknown object attribute %s\n", name);
@@ -153,7 +214,7 @@ topo__look_xml_attr(struct topo_topology *topology, struct topo_obj *obj,
 	break;
       }
     } else {
-	fprintf(stderr, "ignoring unexpected xml attr node type %u name %s\n", node->type, node->name);
+	fprintf(stderr, "ignoring unexpected xml attr node type %u name %s\n", node->type, (const char*) node->name);
     }
   }
 }
@@ -169,19 +230,47 @@ topo__look_xml_node(struct topo_topology *topology, xmlNode *node, int depth)
       if (depth == 0) {
 	/* root node should be in root class */
 	if (strcmp((const char *) node->name, "root"))
-	  fprintf(stderr, "root node of class `%s' instead of `root'\n", node->name);
+	  fprintf(stderr, "root node of class `%s' instead of `root'\n", (const char*) node->name);
 
 	/* no object yet */
 
       } else {
 	/* object node should be in object class */
 	if (strcmp((const char*) node->name, "object"))
-	  fprintf(stderr, "object node of class `%s' instead of `object'\n", node->name);
+	  fprintf(stderr, "object node of class `%s' instead of `object'\n", (const char*) node->name);
 
 	if (depth > 1)
 	  obj = topo_alloc_setup_object(TOPO_OBJ_TYPE_MAX, -1);
 	else
 	  obj = topology->levels[0][0];
+      }
+
+      /* first determine the object type */
+      for (attr = node->properties; attr; attr = attr->next) {
+	if (attr->type == XML_ATTRIBUTE_NODE) {
+	  xmlNode *node;
+	  for (node = attr->children; node; node = node->next) {
+	    if (node->type == XML_TEXT_NODE) {
+	      if (node->content && node->content[0] != '\0' && node->content[0] != '\n') {
+		if (!strcmp((const char*) attr->name, "type")) {
+		  obj->type = topo_obj_type_of_string((const char*) node->content);
+		  if (obj->type == TOPO_OBJ_TYPE_MAX)
+		    fprintf(stderr, "ignoring unknown object type %s\n", (const char*) node->content);
+		  else
+		    break;
+		}
+	      }
+	    } else {
+		fprintf(stderr, "ignoring unexpected xml attr node type %u name %s\n", node->type, (const char*) node->name);
+	    }
+	  }
+	  if (obj->type == TOPO_OBJ_TYPE_MAX) {
+	    fprintf(stderr, "ignoring attributes of object without type\n");
+	    return;
+	  }
+	} else {
+	  fprintf(stderr, "ignoring unexpected xml attr type %u\n", node->type);
+	}
       }
 
       /* process attributes */
@@ -230,7 +319,7 @@ topo__look_xml_node(struct topo_topology *topology, xmlNode *node, int depth)
 
     } else if (node->type == XML_TEXT_NODE) {
       if (node->content && node->content[0] != '\0' && node->content[0] != '\n')
-	fprintf(stderr, "ignoring object text content %s\n", node->content);      
+	fprintf(stderr, "ignoring object text content %s\n", (const char*) node->content);      
     } else {
       fprintf(stderr, "ignoring unexpected xml node type %u\n", node->type);
     }
