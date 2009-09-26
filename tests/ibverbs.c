25b6e6f1dc81dcae056c8826fb72a366df7e1025
Brice Goglin <brice.goglin@inria.fr>
2009-09-26 15:00:11 +0000

    Add a ibverbs test which prints the cpuset of e...
    Add a ibverbs test which prints the cpuset of each infiniband device.
    
    It uses ibverbs so we have to check for its headers and lib
    at configure. I wonder if we should just make this an example
    and let the user build/link it properly when testing it.
    
    This commit was SVN r1013.

---
+++ tests/ibverbs.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2009 CNRS, INRIA, Université Bordeaux 1
+ * See COPYING in top-level directory.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+#include <infiniband/verbs.h>
+#include <hwloc.h>
+#include <hwloc/ibverbs.h>
+
+/* check the ibverbs helpers */
+
+int main() {
+  struct ibv_device **dev_list, *dev;
+  int count, i;
+
+  dev_list = ibv_get_device_list(&count);
+  if (!dev_list) {
+    fprintf(stderr, "ibv_get_device_list failed\n");
+    return 0;
+  }
+  printf("ibv_get_device_list found %d devices\n", count);
+
+  for(i=0; i<count; i++) {
+    char cpuset_string[HWLOC_CPUSET_STRING_LENGTH];
+    hwloc_cpuset_t set;
+    dev = dev_list[i];
+
+    if (hwloc_ibverbs_get_device_cpuset(dev, &set) < 0) {
+      printf("failed to get cpuset for device %d (%s)\n",
+	     i, ibv_get_device_name(dev));
+    } else {
+      hwloc_cpuset_snprintf(cpuset_string, sizeof(cpuset_string), &set);
+      printf("got cpuset %s for device %d (%s)\n",
+	     cpuset_string, i, ibv_get_device_name(dev));
+    }
+  }
+
+  ibv_free_device_list(dev_list);
+
+  return 0;
+}
