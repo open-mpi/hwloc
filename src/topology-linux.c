d7076e1fac566b10fc5826f1b51b96fb433f804d
Samuel Thibault <samuel.thibault@labri.fr>
2009-10-26 13:51:13 +0000

    backport r1246: really use the value returned b...
    backport r1246: really use the value returned by fscanf, even if we'd catch it later
    
    This commit was SVN r1247.
    
    The following SVN revision numbers were found above:
      r1246 --> d9efc5fee2d8c59b492cf45ce84a002c7ca8555d

--- src/topology-linux.c
+++ src/topology-linux.c
@@ -877,7 +877,8 @@ look_cpuinfo(struct hwloc_topology *topology, const char *path,
 	if (str[strlen(str)-1]!='\n')
 	  {
             /* ignore end of line */
-	    (void) fscanf(fd,"%*[^\n]");
+	    if (fscanf(fd,"%*[^\n]") == EOF)
+	      break;
 	    getc(fd);
 	  }
     }
