6d65a3b21adeda9f21a9d3956fd62d0b675b1cab
Samuel Thibault <samuel.thibault@labri.fr>
2009-12-07 13:47:51 +0000

    Add BSD sysctl usage to get the number of CPUs
    This commit was SVN r1442.

--- tests/ports/include/sys/sysctl.h
+++ tests/ports/include/sys/sysctl.h
@@ -6,6 +6,7 @@
 #ifndef HWLOC_PORT_SYS_SYSCTL_H
 #define HWLOC_PORT_SYS_SYSCTL_H
 
+extern int sysctl(int *name, int name_len, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 extern int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 
 #endif /* HWLOC_PORT_SYS_SYSCTL_H */
