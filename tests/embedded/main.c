fadc5f679f9aae27b623c95f91208b2f139e089e
Jeff Squyres <jsquyres@cisco.com>
2010-02-25 22:57:07 +0000

    Add some more print statements to this test, ju...
    Add some more print statements to this test, just to help
    differentiate the output when debugging is enabled
    
    This commit was SVN r1752.

--- tests/embedded/main.c
+++ tests/embedded/main.c
@@ -15,12 +15,18 @@ int main(int argc, char *argv[])
 
     /* Just call a bunch of functions to see if we can link and run */
 
+    printf("*** Test 1: cpuset alloc\n");
     cpu_set = mytest_hwloc_cpuset_alloc();
+    printf("*** Test 2: topology init\n");
     mytest_hwloc_topology_init(&topology);
+    printf("*** Test 3: topology load\n");
     mytest_hwloc_topology_load(topology);
+    printf("*** Test 4: topology get depth\n");
     depth = mytest_hwloc_topology_get_depth(topology);
-    printf("Max depth: %u\n", depth);
+    printf("    Max depth: %u\n", depth);
+    printf("*** Test 5: topology destroy\n");
     mytest_hwloc_topology_destroy(topology);
+    printf("*** Test 6: cpuset free\n");
     mytest_hwloc_cpuset_free(cpu_set);
 
     return 0;
