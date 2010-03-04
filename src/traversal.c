39cc824a5a4d4b03b3981fa025c1dbb3816dc894
Brice Goglin <brice.goglin@inria.fr>
2010-03-04 07:04:08 +0000

    Do not print a separator before DMI vendor sinc...
    Do not print a separator before DMI vendor since we'll add later if really necessary
    
    This commit was SVN r1800.

--- src/traversal.c
+++ src/traversal.c
@@ -222,11 +222,10 @@ hwloc_obj_attr_snprintf(char * __hwloc_restrict string, size_t size, hwloc_obj_t
   switch (obj->type) {
   case HWLOC_OBJ_MACHINE:
     if (verbose)
-      hwloc_snprintf(specific, sizeof(specific), "%s%s%s%s",
-		     obj->attr->machine.dmi_board_vendor?separator:"",
-		     obj->attr->machine.dmi_board_vendor?obj->attr->machine.dmi_board_vendor:"",
-		     obj->attr->machine.dmi_board_name?separator:"",
-		     obj->attr->machine.dmi_board_name?obj->attr->machine.dmi_board_name:"");
+      hwloc_snprintf(specific, sizeof(specific), "%s%s%s",
+		     obj->attr->machine.dmi_board_vendor ? obj->attr->machine.dmi_board_vendor : "",
+		     obj->attr->machine.dmi_board_vendor && obj->attr->machine.dmi_board_name ? separator : "",
+		     obj->attr->machine.dmi_board_name ? obj->attr->machine.dmi_board_name : "");
     break;
   case HWLOC_OBJ_CACHE:
     hwloc_snprintf(specific, sizeof(specific), "%lu%s",
