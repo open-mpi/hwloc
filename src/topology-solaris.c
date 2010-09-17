0af85d4ea9cc33223aef61a026c02abbbfa71b6a
Brice Goglin <brice.goglin@inria.fr>
2010-09-17 08:16:21 +0000

    Fix the solaris grouping code to follow the new...
    Fix the solaris grouping code to follow the new hwloc_setup_misc_level_from_distances prototype
    
    This commit was SVN r2448.

--- src/topology-solaris.c
+++ src/topology-solaris.c
@@ -258,11 +258,14 @@ hwloc_look_lgrp(struct hwloc_topology *topology)
 #ifdef HAVE_LGRP_LATENCY_COOKIE
     {
       unsigned distances[curlgrp][curlgrp];
+      unsigned indexes[curlgrp];
       unsigned i, j;
-      for (i = 0; i < curlgrp; i++)
+      for (i = 0; i < curlgrp; i++) {
+	indexes[i] = i;
 	for (j = 0; j < curlgrp; j++)
 	  distances[i][j] = lgrp_latency_cookie(cookie, glob_lgrps[i]->os_index, glob_lgrps[j]->os_index, LGRP_LAT_CPU_TO_MEM);
-      hwloc_setup_misc_level_from_distances(topology, curlgrp, glob_lgrps, (unsigned*) distances);
+      }
+      hwloc_setup_misc_level_from_distances(topology, curlgrp, glob_lgrps, (unsigned*) distances, (unsigned*) indexes);
     }
 #endif /* HAVE_LGRP_LATENCY_COOKIE */
   }
