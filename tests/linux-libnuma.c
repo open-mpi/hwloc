94030be31e602c6a0cceadf398848406363b3932
Jeff Squyres <jsquyres@cisco.com>
2010-11-08 14:27:27 +0000

    Fix from Brice:
    This commit fixes make check on Linux machines where some numa nodes
    have no cpu.  It is an adaptation of r2561 from the trunk (other
    things have changed such that directly bringing in r2561 on the v1.0
    branch isn't possible).
    
    This commit was SVN r2757.
    
    The following SVN revision numbers were found above:
      r2561 --> 263cfd02db471a66f197451099f1f8d6645f0ecc

--- tests/linux-libnuma.c
+++ tests/linux-libnuma.c
@@ -1,5 +1,7 @@
 /*
- * Copyright © 2009 CNRS, INRIA, Université Bordeaux 1
+ * Copyright © 2009 CNRS
+ * Copyright © 2009-2010 INRIA
+ * Copyright © 2009-2010 Université Bordeaux 1
  * See COPYING in top-level directory.
  */
 
@@ -14,23 +16,28 @@
 int main(void)
 {
   hwloc_topology_t topology;
-  hwloc_cpuset_t set, set2;
+  hwloc_cpuset_t set, set2, nocpunodeset;
   hwloc_obj_t node;
   struct bitmask *bitmask, *bitmask2;
   nodemask_t nodemask, nodemask2;
   unsigned long mask;
   unsigned long maxnode;
+  int i;
 
   hwloc_topology_init(&topology);
   hwloc_topology_load(topology);
 
-  /* convert full nodemask/bitmask to cpuset */
+  /* convert full stuff between cpuset and libnuma */
   set = hwloc_cpuset_alloc();
+  nocpunodeset = hwloc_cpuset_alloc();
   /* gather all nodes if any, or the whole system if no nodes */
   if (hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_NODE)) {
     node = NULL;
-    while ((node = hwloc_get_next_obj_by_type(topology, HWLOC_OBJ_NODE, node)) != NULL)
+    while ((node = hwloc_get_next_obj_by_type(topology, HWLOC_OBJ_NODE, node)) != NULL) {
       hwloc_cpuset_or(set, set, node->cpuset);
+      if (hwloc_cpuset_iszero(node->cpuset))
+	hwloc_cpuset_set(nocpunodeset, node->os_index);
+    }
   } else {
     hwloc_cpuset_or(set, set, hwloc_topology_get_complete_cpuset(topology));
   }
@@ -45,17 +52,19 @@ int main(void)
   assert(hwloc_cpuset_isequal(set, set2));
   hwloc_cpuset_free(set2);
 
-
-  /* convert full cpuset to nodemask/bitmask */
   bitmask = hwloc_cpuset_to_linux_libnuma_bitmask(topology, set);
+  hwloc_cpuset_foreach_begin(i, nocpunodeset) { numa_bitmask_setbit(bitmask, i); } hwloc_cpuset_foreach_end();
   assert(numa_bitmask_equal(bitmask, numa_all_nodes_ptr));
   numa_bitmask_free(bitmask);
 
   hwloc_cpuset_to_linux_libnuma_nodemask(topology, set, &nodemask);
+  hwloc_cpuset_foreach_begin(i, nocpunodeset) { nodemask_set(&nodemask, i); } hwloc_cpuset_foreach_end();
   assert(!memcmp(&nodemask, &numa_all_nodes, sizeof(nodemask_t)));
-  hwloc_cpuset_free(set);
 
-  /* convert empty nodemask/bitmask to cpuset */
+  hwloc_cpuset_free(set);
+  hwloc_cpuset_free(nocpunodeset);
+
+  /* convert empty stuff between cpuset and libnuma */
   nodemask_zero(&nodemask);
   set = hwloc_cpuset_alloc();
   hwloc_cpuset_from_linux_libnuma_nodemask(topology, set, &nodemask);
@@ -75,8 +84,6 @@ int main(void)
   assert(hwloc_cpuset_iszero(set));
   hwloc_cpuset_free(set);
 
-
-  /* convert empty nodemask/bitmask from cpuset */
   set = hwloc_cpuset_alloc();
   bitmask = hwloc_cpuset_to_linux_libnuma_bitmask(topology, set);
   bitmask2 = numa_bitmask_alloc(1);
@@ -98,10 +105,10 @@ int main(void)
   assert(!maxnode);
   hwloc_cpuset_free(set);
 
-
-  /* convert first node nodemask/bitmask from/to cpuset */
+  /* convert first node between cpuset/nodeset and libnuma */
   node = hwloc_get_next_obj_by_type(topology, HWLOC_OBJ_NODE, NULL);
   if (node) {
+    /* convert first node between cpuset and libnuma */
     hwloc_cpuset_to_linux_libnuma_nodemask(topology, node->cpuset, &nodemask);
     assert(nodemask_isset(&nodemask, node->os_index));
     nodemask_clr(&nodemask, node->os_index);
@@ -125,6 +132,31 @@ int main(void)
       assert(maxnode = node->os_index + 1);
       assert(mask == (1U<<node->os_index));
     }
+
+    set = hwloc_cpuset_alloc();
+    nodemask_zero(&nodemask);
+    nodemask_set(&nodemask, node->os_index);
+    hwloc_cpuset_from_linux_libnuma_nodemask(topology, set, &nodemask);
+    assert(hwloc_cpuset_isequal(set, node->cpuset));
+    hwloc_cpuset_free(set);
+
+    set = hwloc_cpuset_alloc();
+    bitmask = numa_bitmask_alloc(1);
+    numa_bitmask_setbit(bitmask, 0);
+    hwloc_cpuset_from_linux_libnuma_bitmask(topology, set, bitmask);
+    numa_bitmask_free(bitmask);
+    assert(hwloc_cpuset_isequal(set, node->cpuset));
+    hwloc_cpuset_free(set);
+
+    set = hwloc_cpuset_alloc();
+    if (node->os_index >= sizeof(mask)*8) {
+      mask = 0;
+    } else {
+      mask = 1 << node->os_index;
+    }
+    hwloc_cpuset_from_linux_libnuma_ulongs(topology, set, &mask, 1);
+    assert(hwloc_cpuset_isequal(set, node->cpuset));
+    hwloc_cpuset_free(set);
   }
 
 
