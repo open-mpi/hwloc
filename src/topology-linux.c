3d8e8e0b361b177ccd43631a104ed917e3aff00e
Samuel Thibault <samuel.thibault@labri.fr>
2011-01-21 14:55:10 +0000

    enums are signed ints by default
    This commit was SVN r3122.

--- src/topology-linux.c
+++ src/topology-linux.c
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2009 CNRS
  * Copyright © 2009-2010 INRIA
- * Copyright © 2009-2010 Université Bordeaux 1
+ * Copyright © 2009-2011 Université Bordeaux 1
  * Copyright © 2009 Cisco Systems, Inc.  All rights reserved.
  * Copyright © 2010 IBM
  * See COPYING in top-level directory.
