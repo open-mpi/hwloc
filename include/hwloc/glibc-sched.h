44c789e7aa0bb0eb9787f6be15e050816fd403d4
Samuel Thibault <samuel.thibault@labri.fr>
2011-02-18 16:18:39 +0000

    also detect when CPU_SETSIZE didn't get defined
    This commit was SVN r3181.

--- include/hwloc/glibc-sched.h
+++ include/hwloc/glibc-sched.h
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2009 CNRS
  * Copyright © 2009-2010 INRIA
- * Copyright © 2009-2010 Université Bordeaux 1
+ * Copyright © 2009-2011 Université Bordeaux 1
  * See COPYING in top-level directory.
  */
 
@@ -20,7 +20,7 @@
 #include <hwloc/helper.h>
 #include <assert.h>
 
-#if !defined _GNU_SOURCE || !defined _SCHED_H
+#if !defined _GNU_SOURCE || !defined _SCHED_H || !defined CPU_SETSIZE
 #error Please make sure to include sched.h before including glibc-sched.h, and define _GNU_SOURCE before any inclusion of sched.h
 #endif
 
