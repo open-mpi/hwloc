82c79ddf2e8bdf40529bcd7afdb3567958ba8972
Samuel Thibault <samuel.thibault@labri.fr>
2011-03-09 17:09:59 +0000

    Avoid crashing just because the user provided a...
    Avoid crashing just because the user provided a bogus cpuset
    
    This commit was SVN r3260.

--- utils/hwloc-calc.c
+++ utils/hwloc-calc.c
@@ -1,7 +1,7 @@
 /*
  * Copyright © 2009 CNRS
  * Copyright © 2009-2011 INRIA.  All rights reserved.
- * Copyright © 2009-2010 Université Bordeaux 1
+ * Copyright © 2009-2011 Université Bordeaux 1
  * Copyright © 2009-2010 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
  */
@@ -80,7 +80,7 @@ hwloc_calc_hierarch_output(hwloc_topology_t topology, const char *prefix, hwloc_
   }
 }
 
-static void
+static int
 hwloc_calc_output(hwloc_topology_t topology, hwloc_bitmap_t set)
 {
   if (singlify)
@@ -93,6 +93,10 @@ hwloc_calc_output(hwloc_topology_t topology, hwloc_bitmap_t set)
       char type[64];
       unsigned idx;
       hwloc_obj_t obj = hwloc_get_first_largest_obj_inside_cpuset(topology, remaining);
+      if (!obj) {
+        fprintf(stderr, "No object included in this cpuset\n");
+        return EXIT_FAILURE;
+      }
       hwloc_obj_type_snprintf(type, sizeof(type), obj, 1);
       idx = logicalo ? obj->logical_index : obj->os_index;
       if (idx == (unsigned) -1)
@@ -131,6 +135,7 @@ hwloc_calc_output(hwloc_topology_t topology, hwloc_bitmap_t set)
     printf("%s\n", string);
     free(string);
   }
+  return EXIT_SUCCESS;
 }
 
 static int hwloc_calc_type_depth(const char *string, hwloc_obj_type_t *typep, int *depthp)
@@ -180,6 +185,7 @@ int main(int argc, char *argv[])
   char *callname;
   int opt;
   int i;
+  int ret = EXIT_SUCCESS;
 
   callname = argv[0];
 
@@ -354,7 +360,7 @@ int main(int argc, char *argv[])
 
   if (cmdline_args) {
     /* process command-line arguments */
-    hwloc_calc_output(topology, set);
+    ret = hwloc_calc_output(topology, set);
 
   } else {
     /* process stdin arguments line-by-line */
@@ -383,5 +389,5 @@ int main(int argc, char *argv[])
   free(hierdepth);
   free(hiertype);
 
-  return EXIT_SUCCESS;
+  return ret;
 }
