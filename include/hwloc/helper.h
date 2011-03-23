ea8f27b88365542f09acc9db16b22246e811b477
Brice Goglin <brice.goglin@inria.fr>
2011-03-23 22:32:25 +0000

    Doc clarification and doxygen syntax fix for hw...
    Doc clarification and doxygen syntax fix for hwloc_obj_is_in_subtree
    
    This commit was SVN r3304.

--- include/hwloc/helper.h
+++ include/hwloc/helper.h
@@ -202,8 +202,10 @@ hwloc_get_common_ancestor_obj (hwloc_topology_t topology __hwloc_attribute_unuse
   return obj1;
 }
 
-/** \brief Returns true if _obj_ is inside the subtree beginning
-    with \p subtree_root. */
+/** \brief Returns true if \p obj is inside the subtree beginning with \p subtree_root.
+ *
+ * \note This function assumes that both \p obj and \p subtree_root have a \p cpuset.
+ */
 static __hwloc_inline int __hwloc_attribute_pure
 hwloc_obj_is_in_subtree (hwloc_topology_t topology __hwloc_attribute_unused, hwloc_obj_t obj, hwloc_obj_t subtree_root)
 {
