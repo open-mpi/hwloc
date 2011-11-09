1f84c196e0d8c27d154e3c5f47b8baecc8a77476
Brice Goglin <brice.goglin@inria.fr>
2011-11-09 10:02:41 +0000

    Backport interesting parts of trunk commit r395...
    Backport interesting parts of trunk commit r3957 into v1.3 branch:
    * Random fixes from clang/scan-build:
      * move closedir inside if-not-null case
      * assert an impossible switch case
    
    This commit was SVN r3958.
    
    The following SVN revision numbers were found above:
      r3957 --> 25eb60cb973927351c0d390a77885974eeb97144

--- src/topology-xml.c
+++ src/topology-xml.c
@@ -1088,7 +1088,7 @@ hwloc__xml_export_escape_string(const char *src)
     case '<':  strcpy(dst, "&lt;");   replen=4; break;
     case '>':  strcpy(dst, "&gt;");   replen=4; break;
     case '&':  strcpy(dst, "&amp;");  replen=5; break;
-    default: break;
+    default: assert(0);
     }
     dst+=replen; src++;
 
