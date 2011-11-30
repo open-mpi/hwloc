fdccd06f2680e95fe2270ff641710d745974a03d
Brice Goglin <brice.goglin@inria.fr>
2011-11-30 14:53:35 +0000

    Rework the solaris code that gathers CPUModel/T...
    Rework the solaris code that gathers CPUModel/Type (commit r3964)
    
    Terry's code stored the attributes in the machine object and assumed
    that all sockets are identical (should be true for all platforms
    that matter for Oracle and OMPI). Move these attributes to the socket
    object to match what Linux does.
    
    We still assume that all sockets are identical because reworking
    the chiptype detection code to handle different sockets isn't trivial.
    
    We could keep a copy of the attributes in the machine object when
    all sockets are identical, but we'd have to document it and users
    may start asking why we don't have copies on other object levels.
    
    This commit was SVN r4008.
    
    The following SVN revision numbers were found above:
      r3964 --> 65e8d420c548a8e67ff6cc780012ed7ce4b863b2

--- src/topology-solaris.c
+++ src/topology-solaris.c
@@ -442,6 +442,10 @@ hwloc_look_lgrp(struct hwloc_topology *topology)
 static int
 hwloc_look_kstat(struct hwloc_topology *topology)
 {
+  /* FIXME this assumes that all sockets are identical */
+  char *CPUType = hwloc_solaris_get_chip_type();
+  char *CPUModel = hwloc_solaris_get_chip_model();
+
   kstat_ctl_t *kc = kstat_open();
   kstat_t *ksp;
   kstat_named_t *stat;
@@ -610,8 +614,22 @@ hwloc_look_kstat(struct hwloc_topology *topology)
        * however. */
     }
 
-  if (look_chips)
-    hwloc_setup_level(procid_max, numsockets, osphysids, proc_physids, topology, HWLOC_OBJ_SOCKET);
+  if (look_chips) {
+    struct hwloc_obj *obj;
+    unsigned j;
+    hwloc_debug("%d Sockets\n", numsockets);
+    for (j = 0; j < numsockets; j++) {
+      obj = hwloc_alloc_setup_object(HWLOC_OBJ_SOCKET, osphysids[j]);
+      if (CPUType)
+	hwloc_obj_add_info(obj, "CPUType", CPUType);
+      if (CPUModel)
+	hwloc_obj_add_info(obj, "CPUModel", CPUModel);
+      hwloc_object_cpuset_from_array(obj, j, proc_physids, procid_max);
+      hwloc_debug_1arg_bitmap("Socket %d has cpuset %s\n", j, obj->cpuset);
+      hwloc_insert_object_by_cpuset(topology, obj);
+    }
+    hwloc_debug("%s", "\n");
+  }
 
   if (look_cores)
     hwloc_setup_level(procid_max, numcores, oscoreids, proc_coreids, topology, HWLOC_OBJ_CORE);
@@ -629,31 +647,17 @@ void
 hwloc_look_solaris(struct hwloc_topology *topology)
 {
   unsigned nbprocs = hwloc_fallback_nbprocessors (topology);
-  char *CPUType;
-  char *CPUModel;
 #ifdef HAVE_LIBLGRP
   hwloc_look_lgrp(topology);
 #endif /* HAVE_LIBLGRP */
 #ifdef HAVE_LIBKSTAT
   nbprocs = 0;
-  if (hwloc_look_kstat(topology)) {
-    /* Set CPU Type and Model for machine. */
-    CPUType = hwloc_solaris_get_chip_type();
-    CPUModel = hwloc_solaris_get_chip_model();
-    hwloc_obj_add_info(topology->levels[0][0], "CPUType", CPUType);
-    hwloc_obj_add_info(topology->levels[0][0], "CPUModel", CPUModel);
+  if (hwloc_look_kstat(topology))
     return;
-  }
 #endif /* HAVE_LIBKSTAT */
   hwloc_setup_pu_level(topology, nbprocs);
 
   hwloc_obj_add_info(topology->levels[0][0], "Backend", "Solaris");
-
-  /* Set CPU Type and Model for machine. */
-  CPUType = hwloc_solaris_get_chip_type();
-  CPUModel = hwloc_solaris_get_chip_model();
-  hwloc_obj_add_info(topology->levels[0][0], "CPUType", CPUType);
-  hwloc_obj_add_info(topology->levels[0][0], "CPUModel", CPUModel);
 }
 
 void
