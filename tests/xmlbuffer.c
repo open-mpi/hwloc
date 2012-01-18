ce04959ee9b55daa263f3a359f3332736f5eca95
Samuel Thibault <samuel.thibault@labri.fr>
2012-01-18 14:29:25 +0000

    Check the XML parser result
    This commit was SVN r4152.

--- tests/xmlbuffer.c
+++ tests/xmlbuffer.c
@@ -25,7 +25,7 @@ static int one_test(void)
   printf("exported to buffer %p length %d\n", buf1, size1);
 
   hwloc_topology_init(&topology);
-  hwloc_topology_set_xmlbuffer(topology, buf1, size1);
+  assert(!hwloc_topology_set_xmlbuffer(topology, buf1, size1));
   hwloc_topology_load(topology);
   assert(!hwloc_topology_is_thissystem(topology));
   hwloc_topology_export_xmlbuffer(topology, &buf2, &size2);
