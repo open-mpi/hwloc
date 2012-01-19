09aa0c5e7c576f4c7c0b7c20bd99c7e0ead115f6
Brice Goglin <brice.goglin@inria.fr>
2012-01-19 07:30:51 +0000

    add new freebsd sysctl defines to tests/ports
    This commit was SVN r4167.

--- tests/ports/include/sys/sysctl.h
+++ tests/ports/include/sys/sysctl.h
@@ -10,4 +10,7 @@
 extern int sysctl(int *name, int name_len, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 extern int sysctlbyname(const char *name, void *oldp, size_t *oldlenp, void *newp, size_t newlen);
 
+#define CTL_HW 6
+#define HW_PHYSMEM 5
+
 #endif /* HWLOC_PORT_SYS_SYSCTL_H */
