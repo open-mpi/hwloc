32f84a9197d7b8e0512629b3d89d644fa30e754b
Brice Goglin <brice.goglin@inria.fr>
2012-02-17 11:33:44 +0000

    Fix some I/O helpers to replace empty bitmap (L...
    Fix some I/O helpers to replace empty bitmap (Linux on buggy BIOS) with full cpuset
    
    Thanks to Albert Solernou for reporting the problem.
    http://www.open-mpi.org/community/lists/hwloc-users/2012/02/0551.php
    
    This commit was SVN r4317.

--- include/hwloc/cudart.h
+++ include/hwloc/cudart.h
@@ -87,6 +87,8 @@ hwloc_cudart_get_device_cpuset(hwloc_topology_t topology __hwloc_attribute_unuse
     return -1;
 
   hwloc_linux_parse_cpumap_file(sysfile, set);
+  if (hwloc_bitmap_iszero(set))
+    hwloc_bitmap_copy(set, hwloc_topology_get_complete_cpuset(topology));
 
   fclose(sysfile);
 #else
