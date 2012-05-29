eac5180d856d5d72b099ac9065c5dc248479cfc7
Brice Goglin <brice.goglin@inria.fr>
2012-05-29 14:43:48 +0000

    tests/ports: update AIX headers for icaches
    This commit was SVN r4527.

--- tests/ports/include/sys/systemcfg.h
+++ tests/ports/include/sys/systemcfg.h
@@ -10,8 +10,12 @@ struct {
   int dcache_size;
   int dcache_asc;
   int dcache_line;
+  int icache_size;
+  int icache_asc;
+  int icache_line;
   int L2_cache_size;
   int L2_cache_asc;
+  int cache_attrib;
 } _system_configuration;
 
 #endif /* HWLOC_PORT_SYS_SYSTEMCFG_H */
