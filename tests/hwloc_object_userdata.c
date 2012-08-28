975f7c03367d1bc36ce721b07e08f414b0e41ad4
Brice Goglin <brice.goglin@inria.fr>
2012-08-28 14:06:34 +0000

    Check export/import userdata callbacks in tests...
    Check export/import userdata callbacks in tests/hwloc_object_userdata.c
    
    This commit was SVN r4782.

--- tests/hwloc_object_userdata.c
+++ tests/hwloc_object_userdata.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009 inria.  All rights reserved.
+ * Copyright © 2009-2012 Inria.  All rights reserved.
  * Copyright © 2009 Université Bordeaux 1
  * Copyright © 2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -9,6 +9,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <string.h>
 #include <assert.h>
 
@@ -27,9 +28,54 @@ static void check(hwloc_topology_t topology)
   }
 }
 
+static void export_cb(void *reserved, hwloc_topology_t topo, hwloc_obj_t obj)
+{
+  char tmp[17];
+  int err;
+  sprintf(tmp, "%016llx", (unsigned long long) (uintptr_t) obj->userdata);
+  err = hwloc_export_obj_userdata(reserved, topo, obj, "MyName", tmp, 16);
+  assert(err >= 0);
+}
+
+static void import_cb(hwloc_topology_t topo __hwloc_attribute_unused, hwloc_obj_t obj, const char *name, const void *buffer, size_t length)
+{
+  int err;
+  char tmp[17];
+  uint64_t val;
+
+  err = strcmp("MyName", name);
+  assert(!err);
+
+  assert(length == 16);
+  memcpy(tmp, buffer, 16);
+  tmp[16] = '\0';
+
+  val = strtoull(buffer, NULL, 0);
+
+  switch (val) {
+  case 0x1:
+    assert(obj->type == HWLOC_OBJ_MACHINE);
+    obj->userdata = (void *)(uintptr_t) 0x4;
+    break;
+  case 0x2:
+    assert(obj->type == HWLOC_OBJ_CACHE);
+    obj->userdata = (void *)(uintptr_t) 0x5;
+    break;
+  case 0x3:
+    assert(obj->type == HWLOC_OBJ_PU);
+    obj->userdata = (void *)(uintptr_t) 0x6;
+    break;
+  default:
+    assert(0);
+  }
+}
+
 int main(void)
 {
-  hwloc_topology_t topology;
+  hwloc_topology_t topology, reimport;
+  hwloc_obj_t obj1, obj2, obj3;
+  char *xmlbuf;
+  int xmlbuflen;
 
   /* check the real topology */
   hwloc_topology_init(&topology);
@@ -42,7 +88,46 @@ int main(void)
   hwloc_topology_set_synthetic(topology, "6 5 4 3 2");
   hwloc_topology_load(topology);
   check(topology);
-  hwloc_topology_destroy(topology);
 
+  /* now place some userdata and see if importing/exporting works well */
+  obj1 = hwloc_get_root_obj(topology);
+  assert(obj1);
+  obj1->userdata = (void *)(uintptr_t) 0x1;
+  obj2 = hwloc_get_obj_by_depth(topology, 3, 13);
+  assert(obj2);
+  obj2->userdata = (void *)(uintptr_t) 0x2;
+  obj3 = hwloc_get_obj_by_depth(topology, 5, 2*3*4*5*6-1);
+  assert(obj3);
+  obj3->userdata = (void *)(uintptr_t) 0x3;
+
+  /* export/import without callback, we get nothing */
+  hwloc_topology_export_xmlbuffer(topology, &xmlbuf, &xmlbuflen);
+
+  hwloc_topology_init(&reimport);
+  hwloc_topology_set_xmlbuffer(reimport, xmlbuf, xmlbuflen);
+  hwloc_topology_load(reimport);
+  check(reimport); /* there should be no userdata */
+  hwloc_topology_destroy(reimport);
+
+  /* export/import with callback, we should get three userdata */
+  hwloc_topology_set_userdata_export_callback(topology, export_cb);
+  hwloc_topology_export_xmlbuffer(topology, &xmlbuf, &xmlbuflen);
+
+  hwloc_topology_init(&reimport);
+  hwloc_topology_set_userdata_import_callback(reimport, import_cb);
+  hwloc_topology_set_xmlbuffer(reimport, xmlbuf, xmlbuflen);
+  hwloc_topology_load(reimport);
+  obj1 = hwloc_get_root_obj(reimport);
+  assert(obj1);
+  assert(obj1->userdata == (void *)(uintptr_t) 0x4);
+  obj2 = hwloc_get_obj_by_depth(reimport, 3, 13);
+  assert(obj2);
+  assert(obj2->userdata == (void *)(uintptr_t) 0x5);
+  obj3 = hwloc_get_obj_by_depth(reimport, 5, 2*3*4*5*6-1);
+  assert(obj3);
+  assert(obj3->userdata == (void *)(uintptr_t) 0x6);
+  hwloc_topology_destroy(reimport);
+
+  hwloc_topology_destroy(topology);
   return 0;
 }
