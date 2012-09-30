ab6074bb7a2794ae08feb81f2344b22a6ccf9ee0
Brice Goglin <brice.goglin@inria.fr>
2012-09-30 08:44:26 +0000

    Fix the management of the thissystem flag
    It is initialized to 1 during init().
    If the application calls set_foo() to change the backend,
    it may overwrite the flag with set_flags().
    If the application changes the backend with env vars,
    it may overwrite the flag with HWLOC_THISSYSTEM env var.
    
    Things must be processed in this order, and the flag must
    be ready early during the discovery because part of the
    code uses it.
    
    HWLOC_FORCE_THISSYSTEM was not stronger than the correct
    HWLOC_THISSYSTEM env var, so it's removed
    (it was never documented anyway).
    
    The main problem that this commit fixes is that set_flags()
    was processed to late and would overwrite what the env var
    did. But the env vars are stronger that set_*().
    Otherwise, it's mostly cleanups, reordering and documentation.
    
    This commit was SVN r4873.

--- src/topology.c
+++ src/topology.c
@@ -2383,9 +2383,6 @@ hwloc_discover(struct hwloc_topology *topology)
    * hooks.
    */
 
-  if (topology->flags & HWLOC_TOPOLOGY_FLAG_IS_THISSYSTEM)
-    topology->is_thissystem = 1;
-
   if (topology->is_thissystem) {
 #    ifdef HWLOC_LINUX_SYS
     hwloc_set_linuxfs_hooks(topology);
@@ -2837,6 +2834,15 @@ hwloc_topology_load (struct hwloc_topology *topology)
     topology->is_loaded = 0;
   }
 
+  /* Apply is_thissystem topology flag before we enforce envvar backends.
+   * If the application changed the backend with set_foo(),
+   * it may use set_flags() update the is_thissystem flag here.
+   * If it changes the backend with environment variables below,
+   * it may use HWLOC_THISSYSTEM envvar below as well.
+   */
+  if (topology->flags & HWLOC_TOPOLOGY_FLAG_IS_THISSYSTEM)
+    topology->is_thissystem = 1;
+
   /* enforce backend anyway if a FORCE variable was given */
 #ifdef HWLOC_LINUX_SYS
   {
@@ -2869,11 +2875,6 @@ hwloc_topology_load (struct hwloc_topology *topology)
       hwloc_backend_xml_init(topology, xmlpath_env, NULL, 0);
   }
 
-  /* always apply non-FORCE THISSYSTEM since it was explicitly designed to override setups from other backends */
-  local_env = getenv("HWLOC_THISSYSTEM");
-  if (local_env)
-    topology->is_thissystem = atoi(local_env);
-
   /* if we haven't chosen the backend, set the OS-specific one if needed */
   if (topology->backend_type == HWLOC_BACKEND_NONE) {
 #ifdef HWLOC_LINUX_SYS
@@ -2881,6 +2882,10 @@ hwloc_topology_load (struct hwloc_topology *topology)
       goto out;
 #endif
   }
+
+  local_env = getenv("HWLOC_THISSYSTEM");
+  if (local_env)
+    topology->is_thissystem = atoi(local_env);
 
   /* get distance matrix from the environment are store them (as indexes) in the topology.
    * indexes will be converted into objects later once the tree will be filled
@@ -2891,11 +2896,6 @@ hwloc_topology_load (struct hwloc_topology *topology)
   err = hwloc_discover(topology);
   if (err < 0)
     goto out;
-
-  /* enforce THISSYSTEM if given in a FORCE variable */
-  local_env = getenv("HWLOC_FORCE_THISSYSTEM");
-  if (local_env)
-    topology->is_thissystem = atoi(local_env);
 
 #ifndef HWLOC_DEBUG
   if (getenv("HWLOC_DEBUG_CHECK"))
