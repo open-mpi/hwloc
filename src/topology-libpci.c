f4baa392d5d743b398fb1d047073aaaf7401cb90
Brice Goglin <brice.goglin@inria.fr>
2012-11-08 15:23:28 +0000

    libpci: make traverse callback data more generi...
    libpci: make traverse callback data more generic
    
    No functional change.
    
    This prepares for componentification by sync'ing all the internal
    synthetic code with the components branch, except the actual
    componentification.
    
    This commit was SVN r5014.

--- src/topology-libpci.c
+++ src/topology-libpci.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2011 inria.  All rights reserved.
+ * Copyright © 2009-2012 Inria.  All rights reserved.
  * Copyright © 2009-2011 Université Bordeaux 1
  * See COPYING in top-level directory.
  */
@@ -23,7 +23,8 @@
 #define CONFIG_SPACE_CACHESIZE 256
 
 static void
-hwloc_pci_traverse_print_cb(struct hwloc_topology *topology __hwloc_attribute_unused, struct hwloc_obj *pcidev, int depth __hwloc_attribute_unused)
+hwloc_pci_traverse_print_cb(void * cbdata __hwloc_attribute_unused,
+			    struct hwloc_obj *pcidev, int depth __hwloc_attribute_unused)
 {
   char busid[14];
   snprintf(busid, sizeof(busid), "%04x:%02x:%02x.%01x",
@@ -45,15 +46,19 @@ hwloc_pci_traverse_print_cb(struct hwloc_topology *topology __hwloc_attribute_un
 }
 
 static void
-hwloc_pci_traverse_setbridgedepth_cb(struct hwloc_topology *topology __hwloc_attribute_unused, struct hwloc_obj *pcidev, int depth)
+hwloc_pci_traverse_setbridgedepth_cb(void * cbdata __hwloc_attribute_unused,
+				     struct hwloc_obj *pcidev, int depth)
 {
   if (pcidev->type == HWLOC_OBJ_BRIDGE)
     pcidev->attr->bridge.depth = depth;
 }
 
 static void
-hwloc_pci_traverse_lookuposdevices_cb(struct hwloc_topology *topology, struct hwloc_obj *pcidev, int depth __hwloc_attribute_unused)
+hwloc_pci_traverse_lookuposdevices_cb(void * cbdata,
+				      struct hwloc_obj *pcidev, int depth __hwloc_attribute_unused)
 {
+  struct hwloc_topology *topology = cbdata;
+
   if (pcidev->type == HWLOC_OBJ_BRIDGE)
     return;
 
@@ -63,24 +68,24 @@ hwloc_pci_traverse_lookuposdevices_cb(struct hwloc_topology *topology, struct hw
 }
 
 static void
-hwloc_pci__traverse(struct hwloc_topology *topology, struct hwloc_obj *root,
-		    void (*cb)(struct hwloc_topology *, struct hwloc_obj *, int depth),
+hwloc_pci__traverse(void * cbdata, struct hwloc_obj *root,
+		    void (*cb)(void * cbdata, struct hwloc_obj *, int depth),
 		    int depth)
 {
   struct hwloc_obj *child = root->first_child;
   while (child) {
-    cb(topology, child, depth);
+    cb(cbdata, child, depth);
     if (child->type == HWLOC_OBJ_BRIDGE)
-      hwloc_pci__traverse(topology, child, cb, depth+1);
+      hwloc_pci__traverse(cbdata, child, cb, depth+1);
     child = child->next_sibling;
   }
 }
 
 static void
-hwloc_pci_traverse(struct hwloc_topology *topology, struct hwloc_obj *root,
-		   void (*cb)(struct hwloc_topology *, struct hwloc_obj *, int depth))
+hwloc_pci_traverse(void * cbdata, struct hwloc_obj *root,
+		   void (*cb)(void * cbdata, struct hwloc_obj *, int depth))
 {
-  hwloc_pci__traverse(topology, root, cb, 0);
+  hwloc_pci__traverse(cbdata, root, cb, 0);
 }
 
 enum hwloc_pci_busid_comparison_e {
@@ -476,14 +481,14 @@ hwloc_look_libpci(struct hwloc_topology *topology)
   pci_cleanup(pciaccess);
 
   hwloc_debug("%s", "\nPCI hierarchy after basic scan:\n");
-  hwloc_pci_traverse(topology, &fakehostbridge, hwloc_pci_traverse_print_cb);
+  hwloc_pci_traverse(NULL, &fakehostbridge, hwloc_pci_traverse_print_cb);
 
   if (!fakehostbridge.first_child)
     /* found nothing, exit */
     return 0;
 
   /* walk the hierarchy, set bridge depth and lookup OS devices */
-  hwloc_pci_traverse(topology, &fakehostbridge, hwloc_pci_traverse_setbridgedepth_cb);
+  hwloc_pci_traverse(NULL, &fakehostbridge, hwloc_pci_traverse_setbridgedepth_cb);
   hwloc_pci_traverse(topology, &fakehostbridge, hwloc_pci_traverse_lookuposdevices_cb);
 
   /*
