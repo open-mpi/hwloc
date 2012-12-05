0cd84d5b78b24c2bfffdd3281c3390b17d4b157e
Brice Goglin <brice.goglin@inria.fr>
2012-12-05 12:42:41 +0000

    cleanup tests/rename/, make sure all headers ge...
    cleanup tests/rename/, make sure all headers get tested (when supported)
    
    This commit was SVN r5096.

--- tests/rename/main.c
+++ tests/rename/main.c
@@ -1,20 +1,25 @@
 #define _GNU_SOURCE 1
 #include "sched.h"
 
+/* all headers should be included */
 #include "hwloc.h"
+#include "hwloc/bitmap.h"
+#include "hwloc/helper.h"
 
 /* enable those that have the right dependencies on your machine */
-#include "hwloc/glibc-sched.h"
-#include "hwloc/linux-libnuma.h"
-#include "hwloc/linux.h"
 //#include "hwloc/cuda.h"
 //#include "hwloc/cudart.h"
+#include "hwloc/glibc-sched.h"
+#include "hwloc/linux.h"
+#include "hwloc/linux-libnuma.h"
 //#include "hwloc/myriexpress.h"
-#include "hwloc/openfabrics-verbs.h"
+//#include "hwloc/openfabrics-verbs.h"
 
 #include "private/autogen/config.h"
+#include "private/components.h"
 #include "private/cpuid.h"
 #include "private/debug.h"
 #include "private/misc.h"
 #include "private/private.h"
+#include "private/solaris-chiptype.h"
 #include "private/xml.h"
