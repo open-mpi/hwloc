2838864103a5c439c36db450d3ea7ea4b849ac2d
Brice Goglin <brice.goglin@inria.fr>
2012-12-07 16:35:47 +0000

    tests/ports/FreeBSD: reenable pthread affinity_...
    tests/ports/FreeBSD: reenable pthread affinity_np prototypes
    
    Add a dummy pthread.h to prevent the Linux one from causing conflicts.
    
    This commit was SVN r5130.

--- tests/ports/include/freebsd/pthread_np.h
+++ tests/ports/include/freebsd/pthread_np.h
@@ -6,10 +6,9 @@
 #ifndef HWLOC_PORT_FREEBSD_PTHREAD_NP_H
 #define HWLOC_PORT_FREEBSD_PTHREAD_NP_H
 
-#if 0
-/* Conflict with Linux' */
+#include <sys/cpuset.h>
+
 int pthread_getaffinity_np(pthread_t, size_t, cpuset_t *);
 int pthread_setaffinity_np(pthread_t, size_t, const cpuset_t *);
-#endif
 
 #endif /* HWLOC_PORT_FREEBSD_PTHREAD_NP_H */
