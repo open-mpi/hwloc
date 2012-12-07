2aa2268a157202505c7c8739be4446bf66e3decd
Brice Goglin <brice.goglin@inria.fr>
2012-12-07 16:35:34 +0000

    tests/ports/NetBSD: declare pthread things wher...
    tests/ports/NetBSD: declare pthread things where they belong
    
    This commit was SVN r5129.

--- tests/ports/include/netbsd/sched.h
+++ tests/ports/include/netbsd/sched.h
@@ -19,9 +19,4 @@ size_t  cpuset_size(const cpuset_t *);
 int     sched_getaffinity_np(pid_t, size_t, cpuset_t *);
 int     sched_setaffinity_np(pid_t, size_t, cpuset_t *);
 
-pthread_t       pthread_self(void);
-
-int     pthread_getaffinity_np(pthread_t, size_t, cpuset_t *);
-int     pthread_setaffinity_np(pthread_t, size_t, cpuset_t *);
-
 #endif /* HWLOC_PORT_NETBSD_SCHED_H */
