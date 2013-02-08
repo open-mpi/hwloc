8b3454d395db0833c413fe819b53e11a7bb84876
Brice Goglin <brice.goglin@inria.fr>
2013-02-08 13:50:20 +0000

    Enable BGQ testing in tests/ports
    This commit was SVN r5301.

---
+++ tests/ports/include/bgq/spi/include/kernel/location.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright © 2013 Inria.  All rights reserved.
+ * See COPYING in top-level directory.
+ */
+
+#ifndef HWLOC_PORT_BGQ_KERNEL_LOCATION_H
+#define HWLOC_PORT_BGQ_KERNEL_LOCATION_H
+
+#include <stdint.h>
+
+uint32_t Kernel_ProcessorID( void );
+
+#endif /* HWLOC_PORT_BGQ_KERNEL_LOCATION_H */
