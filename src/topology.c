893e0ad746c96716784367b3115a6254890e79b7
Brice Goglin <brice.goglin@inria.fr>
2013-05-07 12:25:47 +0000

    Backport trunk commit r5592 into v1.6 branch: * Fix the merging of groups when they are I/O objects   I/O objects must be ignored merge_useless_child(). Two objects   are still identical/mergeable when they don't have the same I/O   children (I/O object locality is a cpuset, it's not attached to   one specific object among identical ones).   This fixes a spurious group object appearing between the machine   and NUMA nodes when importing from XML. The reason is that I/O   objects get added early during XML import, and distance processing   creates a group later. This group must be merged with the machine,   even if the machine has I/O children that the group doesn't have.   This doesn't occur in the non-XML case because I/O are added after   distance groups are created and merged.   Thanks for Elena Elkina for reporting the problem and testing the patch.
    This commit was SVN r5594.
    
    The following SVN revision numbers were found above:
      r5592 --> 9f984baa6d039f2f60e74ed61fb15f58d19debe1

--- src/topology.c
+++ src/topology.c
@@ -1519,23 +1519,36 @@ restrict_object_nodeset(hwloc_topology_t topology, hwloc_obj_t *pobj, hwloc_node
 static void
 merge_useless_child(hwloc_topology_t topology, hwloc_obj_t *pparent)
 {
-  hwloc_obj_t parent = *pparent, child, *pchild;
+  hwloc_obj_t parent = *pparent, child, *pchild, ios;
   int replacechild = 0, replaceparent = 0;
 
   for_each_child_safe(child, parent, pchild)
     merge_useless_child(topology, pchild);
 
   child = parent->first_child;
-  if (!child || child->next_sibling)
-    /* There are no or several children, it's useful to keep them.  */
+  if (!child)
+    /* There are no child, nothing to merge. */
     return;
+
+  if (child->next_sibling && !hwloc_obj_type_is_io(child->next_sibling->type))
+    /* There are several non-I/O children */
+    return;
+
+  /* There is one non-I/O child and possible some I/O children.
+   * I/O children shouldn't prevent merging because they can be attached
+   * to anything with the same locality.
+   * Move them to the side during merging, and append them back later.
+   * This is easy because I/O children are always last in the list.
+   */
+  ios = child->next_sibling;
+  child->next_sibling = NULL;
 
   /* Check whether parent and/or child can be replaced */
   if (topology->ignored_types[parent->type] == HWLOC_IGNORE_TYPE_KEEP_STRUCTURE)
     /* Parent can be ignored in favor of the child.  */
     replaceparent = 1;
   if (topology->ignored_types[child->type] == HWLOC_IGNORE_TYPE_KEEP_STRUCTURE)
-    /* Child can be ignored in favor of the parent.  */    
+    /* Child can be ignored in favor of the parent.  */
     replacechild = 1;
 
   /* Decide which one to actually replace */
@@ -1565,6 +1578,14 @@ merge_useless_child(hwloc_topology_t topology, hwloc_obj_t *pparent)
     print_object(topology, 0, child);
     parent->first_child = child->first_child;
     hwloc_free_unlinked_object(child);
+  }
+
+  if (ios) {
+    /* append I/O children to the list of children of the remaining object */
+    pchild = &((*pparent)->first_child);
+    while (*pchild)
+      pchild = &((*pchild)->next_sibling);
+    *pchild = ios;
   }
 }
 
