089185d9826efa4c5e607e3e2e09ec5ddfb2c9bb
Jeff Squyres <jsquyres@cisco.com>
2014-01-09 09:34:24 -0500

    If a git command fails, also print the repo in the error message

--- contrib/git/github-send-commit-mails.pl
+++ contrib/git/github-send-commit-mails.pl
@@ -8,14 +8,17 @@ die "Must specify location of source git repo"
     if ($#ARGV < 0);
 
 sub doit {
-    my ($cmd) = shift;
+    my ($cmd, $repo) = @_;
 
     my $rc;
     my $outfile = "/tmp/github-send-email-tmp.$$";
     unlink($outfile);
     $rc = system("$cmd >$outfile 2>&1");
     if (0 != $rc) {
-        print "Command failed: $cmd
+        print "Command failed:
+
+Command: $cmd
+Repo:    $repo
 Output:\n";
         open(IN, $outfile);
         print $_
@@ -33,7 +36,7 @@ foreach my $src_repo (@ARGV) {
     die "Could not chdir to $src_repo"
         if (getcwd() != $src_repo);
 
-    doit("/u/mpiteam/git/local/bin/git fetch");
-    doit("/u/mpiteam/git/local/bin/git push email");
+    doit("/u/mpiteam/git/local/bin/git fetch", $src_repo);
+    doit("/u/mpiteam/git/local/bin/git push email", $src_repo);
 }
 
