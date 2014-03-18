76867c4f5f373a81707fc409d6729d03b931291d
Brice Goglin <Brice.Goglin@inria.fr>
2014-03-18 08:30:51 +0100

    tests/ports: Fix Solaris procset.h for recent Linux where idtype_t and P_PID exist
    Don't bother defining the actual idtype_t/P_* as an enum.
    Linux has a different one. Just defines what's missing individually.
    
    (cherry picked from commit 6cad2d3c4180b6c03129b668fdd7454d827517ed)

--- tests/ports/include/solaris/sys/procset.h
+++ tests/ports/include/solaris/sys/procset.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009 inria.  All rights reserved.
+ * Copyright © 2009-2014 Inria.  All rights reserved.
  * Copyright © 2009 Université Bordeaux 1
  * See COPYING in top-level directory.
  */
@@ -7,10 +7,17 @@
 #ifndef HWLOC_PORT_SOLARIS_SYS_PROCSET_H
 #define HWLOC_PORT_SOLARIS_SYS_PROCSET_H
 
-typedef enum idtype {
-  P_PID,
-  P_LWPID,
-  P_MYID
-} idtype_t;
+#ifndef __ENUM_IDTYPE_T
+/* Linux doesn't have idtype_t unless __ENUM_IDTYPE_T is set */
+typedef int idtype_t;
+#ifndef P_PID
+/* If there's no idtype_t, P_PID could exist as a #define */
+#define P_PID 1
+#endif
+#endif
+
+/* Linux never has P_LWPID and P_MYID */
+#define P_LWPID 2
+#define P_MYID 3
 
 #endif /* HWLOC_PORT_SOLARIS_SYS_PROCSET_H */
