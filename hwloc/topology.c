open-mpi/hwloc

commit d1bc538007dfcc3ef1ad34369983431cbefd8b5a

Replace insert_misc_by_parent() with alloc()+insert_group_object()
Having Misc in the middle is a mess.
Only allow Misc insert as leaf of parent, without cpuset.

When inserting inside the tree, insert a Group instead.
Discard it unless it adds some structure.
Split hwloc_alloc_group_object() from hwloc_topology_insert_group_object()
so that we can precisely setup {complete_,}{cpu,node}set depending on the case.

hwloc/topology.c
@@ -1108,50 +1108,48 @@ hwloc_insert_object_by_parent(struct hwloc_topology *topology, hwloc_obj_t paren
   }
 }
 
-/* Adds a misc object _after_ detection, and thus has to reconnect all the pointers */
 hwloc_obj_t
-hwloc_topology_insert_misc_object_by_cpuset(struct hwloc_topology *topology, hwloc_const_bitmap_t cpuset, const char *name)
+hwloc_topology_alloc_group_object(struct hwloc_topology *topology __hwloc_attribute_unused)
 {
-  hwloc_obj_t obj, child;
+  hwloc_obj_t obj = hwloc_alloc_setup_object(HWLOC_OBJ_GROUP, -1);
+  if (!obj)
+    return NULL;
+  obj->attr->group.depth = -1;
+  return obj;
+}
+
+hwloc_obj_t
+hwloc_topology_insert_group_object(struct hwloc_topology *topology, hwloc_obj_t obj)
+{
+  hwloc_obj_t res;
 
   if (!topology->is_loaded) {
+    /* this could actually work, we would just need to disable connect_children/levels below */
     errno = EINVAL;
     return NULL;
   }
 
-  if (hwloc_bitmap_iszero(cpuset))
+  if ((!obj->cpuset || hwloc_bitmap_iszero(obj->cpuset))
+      && (!obj->complete_cpuset || hwloc_bitmap_iszero(obj->complete_cpuset))
+      && (!obj->nodeset || hwloc_bitmap_iszero(obj->nodeset))
+      && (!obj->complete_nodeset || hwloc_bitmap_iszero(obj->complete_nodeset))) {
+    hwloc_free_unlinked_object(obj);
+    errno = EINVAL;
     return NULL;
-  if (!hwloc_bitmap_isincluded(cpuset, hwloc_topology_get_topology_cpuset(topology)))
-    return NULL;
-
-  obj = hwloc_alloc_setup_object(HWLOC_OBJ_MISC, -1);
-  if (name)
-    obj->name = strdup(name);
-
-  /* misc objects go in no level */
-  obj->depth = (unsigned) HWLOC_TYPE_DEPTH_UNKNOWN;
-
-  obj->cpuset = hwloc_bitmap_dup(cpuset);
-  /* initialize default cpusets, we'll adjust them later */
-  obj->complete_cpuset = hwloc_bitmap_dup(cpuset);
-  obj->allowed_cpuset = hwloc_bitmap_dup(cpuset);
-
-  obj = hwloc__insert_object_by_cpuset(topology, obj, NULL /* do not show errors on stdout */);
-  if (!obj)
-    return NULL;
-
-  hwloc_connect_children(topology->levels[0][0]);
-
-  if ((child = obj->first_child) != NULL && child->cpuset) {
-    /* update sets from children (cpusets won't be modified) */
-    hwloc_obj_add_children_sets(obj);
-  } else {
-    /* copy the parent nodesets */
-    obj->nodeset = hwloc_bitmap_dup(obj->parent->nodeset);
-    obj->complete_nodeset = hwloc_bitmap_dup(obj->parent->complete_nodeset);
-    obj->allowed_nodeset = hwloc_bitmap_dup(obj->parent->allowed_nodeset);
   }
 
+  res = hwloc__insert_object_by_cpuset(topology, obj, NULL /* do not show errors on stdout */);
+  if (!res)
+    return NULL;
+  if (res != obj)
+    /* merged */
+    return res;
+
+  /* properly inserted */
+  hwloc_obj_add_children_sets(obj);
+  hwloc_connect_children(topology->levels[0][0]);
+  if (hwloc_connect_levels(topology) < 0)
+    return NULL;
   return obj;
 }
 
@@ -2927,21 +2925,30 @@ hwloc__check_children(struct hwloc_obj *parent)
       hwloc_bitmap_andnot(remaining_parent_nodeset, remaining_parent_nodeset, parent->children[j]->nodeset);
     }
 
-    if (parent->type == HWLOC_OBJ_PU)
-      /* if parent is a PU, its os_index bit may remain.
-       * or it could already have been removed by a Misc child inserted by cpuset. */
-      hwloc_bitmap_clr(remaining_parent_cpuset, parent->os_index);
-    /* nothing remains */
-    assert(hwloc_bitmap_iszero(remaining_parent_cpuset));
-    hwloc_bitmap_free(remaining_parent_cpuset);
+    if (parent->type == HWLOC_OBJ_PU) {
+      /* if parent is a PU (with Misc children for instance),
+       * its os_index bit may remain in cpuset. */
+      assert(hwloc_bitmap_weight(remaining_parent_cpuset) == 1);
+      assert(hwloc_bitmap_first(remaining_parent_cpuset) == (int)parent->os_index);
+    } else {
+      /* nothing remains */
+      assert(hwloc_bitmap_iszero(remaining_parent_cpuset));
+      hwloc_bitmap_free(remaining_parent_cpuset);
+    }
 
     if (parent->type == HWLOC_OBJ_NUMANODE)
       /* if parent is a NUMA node, its os_index bit may remain.
        * or it could already have been removed by a child. */
       hwloc_bitmap_clr(remaining_parent_nodeset, parent->os_index);
-    /* nothing remains */
-    assert(hwloc_bitmap_iszero(remaining_parent_nodeset));
-    hwloc_bitmap_free(remaining_parent_nodeset);
+    if (parent->type == HWLOC_OBJ_PU) {
+      /* if parent is a PU (with Misc children for instance),
+       * one bit may remain in nodeset. */
+      assert(hwloc_bitmap_weight(remaining_parent_nodeset) == 1);
+    } else {
+      /* nothing remains */
+      assert(hwloc_bitmap_iszero(remaining_parent_nodeset));
+      hwloc_bitmap_free(remaining_parent_nodeset);
+    }
 
   } else {
     /* check that children have no sets if the parent has none */

doc/Makefile.am
@@ -446,7 +446,8 @@ man3_tinker_DATA = \
         $(DOX_MAN_DIR)/man3/hwloc_restrict_flags_e.3 \
         $(DOX_MAN_DIR)/man3/hwloc_topology_restrict.3 \
         $(DOX_MAN_DIR)/man3/hwloc_topology_insert_misc_object_by_parent.3 \
-        $(DOX_MAN_DIR)/man3/hwloc_topology_insert_misc_object_by_cpuset.3 \
+        $(DOX_MAN_DIR)/man3/hwloc_topology_alloc_group_object.3 \
+        $(DOX_MAN_DIR)/man3/hwloc_topology_insert_group_object.3 \
         $(DOX_MAN_DIR)/man3/hwloc_obj_add_other_obj_sets.3
 
 man3_xmlexportdir = $(man3dir)

doc/hwloc.doxy
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2014 Inria.  All rights reserved.
+ * Copyright © 2009-2015 Inria.  All rights reserved.
  * Copyright © 2009-2013 Université Bordeaux
  * Copyright © 2009-2014 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -1799,8 +1799,9 @@ For reference, \ref hwloc_topology_t modification operations include
   after these functions return.  </dd>
 
 <dt>Runtime topology modifications</dt>
-  <dd><tt>hwloc_topology_insert_misc_object_by_*</tt> (see
-  \ref hwlocality_tinker) may modify the topology significantly by adding
+  <dd><tt>hwloc_topology_insert_misc_object_by_parent</tt>
+  and <tt>hwloc_topology_insert_group_object()</tt>
+  (see \ref hwlocality_tinker) may modify the topology significantly by adding
   objects inside the tree, changing the topology depth, etc.
   <tt>hwloc_topology_restrict</tt> modifies the topology even more
   dramatically by removing some objects.
@@ -2567,8 +2568,7 @@ These attributes are always exported to XML.
 However only character strings may be used as key names and values.
 
 It is also possible to insert Misc objects with custom names anywhere
-in the topology (<tt>hwloc_topology_insert_misc_object_by_cpuset()</tt>)
-or as a leaf of the topology (<tt>hwloc_topology_insert_misc_object_by_parent()</tt>).
+as a leaf of the topology (<tt>hwloc_topology_insert_misc_object_by_parent()</tt>).
 
 There is also a topology-specific userdata pointer that can be used
 to recognize different topologies by storing a custom pointer.

include/hwloc.h
@@ -2108,29 +2108,52 @@ HWLOC_DECLSPEC int hwloc_topology_restrict(hwloc_topology_t __hwloc_restrict top
  */
 HWLOC_DECLSPEC hwloc_obj_t hwloc_topology_insert_misc_object_by_parent(hwloc_topology_t topology, hwloc_obj_t parent, const char *name);
 
-/** \brief Add a MISC object to the topology
+/** \brief Allocate a Group object to insert later with hwloc_topology_insert_group_object().
  *
- * A new MISC object will be created and inserted into the topology at the
- * position given by bitmap \p cpuset. This offers a way to add new
- * intermediate levels to the topology hierarchy.
+ * This function returns a new Group object.
+ * The caller should (at least) initialize its sets before inserting the object.
+ * See hwloc_topology_insert_group_object().
  *
- * \p cpuset and \p name will be copied to setup the new object attributes.
+ * The caller may also set the object name before insertion.
  *
- * \return the newly-created object.
- * \return \c NULL if the insertion conflicts with the existing topology tree.
- *
- * \note If \p name contains some non-printable characters, they will
- * be dropped when exporting to XML, see hwloc_topology_export_xml() in hwloc/export.h.
+ * The object will be destroyed if passed to hwloc_topology_insert_group_object()
+ * without any set defined.
  */
-HWLOC_DECLSPEC hwloc_obj_t hwloc_topology_insert_misc_object_by_cpuset(hwloc_topology_t topology, hwloc_const_cpuset_t cpuset, const char *name);
+HWLOC_DECLSPEC hwloc_obj_t hwloc_topology_alloc_group_object(hwloc_topology_t topology);
+
+/** \brief Add more structure to the topology by adding an intermediate Group
+ *
+ * The caller should first allocate a new Group object with hwloc_topology_alloc_group_object().
+ * Then it must initialize some of its sets to specify the final location of the Group
+ * in the topology.
+ * Then the object can be passed to this function for actual insertion in the topology.
+ *
+ * Either the cpuset or nodeset field (or both, if compatible) may be used to do so.
+ * If inserting with respect to the complete topology (including disallowed, offline
+ * or unknown object), complete_cpuset and/or complete_nodeset may be used instead.
+ * It grouping several objects, hwloc_obj_add_other_obj_sets() is an easy way to
+ * build the Group sets iteratively.
+ *
+ * \return The inserted object if it was properly inserted.
+ *
+ * \return An existing object if the Group was discarded because the topology already
+ * contained a object at the same location (the Group did not add any locality information).
+ *
+ * \return \c NULL if the insertion failed because of conflicting sets in topology tree.
+ *
+ * \return \c NULL if the object was discarded because no set was initialized in the Group
+ * before insert, or all of them were empty.
+ */
+HWLOC_DECLSPEC hwloc_obj_t hwloc_topology_insert_group_object(hwloc_topology_t topology, hwloc_obj_t group);
 
 /** \brief Setup object cpusets/nodesets by OR'ing another object's sets.
  *
  * For each defined cpuset or nodeset in \p src, allocate the corresponding set
- * in \p dst and add \p src (by OR'ing it).
+ * in \p dst and add \p src to it by OR'ing sets.
  *
- * This function is convenient for building the sets of a Group of object before insertion
- * as a new parent of multiple objects.
+ * This function is convenient between hwloc_topology_alloc_group_object()
+ * and hwloc_topology_insert_group_object(). It builds the sets of the new Group
+ * that will be inserted as a new intermediate parent of several objects.
  */
 HWLOC_DECLSPEC int hwloc_obj_add_other_obj_sets(hwloc_obj_t dst, hwloc_obj_t src);
 

include/hwloc/rename.h
@@ -144,8 +144,10 @@ extern "C" {
 #define HWLOC_RESTRICT_FLAG_ADAPT_MISC HWLOC_NAME_CAPS(RESTRICT_FLAG_ADAPT_MISC)
 #define HWLOC_RESTRICT_FLAG_ADAPT_IO HWLOC_NAME_CAPS(RESTRICT_FLAG_ADAPT_IO)
 #define hwloc_topology_restrict HWLOC_NAME(topology_restrict)
+
 #define hwloc_topology_insert_misc_object_by_parent HWLOC_NAME(topology_insert_misc_object_by_parent)
-#define hwloc_topology_insert_misc_object_by_cpuset HWLOC_NAME(topology_insert_misc_object_by_cpuset)
+#define hwloc_topology_alloc_group_object HWLOC_NAME(topology_alloc_group_object)
+#define hwloc_topology_insert_group_object HWLOC_NAME(topology_insert_group_object)
 #define hwloc_obj_add_other_obj_sets HWLOC_NAME(obj_add_other_obj_sets)
 
 #define hwloc_topology_get_depth HWLOC_NAME(topology_get_depth)

tests/hwloc/hwloc_insert_misc.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2014 Inria.  All rights reserved.
+ * Copyright © 2009-2015 Inria.  All rights reserved.
  * Copyright © 2009-2010 Université Bordeaux
  * Copyright © 2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -16,7 +16,6 @@
 int main(void)
 {
   hwloc_topology_t topology, reload;
-  hwloc_bitmap_t cpuset;
   hwloc_obj_t obj;
   char *buf1, *buf2;
   int buflen1, buflen2, err;
@@ -26,24 +25,6 @@ int main(void)
   err = hwloc_topology_load(topology);
   assert(!err);
   hwloc_topology_check(topology);
-
-  /* insert by cpuset below root */
-  cpuset = hwloc_bitmap_alloc();
-  hwloc_bitmap_copy(cpuset, hwloc_topology_get_topology_cpuset(topology));
-  obj = hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, "test by cpuset under root");
-  assert(obj);
-  obj = hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, "test2 by cpuset under root");
-  assert(obj);
-  hwloc_bitmap_free(cpuset);
-
-  /* insert by cpuset below first available PU */
-  cpuset = hwloc_bitmap_alloc();
-  hwloc_bitmap_set(cpuset, hwloc_bitmap_first(hwloc_topology_get_topology_cpuset(topology)));
-  obj = hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, "test by cpuset under first available PU");
-  assert(obj);
-  obj = hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, "test2 by cpuset under first available PU");
-  assert(obj);
-  hwloc_bitmap_free(cpuset);
 
   /* insert by parent below root */
   obj = hwloc_get_root_obj(topology);

tests/hwloc/hwloc_topology_restrict.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011-2014 Inria.  All rights reserved.
+ * Copyright © 2011-2015 Inria.  All rights reserved.
  * Copyright © 2011 Université Bordeaux.  All rights reserved.
  * See COPYING in top-level directory.
  */
@@ -68,6 +68,7 @@ int main(void)
   hwloc_bitmap_t cpuset = hwloc_bitmap_alloc();
   unsigned node_indexes[3], core_indexes[6];
   float node_distances[9], core_distances[36];
+  hwloc_obj_t obj;
   unsigned i,j;
   int err;
 
@@ -167,14 +168,17 @@ int main(void)
 
   hwloc_topology_destroy(topology);
 
-  /* check that restricting exactly on a Misc object keeps things coherent */
+  /* check that restricting exactly on a Group object keeps things coherent */
   printf("restricting to a Misc covering only the of the PU level\n");
   hwloc_topology_init(&topology);
   hwloc_topology_set_synthetic(topology, "pu:4");
   hwloc_topology_load(topology);
   hwloc_bitmap_zero(cpuset);
   hwloc_bitmap_set_range(cpuset, 1, 2);
-  hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, "toto");
+  obj = hwloc_topology_alloc_group_object(topology);
+  obj->cpuset = hwloc_bitmap_dup(cpuset);
+  obj->name = strdup("toto");
+  hwloc_topology_insert_group_object(topology, obj);
   hwloc_topology_restrict(topology, cpuset, 0);
   hwloc_topology_check(topology);
   hwloc_topology_destroy(topology);

utils/lstopo/lstopo.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2014 Inria.  All rights reserved.
+ * Copyright © 2009-2015 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -77,27 +77,29 @@ FILE *open_output(const char *filename, int overwrite)
 
 static hwloc_obj_t insert_task(hwloc_topology_t topology, hwloc_cpuset_t cpuset, const char * name)
 {
-  hwloc_obj_t obj;
+  hwloc_obj_t group, obj;
 
   hwloc_bitmap_and(cpuset, cpuset, hwloc_topology_get_topology_cpuset(topology));
   if (hwloc_bitmap_iszero(cpuset))
     return NULL;
 
-  /* try to insert at exact position */
-  obj = hwloc_topology_insert_misc_object_by_cpuset(topology, cpuset, name);
-  if (!obj) {
+  /* try to insert a group at exact position */
+  group = hwloc_topology_alloc_group_object(topology);
+  if (!group)
+    return NULL;
+  group->cpuset = hwloc_bitmap_dup(cpuset);
+  group = hwloc_topology_insert_group_object(topology, group);
+  if (!group) {
     /* try to insert in a larger parent */
     char *s;
     hwloc_bitmap_asprintf(&s, cpuset);
-    obj = hwloc_get_obj_covering_cpuset(topology, cpuset);
-    if (obj) {
-      obj = hwloc_topology_insert_misc_object_by_parent(topology, obj, name);
-      fprintf(stderr, "Inserted process `%s' below parent larger than cpuset %s\n", name, s);
-    } else {
-      fprintf(stderr, "Failed to insert process `%s' with cpuset %s\n", name, s);
-    }
+    group = hwloc_get_obj_covering_cpuset(topology, cpuset);
+    fprintf(stderr, "Inserting process `%s' below parent larger than cpuset %s\n", name, s);
     free(s);
   }
+  obj = hwloc_topology_insert_misc_object_by_parent(topology, group, name);
+  if (!obj)
+    fprintf(stderr, "Failed to insert process `%s'\n", name);
 
   return obj;
 }

commit 6741f35bbf57ff7dbd4cd30981d78677b62726b4

core: fix a memory leak in topology_check()
introduced in d1bc5380

hwloc/topology.c
@@ -3019,8 +3019,8 @@ hwloc__check_children(struct hwloc_obj *parent)
     } else {
       /* nothing remains */
       assert(hwloc_bitmap_iszero(remaining_parent_cpuset));
-      hwloc_bitmap_free(remaining_parent_cpuset);
     }
+    hwloc_bitmap_free(remaining_parent_cpuset);
 
     if (parent->type == HWLOC_OBJ_NUMANODE)
       /* if parent is a NUMA node, its os_index bit may remain.
@@ -3033,8 +3033,8 @@ hwloc__check_children(struct hwloc_obj *parent)
     } else {
       /* nothing remains */
       assert(hwloc_bitmap_iszero(remaining_parent_nodeset));
-      hwloc_bitmap_free(remaining_parent_nodeset);
     }
+    hwloc_bitmap_free(remaining_parent_nodeset);
 
   } else {
     /* check that children have no sets if the parent has none */