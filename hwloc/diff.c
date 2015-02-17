46af9e0a9ae9aa52dac790367f444e1ff0497851
Brice Goglin <Brice.Goglin@inria.fr>
2015-02-17 19:15:40 +0100

    diff: more Misc support thanks to the new diff level

--- hwloc/diff.c
+++ hwloc/diff.c
@@ -76,7 +76,6 @@ static int hwloc_append_diff_obj_attr_string(hwloc_obj_t obj,
 					     hwloc_topology_diff_t *lastdiffp)
 {
 	hwloc_topology_diff_t newdiff;
-
 	newdiff = malloc(sizeof(*newdiff));
 	if (!newdiff)
 		return -1;
@@ -101,11 +100,6 @@ static int hwloc_append_diff_obj_attr_uint64(hwloc_obj_t obj,
 					     hwloc_topology_diff_t *lastdiffp)
 {
 	hwloc_topology_diff_t newdiff;
-
-	if (obj->type == HWLOC_OBJ_MISC)
-		/* TODO: add a custom level/depth for Misc */
-		return hwloc_append_diff_too_complex(obj, firstdiffp, lastdiffp);
-
 	newdiff = malloc(sizeof(*newdiff));
 	if (!newdiff)
 		return -1;
