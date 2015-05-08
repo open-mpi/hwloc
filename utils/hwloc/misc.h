open-mpi/hwloc

commit 13a0442916c540dd08e7c31f39d8547ba25b16cd

API: remove hwloc_topology_set_fsroot()
This is only useful for regression testing and remote debugging,
using the HWLOC_FSROOT env var is enough.

utils/hwloc/misc.h
@@ -173,17 +173,18 @@ hwloc_utils_enable_input_format(struct hwloc_topology *topology,
     }
     break;
 
-  case HWLOC_UTILS_INPUT_FSROOT:
+  case HWLOC_UTILS_INPUT_FSROOT: {
 #ifdef HWLOC_LINUX_SYS
-    if (hwloc_topology_set_fsroot(topology, input)) {
-      perror("Setting source filesystem root");
-      return EXIT_FAILURE;
-    }
+    char *env;
+    asprintf(&env, "HWLOC_FSROOT=%s", input);
+    putenv(env);
+    free(env);
 #else /* HWLOC_LINUX_SYS */
     fprintf(stderr, "This installation of hwloc does not support changing the file-system root, sorry.\n");
     exit(EXIT_FAILURE);
 #endif /* HWLOC_LINUX_SYS */
     break;
+  }
 
   case HWLOC_UTILS_INPUT_SYNTHETIC:
     if (hwloc_topology_set_synthetic(topology, input)) {

doc/Makefile.am
@@ -408,7 +408,6 @@ man3_membinding_DATA = \
 man3_setsourcedir = $(man3dir)
 man3_setsource_DATA = \
         $(DOX_MAN_DIR)/man3/hwloc_topology_set_pid.3 \
-        $(DOX_MAN_DIR)/man3/hwloc_topology_set_fsroot.3 \
         $(DOX_MAN_DIR)/man3/hwloc_topology_set_synthetic.3 \
         $(DOX_MAN_DIR)/man3/hwloc_topology_set_xml.3 \
         $(DOX_MAN_DIR)/man3/hwloc_topology_set_xmlbuffer.3

doc/hwloc.doxy
@@ -797,8 +797,7 @@ following environment variables.
 
 <dt>HWLOC_FSROOT=/path/to/linux/filesystem-root/</dt>
   <dd>switches to reading the topology from the specified
-  Linux filesystem root instead of the main file-system root, as if
-  hwloc_topology_set_fsroot() had been called.
+  Linux filesystem root instead of the main file-system root.
   Not using the main file-system root causes hwloc_topology_is_thissystem()
   to return 0.
   For convenience, this backend provides empty binding hooks which just

hwloc/topology.c
@@ -131,7 +131,7 @@ int hwloc_get_sysctl(int name[], unsigned namelen, int *ret)
 
 /* Return the OS-provided number of processors.  Unlike other methods such as
    reading sysfs on Linux, this method is not virtualizable; thus it's only
-   used as a fall-back method, allowing `hwloc_set_fsroot ()' to
+   used as a fall-back method, allowing virtual backends (FSROOT, etc) to
    have the desired effect.  */
 unsigned
 hwloc_fallback_nbprocessors(struct hwloc_topology *topology) {
@@ -2720,15 +2720,6 @@ hwloc_topology_set_pid(struct hwloc_topology *topology __hwloc_attribute_unused,
   errno = ENOSYS;
   return -1;
 #endif /* HWLOC_LINUX_SYS */
-}
-
-int
-hwloc_topology_set_fsroot(struct hwloc_topology *topology, const char *fsroot_path)
-{
-  return hwloc_disc_component_force_enable(topology,
-					   0 /* api */,
-					   HWLOC_DISC_COMPONENT_TYPE_CPU, "linux",
-					   fsroot_path, NULL, NULL);
 }
 
 int

include/hwloc.h
@@ -1663,7 +1663,7 @@ HWLOC_DECLSPEC int hwloc_free(hwloc_topology_t topology, void *addr, size_t len)
  * Setting HWLOC_XMLFILE in the environment enforces the discovery from a XML
  * file as if hwloc_topology_set_xml() had been called.
  * HWLOC_FSROOT switches to reading the topology from the specified Linux
- * filesystem root as if hwloc_topology_set_fsroot() had been called.
+ * filesystem root.
  * Finally, HWLOC_THISSYSTEM enforces the return value of
  * hwloc_topology_is_thissystem().
  *
@@ -1685,34 +1685,6 @@ HWLOC_DECLSPEC int hwloc_free(hwloc_topology_t topology, void *addr, size_t len)
  * support this feature.
  */
 HWLOC_DECLSPEC int hwloc_topology_set_pid(hwloc_topology_t __hwloc_restrict topology, hwloc_pid_t pid);
-
-/** \brief Change the file-system root path when building the topology from sysfs/procfs.
- *
- * On Linux system, use sysfs and procfs files as if they were mounted on the given
- * \p fsroot_path instead of the main file-system root. Setting the environment
- * variable HWLOC_FSROOT may also result in this behavior.
- * Not using the main file-system root causes hwloc_topology_is_thissystem()
- * to return 0.
- *
- * Note that this function does not actually load topology
- * information; it just tells hwloc where to load it from.  You'll
- * still need to invoke hwloc_topology_load() to actually load the
- * topology information.
- *
- * \return -1 with errno set to ENOSYS on non-Linux and on Linux systems that
- * do not support it.
- * \return -1 with the appropriate errno if \p fsroot_path cannot be used.
- *
- * \note For convenience, this backend provides empty binding hooks which just
- * return success.  To have hwloc still actually call OS-specific hooks, the
- * HWLOC_TOPOLOGY_FLAG_IS_THISSYSTEM has to be set to assert that the loaded
- * file is really the underlying system.
- *
- * \note On success, the Linux component replaces the previously enabled
- * component (if any), but the topology is not actually modified until
- * hwloc_topology_load().
- */
-HWLOC_DECLSPEC int hwloc_topology_set_fsroot(hwloc_topology_t __hwloc_restrict topology, const char * __hwloc_restrict fsroot_path);
 
 /** \brief Enable synthetic topology.
  *

include/hwloc/rename.h
@@ -118,7 +118,6 @@ extern "C" {
 #define HWLOC_TOPOLOGY_FLAG_WHOLE_IO HWLOC_NAME_CAPS(TOPOLOGY_FLAG_WHOLE_IO)
 #define HWLOC_TOPOLOGY_FLAG_ICACHES HWLOC_NAME_CAPS(TOPOLOGY_FLAG_ICACHES)
 
-#define hwloc_topology_set_fsroot HWLOC_NAME(topology_set_fsroot)
 #define hwloc_topology_set_pid HWLOC_NAME(topology_set_pid)
 #define hwloc_topology_set_synthetic HWLOC_NAME(topology_set_synthetic)
 #define hwloc_topology_set_xml HWLOC_NAME(topology_set_xml)

tests/hwloc/hwloc_backends.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2012-2014 Inria.  All rights reserved.
+ * Copyright © 2012-2015 Inria.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -28,7 +28,6 @@ int main(void)
   int xmlbuflen;
   char xmlfile[] = "hwloc_backends.tmpxml.XXXXXX";
   int xmlbufok = 0, xmlfileok = 0, xmlfilefd;
-  int err;
 
   printf("trying to export topology to XML buffer and file for later...\n");
   hwloc_topology_init(&topology1);
@@ -57,10 +56,6 @@ int main(void)
   }
   printf("switching to synthetic...\n");
   hwloc_topology_set_synthetic(topology2, "machine:2 node:3 cache:2 pu:4");
-  printf("switching sysfs fsroot to // ...\n");
-  hwloc_topology_set_fsroot(topology2, "//"); /* valid path that won't be recognized as '/' */
-  printf("switching sysfs fsroot to / ...\n");
-  hwloc_topology_set_fsroot(topology2, "/");
   hwloc_topology_destroy(topology2);
 
   if (xmlfileok) {
@@ -89,22 +84,6 @@ int main(void)
   hwloc_topology_load(topology2);
   hwloc_topology_check(topology2);
   assert(!hwloc_topology_is_thissystem(topology2));
-  hwloc_topology_destroy(topology2);
-
-  printf("switching sysfs fsroot to // and loading...\n");
-  hwloc_topology_init(&topology2);
-  err = hwloc_topology_set_fsroot(topology2, "//"); /* '//' isn't recognized as the normal fsroot on Linux, and it fails and falls back to normal topology on !Linux */
-  hwloc_topology_load(topology2);
-  hwloc_topology_check(topology2);
-  assert(!hwloc_topology_is_thissystem(topology2) == !err);
-  hwloc_topology_destroy(topology2);
-
-  printf("switching sysfs fsroot to / and loading...\n");
-  hwloc_topology_init(&topology2);
-  err = hwloc_topology_set_fsroot(topology2, "/");
-  hwloc_topology_load(topology2);
-  hwloc_topology_check(topology2);
-  assert(hwloc_topology_is_thissystem(topology2)); /* '/' is recognized as the normal fsroot on Linux, and it fails and falls back to normal topology on !Linux */
   hwloc_topology_destroy(topology2);
 
   printf("switching to synthetic...\n");

commit 3b59fe57dd5f4d50f6af8755359097ac893d13e0

lstopo: Fix lstopo -i <fsroot>
Bug introduced in commit 13a0442916c540dd08e7c31f39d8547ba25b16cd
when replacing set_fsroot() with HWLOC_FSROOT=

utils/hwloc/misc.h
@@ -178,7 +178,6 @@ hwloc_utils_enable_input_format(struct hwloc_topology *topology,
     char *env;
     asprintf(&env, "HWLOC_FSROOT=%s", input);
     putenv(env);
-    free(env);
 #else /* HWLOC_LINUX_SYS */
     fprintf(stderr, "This installation of hwloc does not support changing the file-system root, sorry.\n");
     exit(EXIT_FAILURE);