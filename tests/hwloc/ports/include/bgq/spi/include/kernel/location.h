456708b1b47d6cbb9e69c6e761532f378f187da6
Brice Goglin <Brice.Goglin@inria.fr>
2015-07-26 12:42:19 +0200

    tests/ports: disable BGQ port when host is a old Linux platform
    Basic cpuset/affinity is missing at least on Red Hat 8, causing make check to break.
    
    Thanks to Paul Hargrove for the report.

--- tests/hwloc/ports/include/bgq/spi/include/kernel/location.h
+++ tests/hwloc/ports/include/bgq/spi/include/kernel/location.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Inria.  All rights reserved.
+ * Copyright © 2013-2015 Inria.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -10,5 +10,11 @@
 
 uint32_t Kernel_ProcessorID( void );
 uint32_t Kernel_MyTcoord( void );
+
+/* don't try to cross-build BGQ port on old Linux platforms */
+#if (!HAVE_DECL_PTHREAD_GETAFFINITY_NP) || (!HAVE_DECL_PTHREAD_SETAFFINITY_NP) || (!defined HWLOC_HAVE_CPU_SET)
+#warning Disabling BGQ port cross-build on old Linux platform
+#define HWLOC_DISABLE_BGQ_PORT_TEST
+#endif
 
 #endif /* HWLOC_PORT_BGQ_KERNEL_LOCATION_H */
