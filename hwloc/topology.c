open-mpi/hwloc

commit 75be794d8f526fd86b7272307042c3c9981126c3

core: add a hwloc_free_object_siblings_and_children()

hwloc/topology.c
@@ -430,6 +430,15 @@ hwloc_free_object_and_children(hwloc_obj_t obj)
   unlink_and_free_object_and_children(&obj);
 }
 
+/* Free an object, its next siblings and their children without unlinking from parent.
+ */
+void
+hwloc_free_object_siblings_and_children(hwloc_obj_t obj)
+{
+  while (obj)
+    unlink_and_free_object_and_children(&obj);
+}
+
 /* insert the (non-empty) list of sibling starting at firstnew as new children of newparent,
  * and return the address of the pointer to the next one
  */
@@ -2944,11 +2953,9 @@ restrict_object(hwloc_topology_t topology, unsigned long flags, hwloc_obj_t *pob
     if (obj->type == HWLOC_OBJ_NUMANODE)
       hwloc_bitmap_set(droppednodeset, obj->os_index);
     if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_IO))
-      while (obj->io_first_child)
-	unlink_and_free_object_and_children(&obj->io_first_child);
+      hwloc_free_object_siblings_and_children(obj->io_first_child);
     if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_MISC))
-      while (obj->misc_first_child)
-	unlink_and_free_object_and_children(&obj->misc_first_child);
+      hwloc_free_object_siblings_and_children(obj->misc_first_child);
     unlink_and_free_single_object(pobj);
     topology->modified = 1;
     /* do not remove children. if they were to be removed, they would have been already */

include/hwloc/rename.h
@@ -607,6 +607,7 @@ extern "C" {
 #define hwloc_add_uname_info HWLOC_NAME(add_uname_info)
 #define hwloc_free_unlinked_object HWLOC_NAME(free_unlinked_object)
 #define hwloc_free_object_and_children HWLOC_NAME(free_object_and_children)
+#define hwloc_free_object_siblings_and_children HWLOC_NAME(free_object_siblings_and_children)
 #define hwloc__duplicate_objects HWLOC_NAME(_duplicate_objects)
 
 #define hwloc_alloc_heap HWLOC_NAME(alloc_heap)

include/private/private.h
@@ -209,6 +209,9 @@ extern void hwloc_free_unlinked_object(hwloc_obj_t obj);
 /* Free obj and its children, assuming it's not linked to a parent */
 extern void hwloc_free_object_and_children(hwloc_obj_t obj);
 
+/* Free obj, its next siblings, and their children, assuming they're not linked to a parent */
+extern void hwloc_free_object_siblings_and_children(hwloc_obj_t obj);
+
 /* Duplicate src and its children under newparent in newtopology */
 extern void hwloc__duplicate_objects(struct hwloc_topology *newtopology, struct hwloc_obj *newparent, struct hwloc_obj *src);
 

commit 816b336f90c929a39ff943aaf9ee8d966f91fdc2

core: clear the list when removing misc/io children on restrict
(broken in 75be794d)

Thanks to Nicolas Denoyelle for report the segfault.

hwloc/topology.c
@@ -2946,10 +2946,14 @@ restrict_object(hwloc_topology_t topology, unsigned long flags, hwloc_obj_t *pob
     hwloc_debug_print_object(0, obj);
     if (obj->type == HWLOC_OBJ_NUMANODE)
       hwloc_bitmap_set(droppednodeset, obj->os_index);
-    if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_IO))
+    if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_IO)) {
       hwloc_free_object_siblings_and_children(obj->io_first_child);
-    if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_MISC))
+      obj->io_first_child = NULL;
+    }
+    if (!(flags & HWLOC_RESTRICT_FLAG_ADAPT_MISC)) {
       hwloc_free_object_siblings_and_children(obj->misc_first_child);
+      obj->misc_first_child = NULL;
+    }
     unlink_and_free_single_object(pobj);
     topology->modified = 1;
     /* do not remove children. if they were to be removed, they would have been already */