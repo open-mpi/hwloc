9e9c125e8cf66aa9ecff24465a8e5d5311b4abcd
Brice Goglin <Brice.Goglin@inria.fr>
2015-08-07 20:57:41 +0200

    cuda: use direct lookup instead of the PCI-notify_new_obj callback

--- hwloc/topology-cuda.c
+++ hwloc/topology-cuda.c
@@ -15,55 +15,6 @@
 
 #include <cuda_runtime_api.h>
 
-struct hwloc_cuda_backend_data_s {
-  unsigned nr_devices; /* -1 when unknown yet, first callback will setup */
-  struct hwloc_cuda_device_info_s {
-    int idx;
-    unsigned pcidomain, pcibus, pcidev, pcifunc;
-  } * devices;
-};
-
-/* query all PCI bus ids for later */
-static void
-hwloc_cuda_query_devices(struct hwloc_cuda_backend_data_s *data)
-{
-  cudaError_t cures;
-  int nb, i;
-
-  /* mark the number of devices as 0 in case we fail below,
-   * so that we don't try again later.
-   */
-  data->nr_devices = 0;
-
-  cures = cudaGetDeviceCount(&nb);
-  if (cures)
-    return;
-
-  /* allocate structs */
-  data->devices = malloc(nb * sizeof(*data->devices));
-  if (!data->devices)
-    return;
-
-  for (i = 0; i < nb; i++) {
-    struct hwloc_cuda_device_info_s *info = &data->devices[data->nr_devices];
-    int domain, bus, dev;
-
-    if (hwloc_cudart_get_device_pci_ids(NULL /* topology unused */, i, &domain, &bus, &dev))
-      continue;
-
-    info->idx = i;
-    info->pcidomain = (unsigned) domain;
-    info->pcibus = (unsigned) bus;
-    info->pcidev = (unsigned) dev;
-    info->pcifunc = 0;
-
-    /* validate this device */
-    data->nr_devices++;
-  }
-
-  return;
-}
-
 static unsigned hwloc_cuda_cores_per_MP(int major, int minor)
 {
   /* based on CUDA C Programming Guide, Annex G */
@@ -92,12 +43,11 @@ static unsigned hwloc_cuda_cores_per_MP(int major, int minor)
 }
 
 static int
-hwloc_cuda_backend_notify_new_object(struct hwloc_backend *backend,
-				     struct hwloc_obj *pcidev)
+hwloc_cuda_discover(struct hwloc_backend *backend)
 {
   struct hwloc_topology *topology = backend->topology;
-  struct hwloc_cuda_backend_data_s *data = backend->private_data;
-  unsigned i;
+  cudaError_t cures;
+  int nb, i;
 
   if (!(hwloc_topology_get_flags(topology) & (HWLOC_TOPOLOGY_FLAG_IO_DEVICES|HWLOC_TOPOLOGY_FLAG_WHOLE_IO)))
     return 0;
@@ -107,39 +57,20 @@ hwloc_cuda_backend_notify_new_object(struct hwloc_backend *backend,
     return 0;
   }
 
-  if (HWLOC_OBJ_PCI_DEVICE != pcidev->type)
+  cures = cudaGetDeviceCount(&nb);
+  if (cures)
     return 0;
 
-  if (data->nr_devices == (unsigned) -1) {
-    /* first call, lookup all devices */
-    hwloc_cuda_query_devices(data);
-    /* if it fails, data->nr_devices = 0 so we won't do anything below and in next callbacks */
-  }
-
-  if (!data->nr_devices)
-    /* found no devices */
-    return 0;
-
-  for(i=0; i<data->nr_devices; i++) {
-    struct hwloc_cuda_device_info_s *info = &data->devices[i];
+  for (i = 0; i < nb; i++) {
+    int domain, bus, dev;
     char cuda_name[32];
     char number[32];
     struct cudaDeviceProp prop;
-    hwloc_obj_t cuda_device;
-    cudaError_t cures;
+    hwloc_obj_t cuda_device, parent;
     unsigned cores;
 
-    if (info->pcidomain != pcidev->attr->pcidev.domain)
-      continue;
-    if (info->pcibus != pcidev->attr->pcidev.bus)
-      continue;
-    if (info->pcidev != pcidev->attr->pcidev.dev)
-      continue;
-    if (info->pcifunc != pcidev->attr->pcidev.func)
-      continue;
-
     cuda_device = hwloc_alloc_setup_object(HWLOC_OBJ_OS_DEVICE, -1);
-    snprintf(cuda_name, sizeof(cuda_name), "cuda%d", info->idx);
+    snprintf(cuda_name, sizeof(cuda_name), "cuda%d", i);
     cuda_device->name = strdup(cuda_name);
     cuda_device->depth = (unsigned) HWLOC_TYPE_DEPTH_UNKNOWN;
     cuda_device->attr->osdev.type = HWLOC_OBJ_OSDEV_COPROC;
@@ -148,7 +79,7 @@ hwloc_cuda_backend_notify_new_object(struct hwloc_backend *backend,
     hwloc_obj_add_info(cuda_device, "Backend", "CUDA");
     hwloc_obj_add_info(cuda_device, "GPUVendor", "NVIDIA Corporation");
 
-    cures = cudaGetDeviceProperties(&prop, info->idx);
+    cures = cudaGetDeviceProperties(&prop, i);
     if (!cures)
       hwloc_obj_add_info(cuda_device, "GPUModel", prop.name);
 
@@ -170,19 +101,16 @@ hwloc_cuda_backend_notify_new_object(struct hwloc_backend *backend,
     snprintf(number, sizeof(number), "%llu", ((unsigned long long) prop.sharedMemPerBlock) >> 10);
     hwloc_obj_add_info(cuda_device, "CUDASharedMemorySizePerMP", number);
 
-    hwloc_insert_object_by_parent(topology, pcidev, cuda_device);
-    return 1;
+    parent = NULL;
+    if (hwloc_cudart_get_device_pci_ids(NULL /* topology unused */, i, &domain, &bus, &dev) == 0)
+      parent = hwloc_pci_belowroot_find_by_busid(topology, domain, bus, dev, 0);
+    if (!parent)
+      parent = hwloc_get_root_obj(topology);
+
+    hwloc_insert_object_by_parent(topology, parent, cuda_device);
   }
 
-  return 0;
-}
-
-static void
-hwloc_cuda_backend_disable(struct hwloc_backend *backend)
-{
-  struct hwloc_cuda_backend_data_s *data = backend->private_data;
-  free(data->devices);
-  free(data);
+  return nb;
 }
 
 static struct hwloc_backend *
@@ -192,27 +120,14 @@ hwloc_cuda_component_instantiate(struct hwloc_disc_component *component,
                                  const void *_data3 __hwloc_attribute_unused)
 {
   struct hwloc_backend *backend;
-  struct hwloc_cuda_backend_data_s *data;
 
   /* thissystem may not be fully initialized yet, we'll check flags in discover() */
 
   backend = hwloc_backend_alloc(component);
   if (!backend)
     return NULL;
-
-  data = malloc(sizeof(*data));
-  if (!data) {
-    free(backend);
-    return NULL;
-  }
   /* the first callback will initialize those */
-  data->nr_devices = (unsigned) -1; /* unknown yet */
-  data->devices = NULL;
-
-  backend->private_data = data;
-  backend->disable = hwloc_cuda_backend_disable;
-
-  backend->notify_new_object = hwloc_cuda_backend_notify_new_object;
+  backend->discover = hwloc_cuda_discover;
   return backend;
 }
 
