ac961515ef0eeb784d77833383a9748e0c5db4fe
Brice Goglin <Brice.Goglin@inria.fr>
2015-09-12 09:05:49 +0200

    windows: support non-contigous PU numbers in the no-group case
    (cherry picked from commit 0a1f2298ed43f89f2a256049d7356ffdc1063297)

--- tests/ports/include/windows/windows.h
+++ tests/ports/include/windows/windows.h
@@ -66,6 +66,7 @@ BOOL GetNumaAvailableMemoryNode(UCHAR Node, PULONGLONG AvailableBytes);
 
 typedef struct _SYSTEM_INFO {
   DWORD dwPageSize;
+  DWORD_PTR dwActiveProcessorMask;
   DWORD dwNumberOfProcessors;
 } SYSTEM_INFO, *LPSYSTEM_INFO;
 
