48f9ddd73d1f5c5582ead52548badd51e552867f
Brice Goglin <Brice.Goglin@inria.fr>
2015-10-14 16:52:24 +0200

    linux: fix a bitmap leak on AMD processors with dual-core compute units
    Don't leak coreset when we didn't actually use it for core->cpuset.
    
    Thanks to Bob Benner for reporting the issue and testing the patch.
    
    Fixes #137
    
    (cherry picked from commit daaf59f259df92417a2cf6e0f0743fd1d8c54ed0)

--- src/topology-linux.c
+++ src/topology-linux.c
@@ -2993,11 +2993,11 @@ socket_done:
 	  hwloc_bitmap_set(core->cpuset, i);
 	} else {
 	  core->cpuset = coreset;
+	  coreset = NULL; /* don't free it */
 	}
         hwloc_debug_1arg_bitmap("os core %u has cpuset %s\n",
                      mycoreid, coreset);
         hwloc_insert_object_by_cpuset(topology, core);
-        coreset = NULL; /* don't free it */
       }
 
       /* look at the books */
