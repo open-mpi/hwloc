open-mpi/hwloc

commit daeaa3fe93489d197df3b96d6e6106756a5a2de1

x86: split AMD Opteron 6100 Magny-Cours dual-node processor L3
AMD K10 model 0x9 have 8 or 12 cores per processors but the L3 is actually
shared by half of them contrary to what CPUID says. Fix that.

Thanks to Guillaume Beauchamp for the patch.

hwloc/topology-x86.c
@@ -236,6 +236,20 @@ static void fill_amd_cache(struct procinfo *infos, unsigned level, int type, uns
   cache->size = size;
   cache->sets = 0;
 
+  if (infos->cpufamilynumber== 0x10 && infos->cpumodelnumber == 0x9
+      && level == 3
+      && (cache->ways == -1 || (cache->ways % 2 == 0)) && cache->nbthreads_sharing >= 8) {
+    /* Fix AMD family 0x10 model 0x9 (Magny-Cours) with 8 or 12 cores.
+     * The L3 (and its associativity) is actually split into two halves).
+     */
+    if (cache->nbthreads_sharing == 16)
+      cache->nbthreads_sharing = 12; /* nbthreads_sharing is a power of 2 but the processor actually has 8 or 12 cores */
+    cache->nbthreads_sharing /= 2;
+    cache->size /= 2;
+    if (cache->ways != -1)
+      cache->ways /= 2;
+  }
+
   hwloc_debug("cache L%u t%u linesize %u ways %u size %luKB\n", cache->level, cache->nbthreads_sharing, cache->linesize, cache->ways, cache->size >> 10);
 }
 
@@ -434,9 +448,6 @@ static void look_proc(struct hwloc_backend *backend, struct procinfo *infos, uns
 	fill_amd_cache(infos, 2, 3, ecx); /* L2u */
       if (edx & 0xf000)
 	fill_amd_cache(infos, 3, 3, edx); /* L3u */
-      /* FIXME: AMD MagnyCours family 0x10 model 0x9 with 8 cores or more actually
-       * have the L3 split in two halves, and associativity is divided as well (48)
-       */
     }
   }
 
@@ -867,7 +878,15 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
 
 	  /* Found a matching cache, now look for others sharing it */
 	  {
-	    unsigned cacheid = infos[i].apicid / infos[i].cache[l].nbthreads_sharing;
+	    /* AMD Magny-Cours 12-cores processor reserve APIC ids as AAAAAABBBBBB....
+	     * among first L3 (A), second L3 (B), and unexisting cores (.).
+	     * On multi-socket servers, L3 in non-first sockets may have APIC id ranges
+	     * such as [16-21] that are not aligned on multiple of nbthreads_sharing (6).
+	     * That means, we can't just compare apicid/nbthreads_sharing to identify siblings.
+	     * Hence we use apicid%max_log_proc instead of apicid to restore the alignment.
+	     * Works because we also compare packageid to identify siblings.
+	     */
+	    unsigned cacheid = (infos[i].apicid % infos[i].max_log_proc) / infos[i].cache[l].nbthreads_sharing;
 	    hwloc_obj_type_t otype;
 
 	    cache_cpuset = hwloc_bitmap_alloc();
@@ -882,7 +901,7 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
 		hwloc_bitmap_clr(caches_cpuset, j);
 		continue;
 	      }
-	      if (infos[j].packageid == packageid && infos[j].apicid / infos[j].cache[l2].nbthreads_sharing == cacheid) {
+	      if (infos[j].packageid == packageid && (infos[j].apicid % infos[j].max_log_proc) / infos[j].cache[l2].nbthreads_sharing == cacheid) {
 		hwloc_bitmap_set(cache_cpuset, j);
 		hwloc_bitmap_clr(caches_cpuset, j);
 	      }

tests/hwloc/x86/AMD-K10-MagnyCours-2xOpteron-6164HE.output
@@ -10,7 +10,7 @@
         <info name="CPUModelNumber" value="9"/>
         <info name="CPUModel" value="AMD Opteron(tm) Processor 6164 HE"/>
         <info name="CPUStepping" value="1"/>
-        <object type="L3Cache" cpuset="0x00000fff" complete_cpuset="0x00000fff" allowed_cpuset="0x00000fff" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="10485760" depth="3" cache_linesize="64" cache_associativity="96" cache_type="0">
+        <object type="L3Cache" cpuset="0x0000003f" complete_cpuset="0x0000003f" allowed_cpuset="0x0000003f" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="5242880" depth="3" cache_linesize="64" cache_associativity="48" cache_type="0">
           <object type="L2Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="524288" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
             <object type="L1Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
               <object type="L1iCache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
@@ -65,6 +65,8 @@
               </object>
             </object>
           </object>
+        </object>
+        <object type="L3Cache" cpuset="0x00000fc0" complete_cpuset="0x00000fc0" allowed_cpuset="0x00000fc0" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="5242880" depth="3" cache_linesize="64" cache_associativity="48" cache_type="0">
           <object type="L2Cache" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="524288" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
             <object type="L1Cache" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
               <object type="L1iCache" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
@@ -127,7 +129,7 @@
         <info name="CPUModelNumber" value="9"/>
         <info name="CPUModel" value="AMD Opteron(tm) Processor 6164 HE"/>
         <info name="CPUStepping" value="1"/>
-        <object type="L3Cache" cpuset="0x00fff000" complete_cpuset="0x00fff000" allowed_cpuset="0x00fff000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="10485760" depth="3" cache_linesize="64" cache_associativity="96" cache_type="0">
+        <object type="L3Cache" cpuset="0x0003f000" complete_cpuset="0x0003f000" allowed_cpuset="0x0003f000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="5242880" depth="3" cache_linesize="64" cache_associativity="48" cache_type="0">
           <object type="L2Cache" cpuset="0x00001000" complete_cpuset="0x00001000" allowed_cpuset="0x00001000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="524288" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
             <object type="L1Cache" cpuset="0x00001000" complete_cpuset="0x00001000" allowed_cpuset="0x00001000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
               <object type="L1iCache" cpuset="0x00001000" complete_cpuset="0x00001000" allowed_cpuset="0x00001000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
@@ -182,6 +184,8 @@
               </object>
             </object>
           </object>
+        </object>
+        <object type="L3Cache" cpuset="0x00fc0000" complete_cpuset="0x00fc0000" allowed_cpuset="0x00fc0000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="5242880" depth="3" cache_linesize="64" cache_associativity="48" cache_type="0">
           <object type="L2Cache" cpuset="0x00040000" complete_cpuset="0x00040000" allowed_cpuset="0x00040000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="524288" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
             <object type="L1Cache" cpuset="0x00040000" complete_cpuset="0x00040000" allowed_cpuset="0x00040000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
               <object type="L1iCache" cpuset="0x00040000" complete_cpuset="0x00040000" allowed_cpuset="0x00040000" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">

commit facaadef488b004e893d4eb520a1b87549bf3449

x86: Don't let the AMD Magny-Cours L3 quirk break detection on other processors
The quirk introduced in daeaa3fe93489d197df3b96d6e6106756a5a2de1 requires
a valid max_log_proc, and what we currently get from cpuid 0x1 ebx won't
be valid in the future.
Compute cacheids earlier and apply the AMD quirk only there instead of
requiring a valid max_log_proc in all cases later.

By the way check that the processor is AMD before enabling that quirk.

hwloc/topology-x86.c
@@ -159,6 +159,7 @@ struct cacheinfo {
   hwloc_obj_cache_type_t type;
   unsigned level;
   unsigned nbthreads_sharing;
+  unsigned cacheid;
 
   unsigned linesize;
   unsigned linepart;
@@ -235,20 +236,6 @@ static void fill_amd_cache(struct procinfo *infos, unsigned level, hwloc_obj_cac
   }
   cache->size = size;
   cache->sets = 0;
-
-  if (infos->cpufamilynumber== 0x10 && infos->cpumodelnumber == 0x9
-      && level == 3
-      && (cache->ways == -1 || (cache->ways % 2 == 0)) && cache->nbthreads_sharing >= 8) {
-    /* Fix AMD family 0x10 model 0x9 (Magny-Cours) with 8 or 12 cores.
-     * The L3 (and its associativity) is actually split into two halves).
-     */
-    if (cache->nbthreads_sharing == 16)
-      cache->nbthreads_sharing = 12; /* nbthreads_sharing is a power of 2 but the processor actually has 8 or 12 cores */
-    cache->nbthreads_sharing /= 2;
-    cache->size /= 2;
-    if (cache->ways != -1)
-      cache->ways /= 2;
-  }
 
   hwloc_debug("cache L%u t%u linesize %u ways %u size %luKB\n", cache->level, cache->nbthreads_sharing, cache->linesize, cache->ways, cache->size >> 10);
 }
@@ -563,6 +550,38 @@ static void look_proc(struct hwloc_backend *backend, struct procinfo *infos, uns
       infos->packageid = apic_id >> apic_shift;
       hwloc_debug("x2APIC remainder: %d\n", infos->packageid);
       hwloc_debug("this is thread %u of core %u\n", infos->threadid, infos->coreid);
+    }
+  }
+
+  /* Now that we have all info, compute cacheids and apply quirks */
+  for (cachenum = 0; cachenum < infos->numcaches; cachenum++) {
+    struct cacheinfo *cache = &infos->cache[cachenum];
+
+    /* default cacheid value */
+    cache->cacheid = infos->apicid / cache->nbthreads_sharing;
+
+    /* AMD quirk */
+    if (cpuid_type == amd
+	&& infos->cpufamilynumber== 0x10 && infos->cpumodelnumber == 0x9
+	&& cache->level == 3
+	&& (cache->ways == -1 || (cache->ways % 2 == 0)) && cache->nbthreads_sharing >= 8) {
+      /* Fix AMD family 0x10 model 0x9 (Magny-Cours) with 8 or 12 cores.
+       * The L3 (and its associativity) is actually split into two halves).
+       */
+      if (cache->nbthreads_sharing == 16)
+	cache->nbthreads_sharing = 12; /* nbthreads_sharing is a power of 2 but the processor actually has 8 or 12 cores */
+      cache->nbthreads_sharing /= 2;
+      cache->size /= 2;
+      if (cache->ways != -1)
+	cache->ways /= 2;
+      /* AMD Magny-Cours 12-cores processor reserve APIC ids as AAAAAABBBBBB....
+       * among first L3 (A), second L3 (B), and unexisting cores (.).
+       * On multi-socket servers, L3 in non-first sockets may have APIC id ranges
+       * such as [16-21] that are not aligned on multiple of nbthreads_sharing (6).
+       * That means, we can't just compare apicid/nbthreads_sharing to identify siblings.
+       */
+      cache->cacheid = (infos->apicid % infos->max_log_proc) / cache->nbthreads_sharing /* cacheid within the package */
+	+ 2 * (infos->apicid / infos->max_log_proc); /* add 2 caches per previous package */
     }
   }
 
@@ -896,15 +915,7 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
 
 	  /* Found a matching cache, now look for others sharing it */
 	  {
-	    /* AMD Magny-Cours 12-cores processor reserve APIC ids as AAAAAABBBBBB....
-	     * among first L3 (A), second L3 (B), and unexisting cores (.).
-	     * On multi-socket servers, L3 in non-first sockets may have APIC id ranges
-	     * such as [16-21] that are not aligned on multiple of nbthreads_sharing (6).
-	     * That means, we can't just compare apicid/nbthreads_sharing to identify siblings.
-	     * Hence we use apicid%max_log_proc instead of apicid to restore the alignment.
-	     * Works because we also compare packageid to identify siblings.
-	     */
-	    unsigned cacheid = (infos[i].apicid % infos[i].max_log_proc) / infos[i].cache[l].nbthreads_sharing;
+	    unsigned cacheid = infos[i].cache[l].cacheid;
 
 	    cache_cpuset = hwloc_bitmap_alloc();
 	    for (j = i; j < nbprocs; j++) {
@@ -918,7 +929,7 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
 		hwloc_bitmap_clr(caches_cpuset, j);
 		continue;
 	      }
-	      if (infos[j].packageid == packageid && (infos[j].apicid % infos[j].max_log_proc) / infos[j].cache[l2].nbthreads_sharing == cacheid) {
+	      if (infos[j].packageid == packageid && infos[j].cache[l2].cacheid == cacheid) {
 		hwloc_bitmap_set(cache_cpuset, j);
 		hwloc_bitmap_clr(caches_cpuset, j);
 	      }