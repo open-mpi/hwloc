open-mpi/hwloc

commit a10e09fa9b58102c05c13c06abdf782ad6fdd8e0

API: remove the unused topology parameter from diff destroy/export/load functions

utils/hwloc/hwloc-patch.c
@@ -26,7 +26,7 @@ static int hwloc_diff_read(const char *inputdiff,
 	int err;
 
 	if (strcmp(inputdiff, "-"))
-		return hwloc_topology_diff_load_xml(NULL, inputdiff, firstdiffp, refnamep);
+		return hwloc_topology_diff_load_xml(inputdiff, firstdiffp, refnamep);
 
 	buflen = 4096;
 	buffer = malloc(buflen+1); /* one more byte for the ending \0 */
@@ -50,7 +50,7 @@ static int hwloc_diff_read(const char *inputdiff,
 		readlen = buflen/2;
 	}
 
-	err = hwloc_topology_diff_load_xmlbuffer(NULL, buffer, (int)(offset+1), firstdiffp, refnamep);
+	err = hwloc_topology_diff_load_xmlbuffer(buffer, (int)(offset+1), firstdiffp, refnamep);
 	free(buffer);
 	return err;
 
@@ -154,13 +154,13 @@ int main(int argc, char *argv[])
 		goto out_with_diff;
 	}
 
-	hwloc_topology_diff_destroy(NULL, firstdiff);
+	hwloc_topology_diff_destroy(firstdiff);
 	hwloc_topology_destroy(topo);
 
 	exit(EXIT_SUCCESS);
 
 out_with_diff:
-	hwloc_topology_diff_destroy(NULL, firstdiff);
+	hwloc_topology_diff_destroy(firstdiff);
 out_with_topo:
 	hwloc_topology_destroy(topo);
 out:

hwloc/diff.c
@@ -7,8 +7,7 @@
 #include <private/private.h>
 #include <private/misc.h>
 
-int hwloc_topology_diff_destroy(hwloc_topology_t topology __hwloc_attribute_unused,
-				hwloc_topology_diff_t diff)
+int hwloc_topology_diff_destroy(hwloc_topology_diff_t diff)
 {
 	hwloc_topology_diff_t next;
 	while (diff) {

hwloc/topology-xml.c
@@ -1116,8 +1116,7 @@ hwloc_look_xml(struct hwloc_backend *backend)
 
 /* this can be the first XML call */
 int
-hwloc_topology_diff_load_xml(hwloc_topology_t topology __hwloc_attribute_unused,
-			     const char *xmlpath,
+hwloc_topology_diff_load_xml(const char *xmlpath,
 			     hwloc_topology_diff_t *firstdiffp, char **refnamep)
 {
   struct hwloc__xml_import_state_s state;
@@ -1163,8 +1162,7 @@ retry:
 
 /* this can be the first XML call */
 int
-hwloc_topology_diff_load_xmlbuffer(hwloc_topology_t topology __hwloc_attribute_unused,
-				   const char *xmlbuffer, int buflen,
+hwloc_topology_diff_load_xmlbuffer(const char *xmlbuffer, int buflen,
 				   hwloc_topology_diff_t *firstdiffp, char **refnamep)
 {
   struct hwloc__xml_import_state_s state;
@@ -1520,8 +1518,7 @@ retry:
 
 /* this can be the first XML call */
 int
-hwloc_topology_diff_export_xml(hwloc_topology_t topology __hwloc_attribute_unused,
-			       hwloc_topology_diff_t diff, const char *refname,
+hwloc_topology_diff_export_xml(hwloc_topology_diff_t diff, const char *refname,
 			       const char *filename)
 {
   hwloc_localeswitch_declare;
@@ -1562,8 +1559,7 @@ retry:
 
 /* this can be the first XML call */
 int
-hwloc_topology_diff_export_xmlbuffer(hwloc_topology_t topology __hwloc_attribute_unused,
-				     hwloc_topology_diff_t diff, const char *refname,
+hwloc_topology_diff_export_xmlbuffer(hwloc_topology_diff_t diff, const char *refname,
 				     char **xmlbuffer, int *buflen)
 {
   hwloc_localeswitch_declare;

include/hwloc/diff.h
@@ -220,10 +220,8 @@ enum hwloc_topology_diff_apply_flags_e {
 HWLOC_DECLSPEC int hwloc_topology_diff_apply(hwloc_topology_t topology, hwloc_topology_diff_t diff, unsigned long flags);
 
 /** \brief Destroy a list of topology differences.
- *
- * \note The \p topology parameter may be \c NULL.
  */
-HWLOC_DECLSPEC int hwloc_topology_diff_destroy(hwloc_topology_t topology, hwloc_topology_diff_t diff);
+HWLOC_DECLSPEC int hwloc_topology_diff_destroy(hwloc_topology_diff_t diff);
 
 /** \brief Load a list of topology differences from a XML file.
  *
@@ -233,12 +231,10 @@ HWLOC_DECLSPEC int hwloc_topology_diff_destroy(hwloc_topology_t topology, hwloc_
  * This identifier is usually the name of the other XML file
  * that contains the reference topology.
  *
- * \note The \p topology parameter may be \c NULL.
- *
  * \note the pointer returned in refname should later be freed
  * by the caller.
  */
-HWLOC_DECLSPEC int hwloc_topology_diff_load_xml(hwloc_topology_t topology, const char *xmlpath, hwloc_topology_diff_t *diff, char **refname);
+HWLOC_DECLSPEC int hwloc_topology_diff_load_xml(const char *xmlpath, hwloc_topology_diff_t *diff, char **refname);
 
 /** \brief Export a list of topology differences to a XML file.
  *
@@ -248,10 +244,8 @@ HWLOC_DECLSPEC int hwloc_topology_diff_load_xml(hwloc_topology_t topology, const
  * This identifier is usually the name of the other XML file
  * that contains the reference topology.
  * This attribute is given back when reading the diff from XML.
- *
- * \note The \p topology parameter may be \c NULL.
  */
-HWLOC_DECLSPEC int hwloc_topology_diff_export_xml(hwloc_topology_t topology, hwloc_topology_diff_t diff, const char *refname, const char *xmlpath);
+HWLOC_DECLSPEC int hwloc_topology_diff_export_xml(hwloc_topology_diff_t diff, const char *refname, const char *xmlpath);
 
 /** \brief Load a list of topology differences from a XML buffer.
  *
@@ -261,12 +255,10 @@ HWLOC_DECLSPEC int hwloc_topology_diff_export_xml(hwloc_topology_t topology, hwl
  * This identifier is usually the name of the other XML file
  * that contains the reference topology.
  *
- * \note The \p topology parameter may be \c NULL.
- *
  * \note the pointer returned in refname should later be freed
  * by the caller.
   */
-HWLOC_DECLSPEC int hwloc_topology_diff_load_xmlbuffer(hwloc_topology_t topology, const char *xmlbuffer, int buflen, hwloc_topology_diff_t *diff, char **refname);
+HWLOC_DECLSPEC int hwloc_topology_diff_load_xmlbuffer(const char *xmlbuffer, int buflen, hwloc_topology_diff_t *diff, char **refname);
 
 /** \brief Export a list of topology differences to a XML buffer.
  *
@@ -278,10 +270,8 @@ HWLOC_DECLSPEC int hwloc_topology_diff_load_xmlbuffer(hwloc_topology_t topology,
  * This attribute is given back when reading the diff from XML.
  *
  * \note The XML buffer should later be freed with hwloc_free_xmlbuffer().
- *
- * \note The \p topology parameter may be \c NULL.
  */
-HWLOC_DECLSPEC int hwloc_topology_diff_export_xmlbuffer(hwloc_topology_t topology, hwloc_topology_diff_t diff, const char *refname, char **xmlbuffer, int *buflen);
+HWLOC_DECLSPEC int hwloc_topology_diff_export_xmlbuffer(hwloc_topology_diff_t diff, const char *refname, char **xmlbuffer, int *buflen);
 
 /** @} */
 

netloc/map.c
@@ -482,18 +482,18 @@ netloc_map__init_servers(struct netloc_map *map)
                 if (map->verbose_flags & NETLOC_MAP_VERBOSE_FLAG_COMPRESS)
                     printf("loading topology diff %s\n", filepath);
 
-                err = hwloc_topology_diff_load_xml(NULL, filepath, &diff, &refname);
+                err = hwloc_topology_diff_load_xml(filepath, &diff, &refname);
                 free(filepath);
                 if (err < 0)
                     continue;
                 if (!refname) {
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     continue;
                 }
                 refnamelen = strlen(refname);
                 if (strcmp(refname+refnamelen-4, ".xml")) {
                     free(refname);
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     continue;
                 }
                 refname[refnamelen-4] = '\0';
@@ -505,7 +505,7 @@ netloc_map__init_servers(struct netloc_map *map)
                 if (!refserver) {
                     fprintf(stderr, "Could not find hwloc topology diff reference server %s\n", refname);
                     free(refname);
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     continue;
                 }
 
@@ -513,27 +513,27 @@ netloc_map__init_servers(struct netloc_map *map)
                 if (netloc_map__prepare_hwloc_topology(map, refserver) < 0) {
                     fprintf(stderr, "Failed to uncompress reference server %s topology\n", refname);
                     free(refname);
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     continue;
                 }
                 free(refname);
 
                 err = hwloc_topology_dup(&topo, refserver->topology);
                 if (err < 0) {
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     continue;
                 }
 
                 err = hwloc_topology_diff_apply(topo, diff, 0);
                 if (err < 0) {
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     hwloc_topology_destroy(topo);
                     continue;
                 }
 
                 server = netloc_map__init_server(map, topo, name);
                 if (!server) {
-                    hwloc_topology_diff_destroy(NULL, diff);
+                    hwloc_topology_diff_destroy(diff);
                     hwloc_topology_destroy(topo);
                 } else {
                     /* ideally, we would walk up the chain of reference servers, but:
@@ -696,7 +696,7 @@ netloc_map__destroy_servers(struct netloc_map *map)
     while (curserver) {
         nextserver = curserver->next;
         if (curserver->topology_diff_refserver)
-            hwloc_topology_diff_destroy(NULL, curserver->topology_diff);
+            hwloc_topology_diff_destroy(curserver->topology_diff);
         if (curserver->topology)
             hwloc_topology_destroy(curserver->topology);
         for(i=0; i<curserver->nr_ports; i++)

tests/hwloc/hwloc_topology_diff.c
@@ -42,7 +42,7 @@ int main(void)
   assert(diff->generic.next == NULL);
   assert(diff->too_complex.obj_depth == 0);
   assert(diff->too_complex.obj_index == 0);
-  hwloc_topology_diff_destroy(topo1, diff);
+  hwloc_topology_diff_destroy(diff);
 
   printf("add a similar info to topo1, and change memory sizes\n");
   obj = hwloc_get_root_obj(topo2);
@@ -98,10 +98,10 @@ int main(void)
   assert(!diff2);
 
   printf("exporting and reloading diff from XML buffer without refname\n");
-  err = hwloc_topology_diff_export_xmlbuffer(NULL, diff, NULL, &xmlbuffer, &xmlbuflen);
+  err = hwloc_topology_diff_export_xmlbuffer(diff, NULL, &xmlbuffer, &xmlbuflen);
   assert(!err);
-  hwloc_topology_diff_destroy(topo1, diff);
-  err = hwloc_topology_diff_load_xmlbuffer(NULL, xmlbuffer, xmlbuflen, &diff2, &refname);
+  hwloc_topology_diff_destroy(diff);
+  err = hwloc_topology_diff_load_xmlbuffer(xmlbuffer, xmlbuflen, &diff2, &refname);
   assert(!err);
   assert(diff2);
   assert(!refname);
@@ -109,10 +109,10 @@ int main(void)
   hwloc_free_xmlbuffer(topo1, xmlbuffer);
 
   printf("exporting and reloading diff from XML buffer with refname\n");
-  err = hwloc_topology_diff_export_xmlbuffer(NULL, diff2, "foobar", &xmlbuffer, &xmlbuflen);
+  err = hwloc_topology_diff_export_xmlbuffer(diff2, "foobar", &xmlbuffer, &xmlbuflen);
   assert(!err);
-  hwloc_topology_diff_destroy(topo1, diff2);
-  err = hwloc_topology_diff_load_xmlbuffer(NULL, xmlbuffer, xmlbuflen, &diff, &refname);
+  hwloc_topology_diff_destroy(diff2);
+  err = hwloc_topology_diff_load_xmlbuffer(xmlbuffer, xmlbuflen, &diff, &refname);
   assert(!err);
   assert(diff);
   err = strcmp(refname, "foobar");
@@ -153,7 +153,7 @@ int main(void)
   assert(err == 0);
   assert(!diff2);
 
-  hwloc_topology_diff_destroy(topo1, diff);
+  hwloc_topology_diff_destroy(diff);
 
   printf("adding new key to the bottom of topo3\n");
   obj = hwloc_get_obj_by_type(topo3, HWLOC_OBJ_PU, 0);
@@ -172,7 +172,7 @@ int main(void)
   tmpdiff = tmpdiff->generic.next;
   assert(tmpdiff->generic.type == HWLOC_TOPOLOGY_DIFF_TOO_COMPLEX);
   assert(tmpdiff->generic.next == NULL);
-  hwloc_topology_diff_destroy(topo1, diff);
+  hwloc_topology_diff_destroy(diff);
 
   printf("adding similar key to topo1\n");
   obj = hwloc_get_obj_by_type(topo1, HWLOC_OBJ_PU, 0);
@@ -184,7 +184,7 @@ int main(void)
   assert(diff);
   err = hwloc_topology_diff_apply(topo2, diff, HWLOC_TOPOLOGY_DIFF_APPLY_REVERSE);
   assert(err == -4);
-  hwloc_topology_diff_destroy(topo1, diff);
+  hwloc_topology_diff_destroy(diff);
 
   hwloc_topology_destroy(topo3);
   hwloc_topology_destroy(topo2);

utils/hwloc/hwloc-diff.c
@@ -125,9 +125,9 @@ int main(int argc, char *argv[])
 	}
 	if (!j) {
 		if (output) {
-			err = hwloc_topology_diff_export_xml(NULL, firstdiff, refname, output);
+			err = hwloc_topology_diff_export_xml(firstdiff, refname, output);
 		} else {
-			err = hwloc_topology_diff_export_xmlbuffer(NULL, firstdiff, refname, &xmlbuffer, &xmlbuflen);
+			err = hwloc_topology_diff_export_xmlbuffer(firstdiff, refname, &xmlbuffer, &xmlbuflen);
 			if (!err) {
 				printf("%s\n", xmlbuffer);
 				hwloc_free_xmlbuffer(topo1, xmlbuffer);
@@ -137,7 +137,7 @@ int main(int argc, char *argv[])
 			fprintf(stderr, "Failed to export topology diff %s\n", output);
 	}
 
-	hwloc_topology_diff_destroy(NULL, firstdiff);
+	hwloc_topology_diff_destroy(firstdiff);
 
 	hwloc_topology_destroy(topo2);
 	hwloc_topology_destroy(topo1);

commit 7937942d4741a6252afb262188784a3cce159d4a

hwloc-patch: fix the error paths
CID #1341675

(partially caused by a10e09fa9b58102c05c13c06abdf782ad6fdd8e0)

utils/hwloc/hwloc-patch.c
@@ -111,7 +111,7 @@ int main(int argc, char *argv[])
 	err = hwloc_diff_read(inputdiff, &firstdiff, &refname);
 	if (err < 0) {
 		fprintf(stderr, "Failed to load XML topology diff %s\n", inputdiff);
-		goto out_with_topo;
+		goto out;
 	}
 
 	/* load the input topology */
@@ -122,19 +122,19 @@ int main(int argc, char *argv[])
 		/* use the diff refname as input */
 		if (!refname) {
 			fprintf(stderr, "Couldn't find the reference topology name from the input diff %s\n", inputdiff);
-			goto out_with_diff;
+			goto out_with_topo;
 		}
 		err = hwloc_topology_set_xml(topo, refname);
 		if (err < 0) {
 			fprintf(stderr, "Failed to load XML topology %s (from input diff %s refname)\n", refname, inputdiff);
-			goto out;
+			goto out_with_topo;
 		}
 	} else {
 		/* use the given input */
 		err = hwloc_topology_set_xml(topo, input);
 		if (err < 0) {
 			fprintf(stderr, "Failed to load XML topology %s\n", input);
-			goto out;
+			goto out_with_topo;
 		}
 	}
 
@@ -145,24 +145,23 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Failed to%s apply topology diff %s, failed for hunk #%d hunk\n",
 			(patchflags & HWLOC_TOPOLOGY_DIFF_APPLY_REVERSE) ? " reverse" : "",
 			inputdiff, -err);
-		goto out_with_diff;
+		goto out_with_topo;
 	}
 
 	err = hwloc_topology_export_xml(topo, output ? output : input);
 	if (err < 0) {
 		fprintf(stderr, "Failed to export patched topology %s\n", output);
-		goto out_with_diff;
+		goto out_with_topo;
 	}
 
-	hwloc_topology_diff_destroy(firstdiff);
 	hwloc_topology_destroy(topo);
+	hwloc_topology_diff_destroy(firstdiff);
 
 	exit(EXIT_SUCCESS);
 
-out_with_diff:
-	hwloc_topology_diff_destroy(firstdiff);
 out_with_topo:
 	hwloc_topology_destroy(topo);
+	hwloc_topology_diff_destroy(firstdiff);
 out:
 	exit(EXIT_FAILURE);
 }