open-mpi/hwloc

commit 11003cc78be0991fd02b411c56f36078ff2b800b

x86: add Inclusive=0/1 info attribute to Cache objects
Either annotate previously inserted objects or those created by the x86 backend.

Thanks to Guillaume Beauchamp for the patch.

hwloc/topology-x86.c
@@ -163,6 +163,7 @@ struct cacheinfo {
 
   unsigned linesize;
   unsigned linepart;
+  int inclusive;
   int ways;
   unsigned sets;
   unsigned long size;
@@ -217,6 +218,8 @@ static void fill_amd_cache(struct procinfo *infos, unsigned level, hwloc_obj_cac
   cache->nbthreads_sharing = nbthreads_sharing;
   cache->linesize = cpuid & 0xff;
   cache->linepart = 0;
+  cache->inclusive = 0; /* old AMD (K8-K10) supposed to have exclusive caches */
+
   if (level == 1) {
     cache->ways = (cpuid >> 16) & 0xff;
     if (cache->ways == 0xff)
@@ -399,6 +402,7 @@ static void look_proc(struct hwloc_backend *backend, struct procinfo *infos, uns
 	cache->ways = ways;
       cache->sets = sets = ecx + 1;
       cache->size = linesize * linepart * ways * sets;
+      cache->inclusive = edx & 0x2;
 
       hwloc_debug("cache %u L%u%c t%u linesize %lu linepart %lu ways %lu sets %lu, size %luKB\n",
 		  cachenum, cache->level,
@@ -489,6 +493,7 @@ static void look_proc(struct hwloc_backend *backend, struct procinfo *infos, uns
         cache->ways = ways;
       cache->sets = sets = ecx + 1;
       cache->size = linesize * linepart * ways * sets;
+      cache->inclusive = edx & 0x2;
 
       hwloc_debug("cache %u L%u%c t%u linesize %lu linepart %lu ways %lu sets %lu, size %luKB\n",
 		  cachenum, cache->level,
@@ -887,77 +892,91 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
     for (j = 0; j < infos[i].numcaches; j++)
       if (infos[i].cache[j].level > level)
         level = infos[i].cache[j].level;
+  while (level > 0) {
+    hwloc_obj_cache_type_t type;
+    HWLOC_BUILD_ASSERT(HWLOC_OBJ_CACHE_DATA == HWLOC_OBJ_CACHE_UNIFIED+1);
+    HWLOC_BUILD_ASSERT(HWLOC_OBJ_CACHE_INSTRUCTION == HWLOC_OBJ_CACHE_DATA+1);
+    for (type = HWLOC_OBJ_CACHE_UNIFIED; type <= HWLOC_OBJ_CACHE_INSTRUCTION; type++) {
+      /* Look for caches of that type at level level */
+      hwloc_obj_type_t otype;
+      hwloc_bitmap_t caches_cpuset;
+      hwloc_obj_t cache;
 
-  /* Look for known types */
-  if (fulldiscovery)
-    while (level > 0) {
-      hwloc_obj_cache_type_t type;
-      HWLOC_BUILD_ASSERT(HWLOC_OBJ_CACHE_DATA == HWLOC_OBJ_CACHE_UNIFIED+1);
-      HWLOC_BUILD_ASSERT(HWLOC_OBJ_CACHE_INSTRUCTION == HWLOC_OBJ_CACHE_DATA+1);
-      for (type = HWLOC_OBJ_CACHE_UNIFIED; type <= HWLOC_OBJ_CACHE_INSTRUCTION; type++) {
-	/* Look for caches of that type at level level */
-	hwloc_obj_type_t otype;
-	hwloc_bitmap_t caches_cpuset;
-	hwloc_bitmap_t cache_cpuset;
-	hwloc_obj_t cache;
+      otype = hwloc_cache_type_by_depth_type(level, type);
+      if (otype == HWLOC_OBJ_TYPE_NONE)
+	continue;
+      if (!hwloc_filter_check_keep_object_type(topology, otype))
+	continue;
 
-	otype = hwloc_cache_type_by_depth_type(level, type);
-	if (otype == HWLOC_OBJ_TYPE_NONE)
+      caches_cpuset = hwloc_bitmap_dup(complete_cpuset);
+      while ((i = hwloc_bitmap_first(caches_cpuset)) != (unsigned) -1) {
+	for (l = 0; l < infos[i].numcaches; l++) {
+	  if (infos[i].cache[l].level == level && infos[i].cache[l].type == type)
+	    break;
+	}
+	if (l == infos[i].numcaches) {
+	  /* no cache Llevel of that type in i */
+	  hwloc_bitmap_clr(caches_cpuset, i);
 	  continue;
-	if (!hwloc_filter_check_keep_object_type(topology, otype))
-	  continue;
+	}
 
-	caches_cpuset = hwloc_bitmap_dup(complete_cpuset);
-	while ((i = hwloc_bitmap_first(caches_cpuset)) != (unsigned) -1) {
+	if (fulldiscovery) {
+	  /* Add caches */
+	  hwloc_bitmap_t cache_cpuset;
 	  unsigned packageid = infos[i].packageid;
-
-	  for (l = 0; l < infos[i].numcaches; l++) {
-	    if (infos[i].cache[l].level == level && infos[i].cache[l].type == type)
-	      break;
-	  }
-	  if (l == infos[i].numcaches) {
-	    /* no cache Llevel of that type in i */
-	    hwloc_bitmap_clr(caches_cpuset, i);
-	    continue;
-	  }
-
+	  unsigned cacheid = infos[i].cache[l].cacheid;
 	  /* Found a matching cache, now look for others sharing it */
-	  {
-	    unsigned cacheid = infos[i].cache[l].cacheid;
 
-	    cache_cpuset = hwloc_bitmap_alloc();
-	    for (j = i; j < nbprocs; j++) {
-	      unsigned l2;
-	      for (l2 = 0; l2 < infos[j].numcaches; l2++) {
-		if (infos[j].cache[l2].level == level && infos[j].cache[l2].type == type)
-		  break;
-	      }
-	      if (l2 == infos[j].numcaches) {
-		/* no cache Llevel of that type in j */
-		hwloc_bitmap_clr(caches_cpuset, j);
-		continue;
-	      }
-	      if (infos[j].packageid == packageid && infos[j].cache[l2].cacheid == cacheid) {
-		hwloc_bitmap_set(cache_cpuset, j);
-		hwloc_bitmap_clr(caches_cpuset, j);
-	      }
+	  cache_cpuset = hwloc_bitmap_alloc();
+	  for (j = i; j < nbprocs; j++) {
+	    unsigned l2;
+	    for (l2 = 0; l2 < infos[j].numcaches; l2++) {
+	      if (infos[j].cache[l2].level == level && infos[j].cache[l2].type == type)
+		break;
 	    }
-	    cache = hwloc_alloc_setup_object(otype, -1);
-	    cache->attr->cache.depth = level;
-	    cache->attr->cache.size = infos[i].cache[l].size;
-	    cache->attr->cache.linesize = infos[i].cache[l].linesize;
-	    cache->attr->cache.associativity = infos[i].cache[l].ways;
-	    cache->attr->cache.type = infos[i].cache[l].type;
-	    cache->cpuset = cache_cpuset;
-	    hwloc_debug_2args_bitmap("os L%u cache %u has cpuset %s\n",
-		level, cacheid, cache_cpuset);
-	    hwloc_insert_object_by_cpuset(topology, cache);
+	    if (l2 == infos[j].numcaches) {
+	      /* no cache Llevel of that type in j */
+	      hwloc_bitmap_clr(caches_cpuset, j);
+	      continue;
+	    }
+	    if (infos[j].packageid == packageid && infos[j].cache[l2].cacheid == cacheid) {
+	      hwloc_bitmap_set(cache_cpuset, j);
+	      hwloc_bitmap_clr(caches_cpuset, j);
+	    }
+	  }
+	  cache = hwloc_alloc_setup_object(otype, -1);
+	  cache->attr->cache.depth = level;
+	  cache->attr->cache.size = infos[i].cache[l].size;
+	  cache->attr->cache.linesize = infos[i].cache[l].linesize;
+	  cache->attr->cache.associativity = infos[i].cache[l].ways;
+	  cache->attr->cache.type = infos[i].cache[l].type;
+	  cache->cpuset = cache_cpuset;
+	  hwloc_obj_add_info(cache, "Inclusive", infos[i].cache[l].inclusive ? "1" : "0");
+	  hwloc_debug_2args_bitmap("os L%u cache %u has cpuset %s\n",
+				   level, cacheid, cache_cpuset);
+	  hwloc_insert_object_by_cpuset(topology, cache);
+
+	} else {
+	  /* Annotate existing caches */
+	  hwloc_bitmap_t set = hwloc_bitmap_alloc();
+	  hwloc_obj_t cache;
+	  hwloc_bitmap_set(set, i);
+	  cache = hwloc_get_next_obj_covering_cpuset_by_type(topology, set, otype, NULL);
+	  if (cache) {
+	    /* Found cache above that PU, annotate if no such attribute yet */
+	    if (!hwloc_obj_get_info_by_name(cache, "Inclusive"))
+	      hwloc_obj_add_info(cache, "Inclusive", infos[i].cache[l].inclusive ? "1" : "0");
+	    hwloc_bitmap_andnot(caches_cpuset, caches_cpuset, cache->cpuset);
+	  } else {
+	    /* No cache above that PU?! */
+	    hwloc_bitmap_clr(caches_cpuset, i);
 	  }
 	}
-	hwloc_bitmap_free(caches_cpuset);
       }
-      level--;
+      hwloc_bitmap_free(caches_cpuset);
     }
+    level--;
+  }
 
   /* FIXME: if KNL and L2 disabled, add tiles instead of L2 */
 

doc/hwloc.doxy
@@ -1494,6 +1494,10 @@ Currently only available on Linux.
 Usually added to Package objects, but can be in Machine instead if
 hwloc failed to discover any package.
 </dd>
+<dt>Inclusive (Caches)</dt>
+<dd>The inclusiveness of a cache (1 if inclusive, 0 otherwise).
+Currently only available on x86 processors.
+</dd>
 <dt>PCIVendor, PCIDevice (PCI devices and bridges)</dt>
 <dd>The vendor and device names of the PCI device.
 </dd>

tests/hwloc/x86/AMD-K8-SantaRosa-2xOpteron-2218.output
@@ -11,8 +11,11 @@
         <info name="CPUModel" value="Dual-Core AMD Opteron(tm) Processor 2218"/>
         <info name="CPUStepping" value="2"/>
         <object type="L2Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -20,8 +23,11 @@
           </object>
         </object>
         <object type="L2Cache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="1" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="1" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -36,8 +42,11 @@
         <info name="CPUModel" value="Dual-Core AMD Opteron(tm) Processor 2218"/>
         <info name="CPUStepping" value="2"/>
         <object type="L2Cache" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="2" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -45,8 +54,11 @@
           </object>
         </object>
         <object type="L2Cache" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="1" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="3" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>

tests/hwloc/x86/AMD-K8-SledgeHammer-2xOpteron-250.output
@@ -11,8 +11,11 @@
         <info name="CPUModel" value="AMD Opteron(tm) Processor 250"/>
         <info name="CPUStepping" value="10"/>
         <object type="L2Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -27,8 +30,11 @@
         <info name="CPUModel" value="AMD Opteron(tm) Processor 250"/>
         <info name="CPUStepping" value="10"/>
         <object type="L2Cache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="1048576" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="65536" depth="1" cache_linesize="64" cache_associativity="2" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="1" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>

tests/hwloc/x86/Intel-Core-2xXeon-E5345.output
@@ -11,15 +11,20 @@
         <info name="CPUModel" value="Intel(R) Xeon(R) CPU           E5345  @ 2.33GHz"/>
         <info name="CPUStepping" value="7"/>
         <object type="L2Cache" cpuset="0x00000011" complete_cpuset="0x00000011" allowed_cpuset="0x00000011" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="4194304" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="0" cpuset="0x00000001" complete_cpuset="0x00000001" allowed_cpuset="0x00000001" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
             </object>
           </object>
           <object type="L1Cache" cpuset="0x00000010" complete_cpuset="0x00000010" allowed_cpuset="0x00000010" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000010" complete_cpuset="0x00000010" allowed_cpuset="0x00000010" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="1" cpuset="0x00000010" complete_cpuset="0x00000010" allowed_cpuset="0x00000010" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="4" cpuset="0x00000010" complete_cpuset="0x00000010" allowed_cpuset="0x00000010" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -27,15 +32,20 @@
           </object>
         </object>
         <object type="L2Cache" cpuset="0x00000044" complete_cpuset="0x00000044" allowed_cpuset="0x00000044" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="4194304" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="2" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="2" cpuset="0x00000004" complete_cpuset="0x00000004" allowed_cpuset="0x00000004" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
             </object>
           </object>
           <object type="L1Cache" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="3" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="6" cpuset="0x00000040" complete_cpuset="0x00000040" allowed_cpuset="0x00000040" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -50,15 +60,20 @@
         <info name="CPUModel" value="Intel(R) Xeon(R) CPU           E5345  @ 2.33GHz"/>
         <info name="CPUStepping" value="7"/>
         <object type="L2Cache" cpuset="0x00000022" complete_cpuset="0x00000022" allowed_cpuset="0x00000022" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="4194304" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="0" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="1" cpuset="0x00000002" complete_cpuset="0x00000002" allowed_cpuset="0x00000002" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
             </object>
           </object>
           <object type="L1Cache" cpuset="0x00000020" complete_cpuset="0x00000020" allowed_cpuset="0x00000020" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000020" complete_cpuset="0x00000020" allowed_cpuset="0x00000020" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="1" cpuset="0x00000020" complete_cpuset="0x00000020" allowed_cpuset="0x00000020" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="5" cpuset="0x00000020" complete_cpuset="0x00000020" allowed_cpuset="0x00000020" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
@@ -66,15 +81,20 @@
           </object>
         </object>
         <object type="L2Cache" cpuset="0x00000088" complete_cpuset="0x00000088" allowed_cpuset="0x00000088" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="4194304" depth="2" cache_linesize="64" cache_associativity="16" cache_type="0">
+          <info name="Inclusive" value="0"/>
           <object type="L1Cache" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="2" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="3" cpuset="0x00000008" complete_cpuset="0x00000008" allowed_cpuset="0x00000008" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>
             </object>
           </object>
           <object type="L1Cache" cpuset="0x00000080" complete_cpuset="0x00000080" allowed_cpuset="0x00000080" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="1">
+            <info name="Inclusive" value="0"/>
             <object type="L1iCache" cpuset="0x00000080" complete_cpuset="0x00000080" allowed_cpuset="0x00000080" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001" cache_size="32768" depth="1" cache_linesize="64" cache_associativity="8" cache_type="2">
+              <info name="Inclusive" value="0"/>
               <object type="Core" os_index="3" cpuset="0x00000080" complete_cpuset="0x00000080" allowed_cpuset="0x00000080" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001">
                 <object type="PU" os_index="7" cpuset="0x00000080" complete_cpuset="0x00000080" allowed_cpuset="0x00000080" nodeset="0x00000001" complete_nodeset="0x00000001" allowed_nodeset="0x00000001"/>
               </object>

commit c21e1d5ce8f669fa3471792b62143b11734c6559

x86: fix a bitmap leak when annotating caches
Leak introduced in 11003cc78be0991fd02b411c56f36078ff2b800b

hwloc/topology-x86.c
@@ -935,6 +935,7 @@ static void summarize(struct hwloc_backend *backend, struct procinfo *infos, int
 	  hwloc_obj_t cache;
 	  hwloc_bitmap_set(set, i);
 	  cache = hwloc_get_next_obj_covering_cpuset_by_type(topology, set, otype, NULL);
+	  hwloc_bitmap_free(set);
 	  if (cache) {
 	    /* Found cache above that PU, annotate if no such attribute yet */
 	    if (!hwloc_obj_get_info_by_name(cache, "Inclusive"))