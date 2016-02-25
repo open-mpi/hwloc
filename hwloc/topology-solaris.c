open-mpi/hwloc

commit 124545f2eb99c6f8b7f385607cc422f3ebbd8ea6

distances: always reject distance matrices with less than 2 objects
Check that environment variables contain at least 2 indexes,
check that Linux/Solaris/XML matrices contain at least 2 objects,
and assert that in the core.

Refs open-mpi/ompi#190

hwloc/topology-solaris.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2015 Inria.  All rights reserved.
+ * Copyright © 2009-2016 Inria.  All rights reserved.
  * Copyright © 2009-2011 Université Bordeaux
  * Copyright © 2011 Cisco Systems, Inc.  All rights reserved.
  * Copyright © 2011      Oracle and/or its affiliates.  All rights reserved.
@@ -412,7 +412,7 @@ hwloc_look_lgrp(struct hwloc_topology *topology)
     }
   nlgrps = lgrp_nlgrps(cookie);
   root = lgrp_root(cookie);
-  {
+  if (nlgrps > 1) {
     hwloc_obj_t *glob_lgrps = calloc(nlgrps, sizeof(hwloc_obj_t));
     browse(topology, cookie, root, glob_lgrps, &curlgrp);
 #ifdef HAVE_LGRP_LATENCY_COOKIE

hwloc/distances.c
@@ -84,6 +84,7 @@ void hwloc_distances_set(hwloc_topology_t __hwloc_restrict topology, hwloc_obj_t
   if (!nbobjs)
     /* we're just clearing, return now */
     return;
+  assert(nbobjs >= 2);
 
   /* create the new element */
   osdist = malloc(sizeof(struct hwloc_os_distances_s));
@@ -136,6 +137,11 @@ static void hwloc_distances__set_from_string(struct hwloc_topology *topology,
 
   if (sscanf(string, "%u-%u:", &i, &j) == 2) {
     /* range i-j */
+    if (j <= i) {
+      fprintf(stderr, "Ignoring %s distances from environment variable, range doesn't cover at least 2 indexes\n",
+	      hwloc_obj_type_string(type));
+      return;
+    }
     nbobjs = j-i+1;
 
     tmp = strchr(string, ':');
@@ -161,6 +167,9 @@ static void hwloc_distances__set_from_string(struct hwloc_topology *topology,
     /* explicit list of indexes, count them */
     while (1) {
       size_t size = strspn(tmp, "0123456789");
+      if (!size)
+	/* missing index */
+	break;
       if (tmp[size] != ',') {
 	/* last element */
 	tmp += size;
@@ -172,6 +181,11 @@ static void hwloc_distances__set_from_string(struct hwloc_topology *topology,
       nbobjs++;
     }
 
+    if (nbobjs < 2) {
+      fprintf(stderr, "Ignoring %s distances from environment variable, needs at least 2 indexes\n",
+	      hwloc_obj_type_string(type));
+      return;
+    }
     if (*tmp != ':') {
       fprintf(stderr, "Ignoring %s distances from environment variable, missing colon\n",
 	      hwloc_obj_type_string(type));
@@ -275,7 +289,7 @@ int hwloc_topology_set_distance_matrix(hwloc_topology_t __hwloc_restrict topolog
     return 0;
   }
 
-  if (!nbobjs || !indexes || !distances)
+  if (nbobjs < 2 || !indexes || !distances)
     return -1;
 
   if (hwloc_distances__check_matrix(topology, type, nbobjs, indexes, NULL, distances) < 0)

hwloc/topology-xml.c
@@ -566,12 +566,23 @@ hwloc__xml_import_distances(struct hwloc_xml_backend_data_s *data,
 
     distances->distances.latency_max = latmax;
 
-    if (data->last_distances)
-      data->last_distances->next = distances;
-    else
-      data->first_distances = distances;
-    distances->prev = data->last_distances;
-    distances->next = NULL;
+    if (nbobjs < 2) {
+      /* distances with a single object are useless, even if the XML isn't invalid */
+      assert(nbobjs == 1);
+      if (hwloc__xml_verbose())
+	fprintf(stderr, "%s: ignoring invalid distance matrix with only 1 object\n",
+		state->global->msgprefix);
+      free(matrix);
+      free(distances);
+    } else {
+      /* queue the distance */
+      if (data->last_distances)
+	data->last_distances->next = distances;
+      else
+	data->first_distances = distances;
+      distances->prev = data->last_distances;
+      distances->next = NULL;
+    }
   }
 
   return state->global->close_tag(state);

include/hwloc.h
@@ -2025,6 +2025,7 @@ hwloc_topology_set_io_types_filter(hwloc_topology_t topology, enum hwloc_type_fi
 /** \brief Provide a distance matrix.
  *
  * Provide the matrix of distances between a set of objects of the given type.
+ * \p nbobjs must be at least 2.
  * The set may or may not contain all the existing objects of this type.
  * The objects are specified by their OS/physical index in the \p os_index
  * array. The \p distances matrix follows the same order.
@@ -2036,8 +2037,6 @@ hwloc_topology_set_io_types_filter(hwloc_topology_t topology, enum hwloc_type_fi
  * it will be replaced by the given one.
  * If \p nbobjs is \c 0, \p os_index is \c NULL and \p distances is \c NULL,
  * the existing distance matrix for the given type is removed.
- *
- * \note Distance matrices are ignored in multi-node topologies.
  */
 HWLOC_DECLSPEC int hwloc_topology_set_distance_matrix(hwloc_topology_t __hwloc_restrict topology,
 						      hwloc_obj_type_t type, unsigned nbobjs,

commit 877203dc00ebf6b47690b9d28614ad05969e97cc

solaris: fix the rejecting of distance matrix
Reject distance matrices with less than 2 NUMA nodes,
but still browse those NUMA nodes first.

(bug introduced in commit 124545f2eb99c6f8b7f385607cc422f3ebbd8ea6)

hwloc/topology-solaris.c
@@ -412,11 +412,11 @@ hwloc_look_lgrp(struct hwloc_topology *topology)
     }
   nlgrps = lgrp_nlgrps(cookie);
   root = lgrp_root(cookie);
-  if (nlgrps > 1) {
+  if (nlgrps > 0) {
     hwloc_obj_t *glob_lgrps = calloc(nlgrps, sizeof(hwloc_obj_t));
     browse(topology, cookie, root, glob_lgrps, &curlgrp);
 #ifdef HAVE_LGRP_LATENCY_COOKIE
-    {
+    if (nlgrps > 1) {
       float *distances = calloc(curlgrp*curlgrp, sizeof(float));
       unsigned *indexes = calloc(curlgrp,sizeof(unsigned));
       unsigned i, j;