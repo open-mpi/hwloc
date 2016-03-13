43507c2ef18e6c7e3abc19ad13e26179871b8b1a
Brice Goglin <Brice.Goglin@inria.fr>
2016-03-13 21:14:45 +0100

    API: improve the doc of hwloc_obj_type_string() and hwloc_obj_type_snprintf()
    (cherry picked from commit febc83e5893c790549428d894db1baf75a784be2)

--- include/hwloc.h
+++ include/hwloc.h
@@ -1242,7 +1242,13 @@ hwloc_get_next_obj_by_type (hwloc_topology_t topology, hwloc_obj_type_t type,
  * @{
  */
 
-/** \brief Return a stringified topology object type */
+/** \brief Return a constant stringified object type.
+ *
+ * This function is the basic way to convert a generic type into a string.
+ *
+ * hwloc_obj_type_snprintf() may return a more precise output for a specific
+ * object, but it requires the caller to provide the output buffer.
+ */
 HWLOC_DECLSPEC const char * hwloc_obj_type_string (hwloc_obj_type_t type) __hwloc_attribute_const;
 
 /** \brief Return an object type and attributes from a type string.
@@ -1281,8 +1287,11 @@ HWLOC_DECLSPEC int hwloc_obj_type_sscanf(const char *string,
 
 /** \brief Stringify the type of a given topology object into a human-readable form.
  *
- * It differs from hwloc_obj_type_string() because it prints type attributes such
- * as cache depth and type.
+ * Contrary to hwloc_obj_type_string(), this function includes object-specific
+ * attributes (such as the Group depth, the Bridge type, or OS device type)
+ * in the output, and it requires the caller to provide the output buffer.
+ *
+ * The output is guaranteed to be the same for all objects of a same topology level.
  *
  * If \p size is 0, \p string may safely be \c NULL.
  *
