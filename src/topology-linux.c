open-mpi/hwloc

commit cf1cfb02a9c7d339e00c3a3d8cafec2155b360a9

linux: use hwloc_getpagesize() instead sysconf() in set_area_membind()
(cherry picked from commit aa87e49644b7d910c8283cafcebfb2eb76867995)

src/topology-linux.c
@@ -1228,7 +1228,7 @@ hwloc_linux_set_area_membind(hwloc_topology_t topology, const void *addr, size_t
   unsigned linuxflags = 0;
   int err;
 
-  remainder = (uintptr_t) addr & (sysconf(_SC_PAGESIZE)-1);
+  remainder = (uintptr_t) addr & hwloc_getpagesize();
   addr = (char*) addr - remainder;
   len += remainder;
 

commit c8e6d4033a4ae754003591d4b12a99f9284a2a14

linux: fix set_area_membind()
(cherry picked from commit 4b2603abca42ea7a244e944f37d454131e311eae)

Typo introduced yesterday in commit cf1cfb02a9c7d339e00c3a3d8cafec2155b360a9
(cherry picked from commit aa87e49644b7d910c8283cafcebfb2eb76867995)

src/topology-linux.c
@@ -1230,7 +1230,7 @@ hwloc_linux_set_area_membind(hwloc_topology_t topology, const void *addr, size_t
   unsigned linuxflags = 0;
   int err;
 
-  remainder = (uintptr_t) addr & hwloc_getpagesize();
+  remainder = (uintptr_t) addr & (hwloc_getpagesize()-1);
   addr = (char*) addr - remainder;
   len += remainder;
 