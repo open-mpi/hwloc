open-mpi/hwloc

commit c0710b77594deede2b38095d22984e45a600e9d3

core: ignore new entries when failing to realloc/extend the target array
Refs #202.

hwloc/pci-common.c
@@ -52,11 +52,17 @@ hwloc_pci_forced_locality_parse_one(struct hwloc_topology *topology,
   hwloc_bitmap_sscanf(set, tmp);
 
   if (!*allocated) {
+    topology->pci_forced_locality = malloc(sizeof(*topology->pci_forced_locality));
+    if (!topology->pci_forced_locality)
+      return; /* failed to allocate, ignore this forced locality */
     *allocated = 1;
-    topology->pci_forced_locality = malloc(*allocated * sizeof(*topology->pci_forced_locality));
   } else if (nr >= *allocated) {
-    topology->pci_forced_locality = realloc(topology->pci_forced_locality,
-					    2 * *allocated * sizeof(*topology->pci_forced_locality));
+    struct hwloc_pci_forced_locality_s *tmp;
+    tmp = realloc(topology->pci_forced_locality,
+		  2 * *allocated * sizeof(*topology->pci_forced_locality));
+    if (!tmp)
+      return; /* failed to allocate, ignore this forced locality */
+    topology->pci_forced_locality = tmp;
     *allocated *= 2;
   }
 

hwloc/distances.c
@@ -472,6 +472,7 @@ hwloc_distances__finalize_logical(struct hwloc_topology *topology,
 				  unsigned nbobjs,
 				  hwloc_obj_t *objs, float *osmatrix)
 {
+  struct hwloc_distances_s ** tmpdistances;
   unsigned i, j, li, lj, minl;
   float min = FLT_MAX, max = FLT_MIN;
   hwloc_obj_t root;
@@ -573,8 +574,12 @@ hwloc_distances__finalize_logical(struct hwloc_topology *topology,
   }
 
   /* store the normalized latency matrix in the root object */
+  tmpdistances = realloc(root->distances, (root->distances_count+1) * sizeof(struct hwloc_distances_s *));
+  if (!tmpdistances)
+    return; /* Failed to allocate, ignore this distance matrix */
+
+  root->distances = tmpdistances;
   idx = root->distances_count++;
-  root->distances = realloc(root->distances, root->distances_count * sizeof(struct hwloc_distances_s *));
   root->distances[idx] = malloc(sizeof(struct hwloc_distances_s));
   root->distances[idx]->relative_depth = relative_depth;
   root->distances[idx]->nbobjs = nbobjs;

hwloc/topology-linux.c
@@ -2438,7 +2438,12 @@ hwloc_read_str(const char *p, const char *p1, int root_fd)
   size_t cb = 0;
   char *ret = hwloc_read_raw(p, p1, &cb, root_fd);
   if ((NULL != ret) && (0 < cb) && (0 != ret[cb-1])) {
-    ret = realloc(ret, cb + 1);
+    char *tmp = realloc(ret, cb + 1);
+    if (!tmp) {
+      free(ret);
+      return NULL;
+    }
+    ret = tmp;
     ret[cb] = 0;
   }
   return ret;
@@ -2475,11 +2480,17 @@ add_device_tree_cpus_node(device_tree_cpus_t *cpus, hwloc_bitmap_t cpuset,
     uint32_t l2_cache, uint32_t phandle, const char *name)
 {
   if (cpus->n == cpus->allocated) {
+    void *tmp;
+    unsigned allocated;
     if (!cpus->allocated)
-      cpus->allocated = 64;
+      allocated = 64;
     else
-      cpus->allocated *= 2;
-    cpus->p = realloc(cpus->p, cpus->allocated * sizeof(cpus->p[0]));
+      allocated = 2 * cpus->allocated;
+    tmp = realloc(cpus->p, allocated * sizeof(cpus->p[0]));
+    if (!tmp)
+      return; /* failed to realloc, ignore this entry */
+    cpus->p = tmp;
+    cpus->allocated = allocated;
   }
   cpus->p[cpus->n].phandle = phandle;
   cpus->p[cpus->n].cpuset = (NULL == cpuset)?NULL:hwloc_bitmap_dup(cpuset);

hwloc/topology-x86.c
@@ -200,7 +200,7 @@ enum cpuid_type {
 
 static void fill_amd_cache(struct procinfo *infos, unsigned level, hwloc_obj_cache_type_t type, unsigned nbthreads_sharing, unsigned cpuid)
 {
-  struct cacheinfo *cache;
+  struct cacheinfo *cache, *tmpcaches;
   unsigned cachenum;
   unsigned long size = 0;
 
@@ -213,8 +213,13 @@ static void fill_amd_cache(struct procinfo *infos, unsigned level, hwloc_obj_cac
   if (!size)
     return;
 
+  tmpcaches = realloc(infos->cache, (infos->numcaches+1)*sizeof(*infos->cache));
+  if (!tmpcaches)
+    /* failed to allocated, ignore that cache */
+    return;
+  infos->cache = tmpcaches;
   cachenum = infos->numcaches++;
-  infos->cache = realloc(infos->cache, infos->numcaches*sizeof(*infos->cache));
+
   cache = &infos->cache[cachenum];
 
   cache->type = type;

hwloc/topology-xml.c
@@ -489,10 +489,14 @@ hwloc__xml_import_pagetype(hwloc_topology_t topology __hwloc_attribute_unused, h
 
   if (size) {
     int idx = obj->memory.page_types_len;
-    obj->memory.page_types = realloc(obj->memory.page_types, (idx+1)*sizeof(*obj->memory.page_types));
-    obj->memory.page_types_len = idx+1;
-    obj->memory.page_types[idx].size = size;
-    obj->memory.page_types[idx].count = count;
+    struct hwloc_obj_memory_page_type_s *tmp;
+    tmp = realloc(obj->memory.page_types, (idx+1)*sizeof(*obj->memory.page_types));
+    if (tmp) { /* if failed to allocate, ignore this page_type entry */
+      obj->memory.page_types = tmp;
+      obj->memory.page_types_len = idx+1;
+      obj->memory.page_types[idx].size = size;
+      obj->memory.page_types[idx].count = count;
+    }
   }
 
   return state->global->close_tag(state);

hwloc/topology.c
@@ -286,8 +286,13 @@ void hwloc__add_info(struct hwloc_obj_info_s **infosp, unsigned *countp, const c
 #define OBJECT_INFO_ALLOC 8
   /* nothing allocated initially, (re-)allocate by multiple of 8 */
   unsigned alloccount = (count + 1 + (OBJECT_INFO_ALLOC-1)) & ~(OBJECT_INFO_ALLOC-1);
-  if (count != alloccount)
-    infos = realloc(infos, alloccount*sizeof(*infos));
+  if (count != alloccount) {
+    struct hwloc_obj_info_s *tmpinfos = realloc(infos, alloccount*sizeof(*infos));
+    if (!tmpinfos)
+      /* failed to allocate, ignore this info */
+      return;
+    infos = tmpinfos;
+  }
   infos[count].name = strdup(name);
   infos[count].value = value ? strdup(value) : NULL;
   *infosp = infos;
@@ -316,8 +321,13 @@ void hwloc__move_infos(struct hwloc_obj_info_s **dst_infosp, unsigned *dst_count
 #define OBJECT_INFO_ALLOC 8
   /* nothing allocated initially, (re-)allocate by multiple of 8 */
   unsigned alloccount = (dst_count + src_count + (OBJECT_INFO_ALLOC-1)) & ~(OBJECT_INFO_ALLOC-1);
-  if (dst_count != alloccount)
-    dst_infos = realloc(dst_infos, alloccount*sizeof(*dst_infos));
+  if (dst_count != alloccount) {
+    struct hwloc_obj_info_s *tmp_infos = realloc(dst_infos, alloccount*sizeof(*dst_infos));
+    if (!tmp_infos)
+      /* Failed to realloc, ignore the appended infos */
+      goto drop;
+    dst_infos = tmp_infos;
+  }
   for(i=0; i<src_count; i++, dst_count++) {
     dst_infos[dst_count].name = src_infos[i].name;
     dst_infos[dst_count].value = src_infos[i].value;
@@ -327,6 +337,17 @@ void hwloc__move_infos(struct hwloc_obj_info_s **dst_infosp, unsigned *dst_count
   free(src_infos);
   *src_infosp = NULL;
   *src_countp = 0;
+  return;
+
+ drop:
+  for(i=0; i<src_count; i++, dst_count++) {
+    free(src_infos[i].name);
+    free(src_infos[i].value);
+  }
+  free(src_infos);
+  *src_infosp = NULL;
+  *src_countp = 0;
+  /* dst_infos not modified */
 }
 
 void hwloc_obj_add_info(hwloc_obj_t obj, const char *name, const char *value)
@@ -1023,10 +1044,17 @@ merge_insert_equal(hwloc_obj_t new, hwloc_obj_t old)
 
   if (new->distances_count) {
     if (old->distances_count) {
-      old->distances_count += new->distances_count;
-      old->distances = realloc(old->distances, old->distances_count * sizeof(*old->distances));
-      memcpy(old->distances + new->distances_count, new->distances, new->distances_count * sizeof(*old->distances));
-      free(new->distances);
+      struct hwloc_distances_s **tmpdists;
+      tmpdists = realloc(old->distances, (old->distances_count+new->distances_count) * sizeof(*old->distances));
+      if (!tmpdists) {
+	/* failed to realloc, ignore new distances */
+	hwloc_clear_object_distances(new);
+      } else {
+	old->distances = tmpdists;
+	old->distances_count += new->distances_count;
+	memcpy(old->distances + new->distances_count, new->distances, new->distances_count * sizeof(*old->distances));
+	free(new->distances);
+      }
     } else {
       old->distances_count = new->distances_count;
       old->distances = new->distances;

commit 5b49a8f35a3bccde0b408e524967dd1fd709184b

pci: fix a memory leak in case of forced locality realloc failure
Introduced by c0710b77594deede2b38095d22984e45a600e9d3

CID #1368960

hwloc/pci-common.c
@@ -54,14 +54,14 @@ hwloc_pci_forced_locality_parse_one(struct hwloc_topology *topology,
   if (!*allocated) {
     topology->pci_forced_locality = malloc(sizeof(*topology->pci_forced_locality));
     if (!topology->pci_forced_locality)
-      return; /* failed to allocate, ignore this forced locality */
+      goto out_with_set; /* failed to allocate, ignore this forced locality */
     *allocated = 1;
   } else if (nr >= *allocated) {
     struct hwloc_pci_forced_locality_s *tmp;
     tmp = realloc(topology->pci_forced_locality,
 		  2 * *allocated * sizeof(*topology->pci_forced_locality));
     if (!tmp)
-      return; /* failed to allocate, ignore this forced locality */
+      goto out_with_set; /* failed to allocate, ignore this forced locality */
     topology->pci_forced_locality = tmp;
     *allocated *= 2;
   }
@@ -71,6 +71,11 @@ hwloc_pci_forced_locality_parse_one(struct hwloc_topology *topology,
   topology->pci_forced_locality[nr].bus_last = bus_last;
   topology->pci_forced_locality[nr].cpuset = set;
   topology->pci_forced_locality_nr++;
+  return;
+
+ out_with_set:
+  hwloc_bitmap_free(set);
+  return;
 }
 
 static void