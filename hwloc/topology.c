open-mpi/hwloc

commit c04eb5de4b5ed91e73740881c4e73c0651943cab

core: properly handle realloc() erros when reconnecting levels
Refs #202.

hwloc/topology.c
@@ -2494,11 +2494,12 @@ hwloc_connect_levels(hwloc_topology_t topology)
 
     if (topology->nb_levels == topology->nb_levels_allocated) {
       /* extend the arrays of levels */
-      topology->levels = realloc(topology->levels,
-				 2 * topology->nb_levels_allocated * sizeof(*topology->levels));
-      topology->level_nbobjects = realloc(topology->level_nbobjects,
-					  2 * topology->nb_levels_allocated * sizeof(*topology->level_nbobjects));
-      if (!topology->levels || !topology->level_nbobjects) {
+      void *tmplevels, *tmpnbobjs;
+      tmplevels = realloc(topology->levels,
+			  2 * topology->nb_levels_allocated * sizeof(*topology->levels));
+      tmpnbobjs = realloc(topology->level_nbobjects,
+			  2 * topology->nb_levels_allocated * sizeof(*topology->level_nbobjects));
+      if (!tmplevels || !tmpnbobjs) {
 	fprintf(stderr, "hwloc failed to realloc level arrays to %u\n", topology->nb_levels_allocated * 2);
 	free(objs);
 	free(taken_objs);
@@ -2506,6 +2507,8 @@ hwloc_connect_levels(hwloc_topology_t topology)
 	errno = ENOMEM;
 	return -1;
       }
+      topology->levels = tmplevels;
+      topology->level_nbobjects = tmpnbobjs;
       memset(topology->levels + topology->nb_levels_allocated,
 	     0, topology->nb_levels_allocated * sizeof(*topology->levels));
       memset(topology->level_nbobjects + topology->nb_levels_allocated,

commit cbcc1e59c741c589398b5b842a9bd1a1b2276faa

core: fix memory leaks in the case of level reconnect realloc failure
Introduced in c04eb5de4b5ed91e73740881c4e73c0651943cab

CID #1368961, #1368962

hwloc/topology.c
@@ -2501,6 +2501,14 @@ hwloc_connect_levels(hwloc_topology_t topology)
 			  2 * topology->nb_levels_allocated * sizeof(*topology->level_nbobjects));
       if (!tmplevels || !tmpnbobjs) {
 	fprintf(stderr, "hwloc failed to realloc level arrays to %u\n", topology->nb_levels_allocated * 2);
+
+	/* if one realloc succeeded, make sure the caller will free the new buffer */
+	if (tmplevels)
+	  topology->levels = tmplevels;
+	if (tmpnbobjs)
+	  topology->level_nbobjects = tmpnbobjs;
+	/* the realloc that failed left topology->level_foo untouched, will be freed by the caller */
+
 	free(objs);
 	free(taken_objs);
 	free(new_objs);