0ab0dea6fac3cabd387f48a810131837f31ef046
Brice Goglin <Brice.Goglin@inria.fr>
2017-10-27 16:54:51 +0200

    lstopo/draw: fix lstopoStyle
    Text would only work if Background2 was set, and the opposite.
    
    Cleanup things by using macros for style bits.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- utils/lstopo/lstopo.h
+++ utils/lstopo/lstopo.h
@@ -90,7 +90,11 @@ struct lstopo_obj_userdata {
 
   /* custom style */
   struct style style;
-  unsigned style_set; /* 0x1 if bg set, 0x2 for t, 0x4 for bg2, 0x8 for t2 */
+#define LSTOPO_STYLE_BG  0x1
+#define LSTOPO_STYLE_T   0x2
+#define LSTOPO_STYLE_BG2 0x4
+#define LSTOPO_STYLE_T2  0x8
+  unsigned style_set; /* OR'ed LSTOPO_STYLE_* */
 
   /* object size (including children if they are outside of it, not including borders) */
   unsigned width;
