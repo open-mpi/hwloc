open-mpi/hwloc

commit 6e4a00b961b6e2b0a5dd2ab20f10bb0f7c583431

core: restrict() must error out if removing all allowed PUs or NUMA nodes
We must keep at least one *allowed* PU and and one *allowed* NUMA node.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

hwloc/topology.c
@@ -3655,7 +3655,7 @@ hwloc_topology_restrict(struct hwloc_topology *topology, hwloc_const_cpuset_t cp
   }
 
   /* make sure we'll keep something in the topology */
-  if (!hwloc_bitmap_intersects(cpuset, topology->levels[0][0]->cpuset)) {
+  if (!hwloc_bitmap_intersects(cpuset, topology->levels[0][0]->allowed_cpuset)) {
     errno = EINVAL; /* easy failure, just don't touch the topology */
     return -1;
   }
@@ -3680,6 +3680,12 @@ hwloc_topology_restrict(struct hwloc_topology *topology, hwloc_const_cpuset_t cp
 	hwloc_bitmap_set(droppednodeset, node->os_index);
       node = node->next_cousin;
     } while (node);
+
+    /* check we're not removing all NUMA nodes */
+    if (hwloc_bitmap_isincluded(topology->levels[0][0]->allowed_nodeset, droppednodeset)) {
+      errno = EINVAL; /* easy failure, just don't touch the topology */
+      return -1;
+    }
   }
   /* remove nodeset if empty */
   if (!(flags & HWLOC_RESTRICT_FLAG_REMOVE_CPULESS)

tests/hwloc/hwloc_topology_restrict.c
@@ -145,6 +145,10 @@ int main(void)
   hwloc_bitmap_zero(cpuset);
   err = hwloc_topology_restrict(topology, cpuset, 0);
   assert(err < 0 && errno == EINVAL);
+  printf("restricting to unexisting PU:24, must fail\n");
+  hwloc_bitmap_only(cpuset, 24);
+  err = hwloc_topology_restrict(topology, cpuset, 0);
+  assert(err < 0 && errno == EINVAL);
   check(1, 3, 6, 24);
   check_distances(3, 6);
 

commit 8060631cbdac3d93314877543160dd6ba52eecfc

core: fix memory leaks on error during restrict()
Introduced in 6e4a00b961b6e2b0a5dd2ab20f10bb0f7c583431

CID#1424289 and CID#1424290

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

hwloc/topology.c
@@ -3667,6 +3667,8 @@ hwloc_topology_restrict(struct hwloc_topology *topology, hwloc_const_cpuset_t cp
     /* check we're not removing all NUMA nodes */
     if (hwloc_bitmap_isincluded(topology->allowed_nodeset, droppednodeset)) {
       errno = EINVAL; /* easy failure, just don't touch the topology */
+      hwloc_bitmap_free(droppedcpuset);
+      hwloc_bitmap_free(droppednodeset);
       return -1;
     }
   }