448f86109dc06300d5137e4c6a79cc96d71e49fe
Brice Goglin <Brice.Goglin@inria.fr>
2017-12-28 19:25:52 +0100

    tests/get_last_cpu_location: add many printf
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 5aa300a934be493dc57784aeac0db500c97630e4)

--- tests/hwloc_get_last_cpu_location.c
+++ tests/hwloc_get_last_cpu_location.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2011 inria.  All rights reserved.
+ * Copyright © 2011-2017 Inria.  All rights reserved.
  * Copyright © 2011 Université Bordeaux.  All rights reserved.
  * See COPYING in top-level directory.
  */
@@ -19,14 +19,17 @@ static int check(hwloc_const_cpuset_t set, int flags)
   hwloc_cpuset_t last;
   int ret;
 
+  printf("  binding\n");
   ret = hwloc_set_cpubind(topology, set, flags);
   if (ret)
     return 0;
 
+  printf("  getting cpu location\n");
   last = hwloc_bitmap_alloc();
   ret = hwloc_get_last_cpu_location(topology, last, flags);
   assert(!ret);
   assert(!hwloc_bitmap_iszero(last));
+  printf("  checking inclusion\n");
   assert(hwloc_bitmap_isincluded(last, set));
 
   hwloc_bitmap_free(last);
@@ -35,12 +38,18 @@ static int check(hwloc_const_cpuset_t set, int flags)
 
 static int checkall(hwloc_const_cpuset_t set)
 {
-  if (support->cpubind->get_thisthread_last_cpu_location)
+  if (support->cpubind->get_thisthread_last_cpu_location) {
+    printf(" with HWLOC_CPUBIND_THREAD...\n");
     check(set, HWLOC_CPUBIND_THREAD);
-  if (support->cpubind->get_thisproc_last_cpu_location)
+  }
+  if (support->cpubind->get_thisproc_last_cpu_location) {
+    printf(" with HWLOC_CPUBIND_PROCESS...\n");
     check(set, HWLOC_CPUBIND_PROCESS);
-  if (support->cpubind->get_thisthread_last_cpu_location || support->cpubind->get_thisproc_last_cpu_location)
+  }
+  if (support->cpubind->get_thisthread_last_cpu_location || support->cpubind->get_thisproc_last_cpu_location) {
+    printf(" with flags 0...\n");
     check(set, 0);
+  }
   return 0;
 }
 
@@ -55,18 +64,21 @@ int main(void)
   support = hwloc_topology_get_support(topology);
 
   /* check at top level */
+  printf("testing at top level\n");
   obj = hwloc_get_root_obj(topology);
   checkall(obj->cpuset);
 
   depth = hwloc_topology_get_depth(topology);
   /* check at intermediate level if it exists */
   if (depth >= 3) {
+    printf("testing at depth %d\n", (depth-1)/2);
     obj = NULL;
     while ((obj = hwloc_get_next_obj_by_depth(topology, (depth-1)/2, obj)) != NULL)
       checkall(obj->cpuset);
   }
 
   /* check at bottom level */
+  printf("testing at bottom level\n");
   obj = NULL;
   while ((obj = hwloc_get_next_obj_by_type(topology, HWLOC_OBJ_PU, obj)) != NULL)
     checkall(obj->cpuset);
