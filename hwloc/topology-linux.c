80863a7dec8f76ade5de1d82075a5e447776c95f
Brice Goglin <Brice.Goglin@inria.fr>
2018-01-05 11:47:59 +0100

    linuxio: only display pci/usb osdev when filtering to "ALL"
    USB keys, USB/thunderbolt docks, etc bring OS devices that
    are connected through USB behind PCI.
    By the default, the PCI->USB converter is filtered-out,
    hence we get OS devices directly attached to a PCI bridge.
    
    Disable those OSdev by default since they likely don't matter for locality.
    If PCIdevs are filtered to "ALL", we'll get the PCI-USB converters.
    If OSdevs are filtered to "ALL", we'll get those USB devices.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- hwloc/topology-linux.c
+++ hwloc/topology-linux.c
@@ -4653,6 +4653,7 @@ const struct hwloc_component hwloc_linux_component = {
  ***********************************/
 
 #define HWLOC_LINUXFS_FIND_OSDEV_FLAG_VIRTUAL (1<<0)
+#define HWLOC_LINUXFS_FIND_OSDEV_FLAG_USB (1<<1)
 
 static hwloc_obj_t
 hwloc_linuxfs_find_osdev_parent(struct hwloc_backend *backend, int root_fd,
@@ -4684,6 +4685,11 @@ hwloc_linuxfs_find_osdev_parent(struct hwloc_backend *backend, int root_fd,
 
   if (!(osdev_flags & HWLOC_LINUXFS_FIND_OSDEV_FLAG_VIRTUAL)) {
     if (strstr(path, "/virtual/"))
+      return NULL;
+  }
+
+  if (!(osdev_flags & HWLOC_LINUXFS_FIND_OSDEV_FLAG_USB)) {
+    if (strstr(path, "/usb"))
       return NULL;
   }
 
@@ -5827,6 +5833,9 @@ hwloc_look_linuxfs_io(struct hwloc_backend *backend)
     unsigned osdev_flags = 0;
     if (getenv("HWLOC_VIRTUAL_LINUX_OSDEV"))
       osdev_flags |= HWLOC_LINUXFS_FIND_OSDEV_FLAG_VIRTUAL;
+    if (ofilter == HWLOC_TYPE_FILTER_KEEP_ALL)
+      osdev_flags |= HWLOC_LINUXFS_FIND_OSDEV_FLAG_USB;
+
     hwloc_linuxfs_lookup_block_class(backend, osdev_flags);
     hwloc_linuxfs_lookup_net_class(backend, osdev_flags);
     hwloc_linuxfs_lookup_infiniband_class(backend, osdev_flags);
