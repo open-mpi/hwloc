open-mpi/hwloc

commit c44d913046a425d1129b154d8bf4e973219ea293

linux: add a heuristic for finding MCDRAM cache config without hwloc-dump-hwdata
When no hwloc-dump-hwdata is available to find KNL cluster and
memory modes, fallback to a heuristic based at looking the NUMA
nodes.

All commercial KNL/KNM have 16GB of MCDRAM, and there won't be
any new model in the future. Look at MCDRAM NUMA node sizes to
guess the cache size as the missing part of these 16GB.
Also guess the clustering modes from the number of nodes.
However we can't distinguish between all2all, quadrant and hemisphere.

Add the non-documented envvar HWLOC_KNL_HDH_FALLBACK to force (1)
or disable that fallback (0) in case we ever need it.

Disable this fallback (unless envvar==1) if we find no CPUless NUMA
nodes while the total size of other nodes is <=16GB (can be either
a Cache mode with very few DDR, or a no-DDR case in flat/hybrid mode).
Hence the fallback currently fails on 64intel64-fakeKNL-A2A-cache
which artificially got only 1GB of DDR.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

hwloc/topology-linux.c
@@ -2950,12 +2950,18 @@ struct knl_hwdata {
 /* Try to handle knl hwdata properties
  * Returns 0 on success and -1 otherwise */
 static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backend_data_s *data,
-							struct knl_hwdata *hwdata)
+							struct knl_hwdata *hwdata,
+							unsigned DDR_nbnodes,
+							unsigned long DDR_numa_size,
+							unsigned MCDRAM_nbnodes,
+							unsigned long MCDRAM_numa_size)
 {
   char *knl_cache_file;
   int version = 0;
   char buffer[512] = {0};
   char *data_beg = NULL;
+  char * fallback_env = getenv("HWLOC_KNL_HDH_FALLBACK");
+  int fallback = fallback_env ? atoi(fallback_env) : -1; /* by default, only fallback if needed */
 
   hwdata->memory_mode[0] = '\0';
   hwdata->cluster_mode[0] = '\0';
@@ -2964,14 +2970,19 @@ static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backe
   hwdata->mcdram_cache_inclusiveness = -1;
   hwdata->mcdram_cache_line_size = -1;
 
+  if (fallback == 1) {
+    hwloc_debug("KNL dumped hwdata ignored, forcing fallback\n");
+    goto fallback;
+  }
+
   if (asprintf(&knl_cache_file, "%s/knl_memoryside_cache", data->dumped_hwdata_dirname) < 0)
-    return -1;
+    goto fallback;
 
   hwloc_debug("Reading knl cache data from: %s\n", knl_cache_file);
   if (hwloc_read_path_by_length(knl_cache_file, buffer, sizeof(buffer), data->root_fd) < 0) {
     hwloc_debug("Unable to open KNL data file `%s' (%s)\n", knl_cache_file, strerror(errno));
     free(knl_cache_file);
-    return -1;
+    goto fallback;
   }
   free(knl_cache_file);
 
@@ -2980,7 +2991,7 @@ static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backe
   /* file must start with version information */
   if (sscanf(data_beg, "version: %d", &version) != 1) {
     fprintf(stderr, "Invalid knl_memoryside_cache header, expected \"version: <int>\".\n");
-    return -1;
+    goto fallback;
   }
 
   while (1) {
@@ -3038,6 +3049,64 @@ static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backe
 		hwdata->mcdram_cache_inclusiveness);
     hwdata->mcdram_cache_size = -1; /* mark cache as invalid */
   }
+
+  return 0;
+
+ fallback:
+  if (fallback == 0) {
+    hwloc_debug("KNL hwdata fallback disabled\n");
+    return -1;
+  }
+
+  hwloc_debug("Falling back to a heuristic\n");
+
+  /* there can be 0 MCDRAM_nbnodes, but we must have at least one DDR node (not cpuless) */
+  assert(DDR_nbnodes);
+  /* there are either no MCDRAM nodes, or as many as DDR nodes */
+  assert(!MCDRAM_nbnodes || MCDRAM_nbnodes == DDR_nbnodes);
+
+  if (!MCDRAM_nbnodes && DDR_numa_size <= 16UL*1024*1024*1024) {
+    /* We only found DDR numa nodes, but they are <=16GB.
+     * It could be a DDR-less KNL where numa nodes are actually MCDRAM, we can't know for sure.
+     * Both cases are unlikely, disable the heuristic for now.
+     *
+     * In theory we could check if DDR_numa_size == 8/12/16GB exactly (amount of MCDRAM numa size in H50/H25/Flat modes),
+     * but that's never the case since some kilobytes are always stolen by the system.
+     */
+    hwloc_debug("Cannot guess if MCDRAM is in Cache or if the node is DDR-less (total NUMA node size %ld)\n",
+		DDR_numa_size);
+    return -1;
+  }
+
+  /* all commercial KNL/KNM have 16GB of MCDRAM */
+  unsigned long total_cache_size = 16UL*1024*1024*1024 - MCDRAM_numa_size;
+
+  if (!MCDRAM_nbnodes) {
+    strcpy(hwdata->memory_mode, "Cache");
+  } else {
+    if (!total_cache_size)
+      strcpy(hwdata->memory_mode, "Flat");
+    else if (total_cache_size == 8UL*1024*1024*1024)
+      strcpy(hwdata->memory_mode, "Hybrid50");
+    else if (total_cache_size == 4UL*1024*1024*1024)
+      strcpy(hwdata->memory_mode, "Hybrid25");
+    else
+      fprintf(stderr, "Unexpected KNL MCDRAM cache size %lu\n", total_cache_size);
+  }
+  if (DDR_nbnodes == 4) {
+    strcpy(hwdata->cluster_mode, "SNC4");
+  } else if (DDR_nbnodes == 2) {
+    strcpy(hwdata->cluster_mode, "SNC2");
+  } else if (DDR_nbnodes == 1) {
+    /* either Quadrant, All2ALL or Hemisphere */
+  } else {
+    fprintf(stderr, "Unexpected number of KNL non-MCDRAM NUMA nodes %d\n", DDR_nbnodes);
+  }
+
+  hwdata->mcdram_cache_size = total_cache_size/DDR_nbnodes;
+  hwdata->mcdram_cache_associativity = 1;
+  hwdata->mcdram_cache_inclusiveness = 1;
+  hwdata->mcdram_cache_line_size = 64;
 
   return 0;
 }
@@ -3212,8 +3281,26 @@ look_sysfsnode(struct hwloc_topology *topology,
 	int noquirk = (env && !atoi(env)) || !distances || !hwloc_filter_check_keep_object_type(topology, HWLOC_OBJ_GROUP);
 	int mscache;
 	unsigned j, closest;
+	unsigned long MCDRAM_numa_size, DDR_numa_size;
+	unsigned MCDRAM_nbnodes, DDR_nbnodes;
 
-	hwloc_linux_try_handle_knl_hwdata_properties(data, &knl_hwdata);
+	DDR_numa_size = 0;
+	DDR_nbnodes = 0;
+	MCDRAM_numa_size = 0;
+	MCDRAM_nbnodes = 0;
+	for(i=0; i<nbnodes; i++)
+	  if (hwloc_bitmap_iszero(nodes[i]->cpuset)) {
+	    MCDRAM_numa_size += nodes[i]->attr->numanode.local_memory;
+	    MCDRAM_nbnodes++;
+	  } else {
+	    DDR_numa_size += nodes[i]->attr->numanode.local_memory;
+	    DDR_nbnodes++;
+	  }
+	assert(DDR_nbnodes + MCDRAM_nbnodes == nbnodes);
+
+	hwloc_linux_try_handle_knl_hwdata_properties(data, &knl_hwdata,
+						     DDR_nbnodes, DDR_numa_size,
+						     MCDRAM_nbnodes, MCDRAM_numa_size);
 	mscache = knl_hwdata.mcdram_cache_size > 0 && hwloc_filter_check_keep_object_type(topology, HWLOC_OBJ_L3CACHE);
 
 	if (knl_hwdata.cluster_mode[0])

doc/hwloc.doxy
@@ -1157,6 +1157,9 @@ following environment variables.
   may be useful in the doc for debugging?
  HWLOC_KNL_NUMA_QUIRK
   disables the KNL NUMA Cluster quirk in the linux backend
+ HWLOC_KNL_HDH_FALLBACK
+  if 1, ignore KNL info from hwloc-dump-hwdata and fallback to heuristic
+  if 0, never fallback to the hardwired heuristic, useful if the heuristic is wrong
  HWLOC_DEBUG_CHECK
   runs sanity checks during discovery, as if \--enable-debug was passed but
   without debug messages
@@ -1708,8 +1711,10 @@ hwloc failed to discover any package.
 </dd>
 <dt>MemoryMode, ClusterMode (topology root, Machine object)</dt>
 <dd>
-Intel Knights Landing configuration modes, only available if
-hwloc-dump-hwdata was used (see \ref faq_knl_dump).
+Intel Knights Landing configuration modes.
+Available if hwloc-dump-hwdata was used (see \ref faq_knl_dump)
+or if hwloc managed to guess them from the NUMA configuration.
+
 The memory mode may be <em>Cache</em>, <em>Flat</em>,
 <em>Hybrid50</em> (half the MCDRAM is used as a cache)
 or <em>Hybrid25</em> (25% of MCDRAM as cache).
@@ -3496,6 +3501,7 @@ as either memory or a memory-side cache (currently reported as a L3 cache by hwl
 There are also several clustering modes that significantly affect the memory organization
 (see \ref faq_knl_numa for more information about these modes).
 Details about these are currently only available to privileged users.
+Without them, hwloc relies on a heuristic for guessing the modes.
 
 The hwloc-dump-hwdata utility may be used to dump this privileged binary information
 into human-readable and world-accessible files that the hwloc library will later load.

commit e83e5d8a93951436013b7f35453bf03cc035fd32

linux: fix some printf signedness issues
Introduced in commit c44d913046a425d1129b154d8bf4e973219ea293

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

hwloc/topology-linux.c
@@ -3073,7 +3073,7 @@ static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backe
      * In theory we could check if DDR_numa_size == 8/12/16GB exactly (amount of MCDRAM numa size in H50/H25/Flat modes),
      * but that's never the case since some kilobytes are always stolen by the system.
      */
-    hwloc_debug("Cannot guess if MCDRAM is in Cache or if the node is DDR-less (total NUMA node size %ld)\n",
+    hwloc_debug("Cannot guess if MCDRAM is in Cache or if the node is DDR-less (total NUMA node size %lu)\n",
 		DDR_numa_size);
     return -1;
   }
@@ -3100,7 +3100,7 @@ static int hwloc_linux_try_handle_knl_hwdata_properties(struct hwloc_linux_backe
   } else if (DDR_nbnodes == 1) {
     /* either Quadrant, All2ALL or Hemisphere */
   } else {
-    fprintf(stderr, "Unexpected number of KNL non-MCDRAM NUMA nodes %d\n", DDR_nbnodes);
+    fprintf(stderr, "Unexpected number of KNL non-MCDRAM NUMA nodes %u\n", DDR_nbnodes);
   }
 
   hwdata->mcdram_cache_size = total_cache_size/DDR_nbnodes;