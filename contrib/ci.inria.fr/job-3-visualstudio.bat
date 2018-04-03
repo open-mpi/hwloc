ebda526556774f7af6523e90586ac7304898c030
Brice Goglin <Brice.Goglin@inria.fr>
2018-04-03 15:57:38 +0200

    contrib/ci.inria.fr: use slave-specific config file for specific PATH on windows
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/ci.inria.fr/job-3-visualstudio.bat
+++ contrib/ci.inria.fr/job-3-visualstudio.bat
@@ -1,9 +1,9 @@
 REM
-REM  Copyright © 2012-2017 Inria.  All rights reserved.
+REM  Copyright © 2012-2018 Inria.  All rights reserved.
 REM  See COPYING in top-level directory.
 REM
 
-set PATH=%PATH%;C:\msys64\bin;C:\msys64\usr\bin
+call %JENKINS_CONFIG_DIR%\\ciprofile.bat
 
 REM  remove everything but the last 10 builds
 sh -c "rm -rf $(ls | grep -v ^hwloc- | grep -v ^job-) || true"
