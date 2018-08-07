30d071e431a59d914853a8f4988f0a60afb0b58b
Jeff Squyres <jsquyres@cisco.com>
2018-08-07 10:27:55 -0700

    Miscellaneous compiler warning fixes
    Signed-off-by: Jeff Squyres <jsquyres@cisco.com>
    (cherry picked from commit 74420130cfbc7501cb470bfa16db534404b6efb3)

--- utils/hwloc/hwloc-bind.c
+++ utils/hwloc/hwloc-bind.c
@@ -2,7 +2,7 @@
  * Copyright © 2009 CNRS
  * Copyright © 2009-2018 Inria.  All rights reserved.
  * Copyright © 2009-2010, 2012 Université Bordeaux
- * Copyright © 2009 Cisco Systems, Inc.  All rights reserved.
+ * Copyright © 2009-2018 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -66,7 +66,7 @@ int main(int argc, char *argv[])
 {
   hwloc_topology_t topology;
   int loaded = 0;
-  int depth;
+  int depth = -1;
   hwloc_bitmap_t cpubind_set, membind_set;
   int got_cpubind = 0, got_membind = 0;
   int working_on_cpubind = 1; /* membind if 0 */
