28efc6966bb1d0710f8f69581c4d58aa4322d1e3
Brice Goglin <Brice.Goglin@inria.fr>
2018-09-02 14:52:33 +0200

    VERSION: 1.11.11 released, doing .12rc1 now
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/windows/private_config.h
+++ contrib/windows/private_config.h
@@ -665,7 +665,7 @@
 
 
 /* Version number of package */
-#define HWLOC_VERSION "1.11.11"
+#define HWLOC_VERSION "1.11.12"
 #define VERSION HWLOC_VERSION
 
 /* Define to 1 if the X Window System is missing or not being used. */
