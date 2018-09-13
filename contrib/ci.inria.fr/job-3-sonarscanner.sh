0c2121bc4bc6f4b37b3fb9ee29a1631ea2df754d
Brice Goglin <Brice.Goglin@inria.fr>
2018-09-13 09:49:17 +0100

    ci.inria.fr: fix sonarqube filters for C warnings
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/ci.inria.fr/job-3-sonarscanner.sh
+++ contrib/ci.inria.fr/job-3-sonarscanner.sh
@@ -152,22 +152,22 @@ sonar.c.rats.reportPath=${RATS_XMLS}
 sonar.c.valgrind.reportPath=${VALGRIND_XMLS}
 sonar.issue.ignore.multicriteria=e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14
 # Complete the task associated to this TODO comment.
-sonar.issue.ignore.multicriteria.e1.ruleKey=cxx:TodoTagPresence
+sonar.issue.ignore.multicriteria.e1.ruleKey=c:TodoTagPresence
 sonar.issue.ignore.multicriteria.e1.resourceKey=**
 # Missing curly brace.
-sonar.issue.ignore.multicriteria.e2.ruleKey=cxx:MissingCurlyBraces
+sonar.issue.ignore.multicriteria.e2.ruleKey=c:MissingCurlyBraces
 sonar.issue.ignore.multicriteria.e2.resourceKey=**
 # Extract this magic number '3' into a constant, variable declaration or an enum.
-sonar.issue.ignore.multicriteria.e3.ruleKey=cxx:MagicNumber
+sonar.issue.ignore.multicriteria.e3.ruleKey=c:MagicNumber
 sonar.issue.ignore.multicriteria.e3.resourceKey=**
 # Undocumented API: hwloc_noos_component
-sonar.issue.ignore.multicriteria.e4.ruleKey=cxx:UndocumentedApi
+sonar.issue.ignore.multicriteria.e4.ruleKey=c:UndocumentedApi
 sonar.issue.ignore.multicriteria.e4.resourceKey=**
 # At most one statement is allowed per line, but 2 statements were found on this line.
-sonar.issue.ignore.multicriteria.e5.ruleKey=cxx:TooManyStatementsPerLine
+sonar.issue.ignore.multicriteria.e5.ruleKey=c:TooManyStatementsPerLine
 sonar.issue.ignore.multicriteria.e5.resourceKey=**
 # Split this 166 characters long line (which is greater than 160 authorized).
-sonar.issue.ignore.multicriteria.e6.ruleKey=cxx:TooLongLine
+sonar.issue.ignore.multicriteria.e6.ruleKey=c:TooLongLine
 sonar.issue.ignore.multicriteria.e6.resourceKey=**
 # 196 more comment lines need to be written to reach the minimum threshold of 25.0% comment density.
 # BUG: doesn't seem to match properly, even with * or so on instead of ++
