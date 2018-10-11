be5a7dd5b3f23079bae3886e464091f506bb6acd
Valentin Hoyet <Valentin.Hoyet@inria.fr>
2018-10-11 13:22:19 +0200

    contrib/ci.inria.fr: add cygwin job
    Signed-off-by: Valentin Hoyet <valentin.hoyet@inria.fr>

---
+++ contrib/ci.inria.fr/job-3-cygwin.bat
@@ -0,0 +1,12 @@
+REM
+REM  Copyright © 2012-2018 Inria.  All rights reserved.
+REM  See COPYING in top-level directory.
+REM
+
+call %JENKINS_CONFIG_DIR%\\ciprofile.bat
+
+sh -c "chmod 755 job-3-cygwin.sh"
+sh -c "./job-3-cygwin.sh %1"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+exit /b 0
