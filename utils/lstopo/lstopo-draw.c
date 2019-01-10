open-mpi/hwloc

commit d357cf058547d583318a024c77c7b869a06eb47e

lstopo/draw: remove FONTGRIDSIZE
We already have checks for the number of lines to display,
that depends on notext.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

utils/lstopo/lstopo-draw.c
@@ -556,9 +556,6 @@ place_children(struct lstopo_output *loutput, hwloc_obj_t parent,
  * Drawing children
  */
 
-/* additional gridsize when fontsize > 0 */
-#define FONTGRIDSIZE (fontsize ? gridsize : 0)
-
 static void
 draw__children(struct lstopo_output *loutput, hwloc_obj_t parent,
 	       struct lstopo_children_position *children,
@@ -985,8 +982,8 @@ pci_device_draw(struct lstopo_output *loutput, hwloc_obj_t level, unsigned depth
     lud->width = gridsize + overlaidoffset;
     lud->height = gridsize + overlaidoffset;
     if (lud->ntext > 0) {
-      lud->width += lud->textwidth + FONTGRIDSIZE;
-      lud->height += fontsize + FONTGRIDSIZE;
+      lud->width += lud->textwidth + gridsize;
+      lud->height += fontsize + gridsize;
     }
     place_children(loutput, level,
 		   gridsize, lud->height);
@@ -1089,8 +1086,8 @@ cache_draw(struct lstopo_output *loutput, hwloc_obj_t level, unsigned depth, uns
     lud->width = gridsize;
     lud->height = gridsize;
     if (lud->ntext > 0) {
-      lud->width += lud->textwidth + FONTGRIDSIZE;
-      lud->height += fontsize + FONTGRIDSIZE;
+      lud->width += lud->textwidth + fontsize;
+      lud->height += fontsize + gridsize;
     }
     place_children(loutput, level,
 		   0, lud->height + gridsize);
@@ -1110,7 +1107,7 @@ cache_draw(struct lstopo_output *loutput, hwloc_obj_t level, unsigned depth, uns
      */
     myheight = gridsize;
     if (lud->ntext > 0)
-      myheight += fontsize + FONTGRIDSIZE;
+      myheight += fontsize + gridsize;
 
     if (lud->above_children.kinds) {
       /* display above_children even above the cache itself */
@@ -1141,8 +1138,8 @@ normal_draw(struct lstopo_output *loutput, hwloc_obj_t level, unsigned depth, un
     lud->width = gridsize;
     lud->height = gridsize;
     if (lud->ntext > 0) {
-      lud->width += lud->textwidth + FONTGRIDSIZE;
-      lud->height += (fontsize + FONTGRIDSIZE) * lud->ntext;
+      lud->width += lud->textwidth + gridsize;
+      lud->height += (fontsize + gridsize) * lud->ntext;
     }
     place_children(loutput, level,
 		   gridsize, lud->height);

commit 876628544ba1aeee538018d72008dbf176684a5c

lstopo/draw: fix cache box width
Fix a typo introduced in d357cf058547d583318a024c77c7b869a06eb47e

Basically only mattered when changing gridsize/fontsize.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

utils/lstopo/lstopo-draw.c
@@ -1,6 +1,6 @@
 /*
  * Copyright © 2009 CNRS
- * Copyright © 2009-2018 Inria.  All rights reserved.
+ * Copyright © 2009-2019 Inria.  All rights reserved.
  * Copyright © 2009-2013, 2015 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -1158,7 +1158,7 @@ cache_draw(struct lstopo_output *loutput, hwloc_obj_t level, unsigned depth, uns
     lud->width = gridsize;
     lud->height = gridsize;
     if (lud->ntext > 0) {
-      lud->width += lud->textwidth + fontsize;
+      lud->width += lud->textwidth + gridsize;
       lud->height += fontsize + gridsize;
     }
     place_children(loutput, level,