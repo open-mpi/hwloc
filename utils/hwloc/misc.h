a49c4f7c71028c0b95ae0d4072daa6c83768e1a7
Brice Goglin <Brice.Goglin@inria.fr>
2019-05-04 21:28:55 +0200

    fsroot+pci: allow the pci backend to add vendor/device ids on modified fsroot
    Modified fsroot cannot use the pciaccess backend for getting PCI devices,
    but it may still annotate sysfs-got devices with vendor/device names later.
    
    Hence changing fsroot shouldn't disable pci entirely anymore.
    
    However linux tests must re-disable it, otherwise the output would change
    depending on whether pciaccess is available, and with the version of the
    pci.ids database.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- utils/hwloc/misc.h
+++ utils/hwloc/misc.h
@@ -257,7 +257,7 @@ hwloc_utils_enable_input_format(struct hwloc_topology *topology,
     if (env)
       fprintf(stderr, "Cannot force linux and linuxio components first because HWLOC_COMPONENTS environment variable is already set to %s.\n", env);
     else
-      putenv((char *) "HWLOC_COMPONENTS=linux,linuxio,stop");
+      putenv((char *) "HWLOC_COMPONENTS=linux,linuxio,pci,stop");
 #else /* HWLOC_LINUX_SYS */
     fprintf(stderr, "This installation of hwloc does not support changing the file-system root, sorry.\n");
     exit(EXIT_FAILURE);
