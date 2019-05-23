fc59fc5a88b6cba4e422c85001a17552d0de1b41
Nicolas Denoyelle <ndenoyelle@anl.gov>
2019-05-23 14:50:30 -0500

    remove now useless functions
    Signed-off-by: Nicolas Denoyelle <ndenoyelle@anl.gov>

--- utils/hwloc/hwloc-tbind-obj.c
+++ utils/hwloc/hwloc-tbind-obj.c
@@ -59,63 +59,6 @@ error:
 	return NULL;
 }
 
-hwloc_obj_t hwloc_obj_from_string(hwloc_topology_t topology, const char *str)
-{
-	hwloc_obj_t obj;
-	hwloc_obj_type_t type;
-	int id;
-	char *obj_str, *save_ptr, *type_str, *id_str;
-
-	obj_str = strdup(str);
-	save_ptr = obj_str;
-	if (obj_str == NULL) {
-		errno = ENOMEM;
-		return NULL;
-	}
-
-	type_str = strtok(obj_str, ":");
-	if (type_str == NULL)
-		goto out_einval;
-
-	if (hwloc_type_sscanf(type_str, &type, NULL, 0) < 0)
-		goto out_einval;
-
-	id_str = strtok(NULL, ":");
-
-	if (id_str == NULL)
-		goto out_einval;
-
-	id = atoi(id_str);
-	if (id < 0 || id >= hwloc_get_nbobjs_by_type(topology, type))
-		goto out_einval;
-
-	obj = hwloc_get_obj_by_type(topology, type, atoi(id_str));
-	if (obj == NULL)
-		goto out_einval;
-
-	free(save_ptr);
-	return obj;
-
-out_einval:
-	free(save_ptr);
-	errno = EINVAL;
-	return NULL;
-}
-
-int hwloc_topology_restrict_obj(hwloc_topology_t topology, hwloc_obj_t obj)
-{
-	if (hwloc_bitmap_weight(obj->cpuset) > 0) {
-		return hwloc_topology_restrict(topology, obj->cpuset,
-					       HWLOC_RESTRICT_FLAG_REMOVE_CPULESS);
-	} else if (hwloc_bitmap_weight(obj->nodeset) > 0) {
-		return hwloc_topology_restrict(topology, obj->nodeset,
-					       HWLOC_RESTRICT_FLAG_BYNODESET |
-					       HWLOC_RESTRICT_FLAG_REMOVE_MEMLESS);
-	}
-	errno = EINVAL;
-	return -1;
-}
-
 /***************************************************************************/
 >>>>>>> a1491427... move hwloc-tbind outside its directory
 /*                              enum  structure                            */
