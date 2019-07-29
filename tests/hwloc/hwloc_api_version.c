00aeac91c730293d3b540f5866a84649bd62e31c
Brice Goglin <Brice.Goglin@inria.fr>
2019-07-29 11:12:40 +0200

    tests: add a basic test for static/dynamic api version
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

---
+++ tests/hwloc/hwloc_api_version.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2019 Inria.  All rights reserved.
+ * See COPYING in top-level directory.
+ */
+
+#include "hwloc.h"
+
+#include <printf.h>
+#include <assert.h>
+
+int main(void)
+{
+  unsigned static_version = HWLOC_API_VERSION;
+  unsigned dynamic_version = hwloc_get_api_version();
+  assert(static_version == dynamic_version);
+  return 0;
+}
