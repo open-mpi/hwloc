7392faa97ed3cc0c7cd0ae39baed81c1ecc495d7
Brice Goglin <Brice.Goglin@inria.fr>
2019-10-28 14:39:27 +0100

    lstopo/windows: draw the white background when we really start drawing
    It worked when done earlier (during PREPARE),
    but it would break if we resize the window in between (upcoming changes).
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 742be690004e2bad17a01db7ea6b372d030e59f2)

--- utils/lstopo/lstopo-windows.c
+++ utils/lstopo/lstopo-windows.c
@@ -156,12 +156,12 @@ WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
       font = CreateFont(loutput->fontsize, 0, 0, 0, 0, FALSE, FALSE, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, CLIP_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH, NULL);
       SelectObject(the_output.ps.hdc, (HGDIOBJ) font);
       SetBkMode(the_output.ps.hdc, TRANSPARENT);
-      windows_box(loutput, &white, 0, 0, win_width, 0, win_height, NULL, 0);
       loutput->drawing = LSTOPO_DRAWING_PREPARE;
       output_draw(loutput);
       the_width = loutput->width;
       the_height = loutput->height;
       loutput->drawing = LSTOPO_DRAWING_DRAW;
+      windows_box(loutput, &white, 0, 0, win_width, 0, win_height, NULL, 0);
       output_draw(loutput);
       DeleteObject(font);
       EndPaint(hwnd, &the_output.ps);
