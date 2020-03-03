open-mpi/hwloc

commit 777671f547b1b3d050b89a730a99c9a9b505f598

configure: add _HWLOC_CHECK_GCC_OPTION()
To check whether an gcc option is supported

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

config/hwloc_check_vendor.m4
@@ -11,7 +11,7 @@ dnl                         University of Stuttgart.  All rights reserved.
 dnl Copyright © 2004-2005 The Regents of the University of California.
 dnl                         All rights reserved.
 dnl Copyright © 2011      Cisco Systems, Inc.  All rights reserved.
-dnl Copyright © 2015 Inria.  All rights reserved.
+dnl Copyright © 2015-2020 Inria.  All rights reserved.
 dnl $COPYRIGHT$
 dnl
 dnl Additional copyrights may follow
@@ -243,4 +243,18 @@ AC_DEFUN([_HWLOC_CHECK_COMPILER_VENDOR], [
 
     $1="$hwloc_check_compiler_vendor_result"
     unset hwloc_check_compiler_vendor_result
+])
+
+# _HWLOC_CHECK_GCC_OPTION([option], [variable to append the option to])
+# ----------------------------------------------
+# Run gcc to determine if option is supported.
+AC_DEFUN([_HWLOC_CHECK_GCC_OPTION], [
+    tmp_save_CFLAGS="$CFLAGS"
+    CFLAGS="$1"
+    AC_MSG_CHECKING([if gcc supports $1])
+    AC_COMPILE_IFELSE([AC_LANG_PROGRAM([[int i;]])],
+		      [AC_MSG_RESULT(yes)
+		       $2="$$2 $1"],
+		      [AC_MSG_RESULT(no)])
+    CFLAGS="$tmp_save_LDFLAGS"
 ])

commit 6854da26cf04a7bfe34dc77aa1b50c25ac201a86

configure: properly restore CFLAGS after checking GCC options
Bug introduced in commit 777671f547b1b3d050b89a730a99c9a9b505f598
  configure: add _HWLOC_CHECK_GCC_OPTION()

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

config/hwloc_check_vendor.m4
@@ -257,5 +257,5 @@ AC_DEFUN([_HWLOC_CHECK_GCC_OPTION], [
 		       $2="$$2 $1"
 		       $3],
 		      [AC_MSG_RESULT(no)])
-    CFLAGS="$tmp_save_LDFLAGS"
+    CFLAGS="$tmp_save_CFLAGS"
 ])