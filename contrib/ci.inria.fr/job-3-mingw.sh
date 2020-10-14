916163b0baede072ddf6425b864b2cdfb3b0b344
Brice Goglin <Brice.Goglin@inria.fr>
2020-10-14 15:27:38 +0200

    ci.inria.fr: modernize MinGW builds
    * Move ci.inria.fr specific config to the slave profile
      so that we don't explicitly rely on a precis msys2 installation.
    * Split 32bits and 64bits builds
      + Check that we're indeed running in the right msys/mingw 32/64 shell.
    * Check that we found MSVC lib.exe early.
    * Remove many options in the job, they were mostly useful
      when debugging the initial script itself.
    
    New node labels mean we are switching from Win7 to Win10
    with a recent MSYS2/MinGW-W64 install.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 262ea5d4e1e0ad1de30a374651055aa9eb998422,
     including mingw parts of 777e69422278bc182d647d3d9faa29d0c240a303)

--- contrib/ci.inria.fr/job-3-mingw.sh
+++ contrib/ci.inria.fr/job-3-mingw.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2012-2018 Inria.  All rights reserved.
+# Copyright © 2012-2020 Inria.  All rights reserved.
 # See COPYING in top-level directory.
 #
 
@@ -13,169 +13,75 @@ echo "############################"
 set -e
 set -x
 
-dotar=1
-dokeeptar=0
-doconf=1
-dobuild32=1
-dobuild64=1
-docheck=1
-doinstall=1
-confopts=
+tarball="$1"
+arch="$2"
 
-while test $# -gt 0; do
-  if test "$1" = "--no-tar"; then
-    dotar=0
-  else if test "$1" = "--keep-tar"; then
-    dokeeptar=1
-  else if test "$1" = "--no-conf"; then
-    doconf=0
-  else if test "$1" = "--debug"; then
-    confopts="$confopts --enable-debug"
-  else if test "$1" = "--no-32"; then
-    dobuild32=0
-  else if test "$1" = "--no-64"; then
-    dobuild64=0
-  else if test "$1" = "--no-check"; then
-    docheck=0
-  else if test "$1" = "--no-install"; then
-    doinstall=0
-  else if test "$1" = "--help" -o "$1" = "-h"; then
-    echo "  --no-tar      Use current directory instead of latest hwloc-*.tar.gz"
-    echo "  --keep-tar    Don't delete tarball after unpacking"
-    echo "  --no-conf     Don't reconfigure tarball"
-    echo "  --debug       Enable debug"
-    echo "  --no-32       Don't build in 32bits"
-    echo "  --no-64       Don't build in 64bits"
-    echo "  --no-check    Don't run make check"
-    echo "  --no-install  Don't install"
-    echo "  --help        Show this help"
-    exit 0
-  else
-    break
-  fi fi fi fi fi fi fi fi fi
-  shift
-done
+if test "x$arch" = "x32"; then
+  # check the mingw shell
+  if test "x$MINGW_CHOST" != xi686-w64-mingw32; then
+    echo "MINGW_CHOST is $MINGW_CHOST instead of i686-w64-mingw32."
+    exit 1
+  fi
+  # for MS 'lib' program in dolib.c
+  export PATH=$PATH:$MSLIB32_PATH
+else if test "x$arch" = "x64"; then
+  # check the mingw shell
+  if test "x$MINGW_CHOST" != xx86_64-w64-mingw32; then
+    echo "MINGW_CHOST is $MINGW_CHOST instead of x86_64-w64-mingw32."
+    exit 1
+  fi
+  # for MS 'lib' program in dolib.c
+  export PATH=$PATH:$MSLIB64_PATH
+else
+  echo "Architecture parameter must be 32 or 64."
+  exit 1
+fi
+fi
 
-oldPWD=$PWD
-oldPATH=$PATH
+# check that we have a lib.exe
+if ! which lib.exe ; then
+  echo "Couldn't find MSVC lib.exe"
+  exit 1
+fi
 
-# remove previous artifacts so that they don't exported again by this build
+# remove previous artifacts so that they aren't exported again by this build
 rm -f hwloc-win*-build-*.zip || true
 
-if test x$dotar = x1; then
-  # extract the tarball
-  tarball="$1"
-  if ! test -f "$tarball"; then
-    echo "Invalid tarball parameter."
-    exit 0
-  fi
-  basename=$(basename $tarball .tar.gz)
-  version=$(echo $basename | cut -d- -f2-)
-  test -d $basename && chmod -R u+rwX $basename && rm -rf $basename
-  tar xfz $tarball
-  if test x$dokeeptar = x0; then
-    rm $tarball
-  fi
-else
-  basename=./
-  version=custom
+# extract the tarball
+if ! test -f "$tarball"; then
+  echo "Invalid tarball parameter."
+  exit 1
+fi
+basename=$(basename $tarball .tar.gz)
+version=$(echo $basename | cut -d- -f2-)
+test -d $basename && chmod -R u+rwX $basename && rm -rf $basename
+tar xfz $tarball
+
+mkdir ${basename}/build || true
+cd ${basename}/build
+
+winball=hwloc-win${arch}-build-${version}
+prefix=${PWD}/../${winball}
+../configure --prefix=$prefix --enable-static CC="gcc -static-libgcc" $confopts
+
+make
+
+if test x$NO_CHECK != xtrue; then
+  make check
 fi
 
-if test x$dobuild32 = x1; then
+make install
+winball=$(basename $(head config.log | sed -r -n -e 's/.*--prefix=([^ ]+).*/\1/p'))
+cd ..
 
-  echo "*** Switching to MinGW32 ***"
-  set +xe # cannot keep set -e while sourcing /etc/profile
-  MSYSTEM=MINGW32 . /etc/profile || true
-  set -xe
-  cd $oldPWD
+zip -r ../${winball}.zip ${winball}
+test -f ${winball}/lib/libhwloc.lib || false
 
-  # for MS 'lib' program in dolib.c
-  export PATH=$PATH:$MSLIB_PATH
+build/utils/lstopo/lstopo-no-graphics -v
+build/utils/hwloc/hwloc-info --support
 
-  mkdir ${basename}/build32 || true
-  cd ${basename}/build32
+cd ..
 
-  if test x$doconf = x1; then
-    winball=hwloc-win32-build-${version}
-    prefix=${PWD}/../${winball}
-    ../configure --prefix=$prefix --enable-static CC="gcc -static-libgcc" $confopts
-  fi
-
-  make
-
-  if test x$docheck = x1; then
-    make check
-  fi
-
-  if test x$doinstall = x1; then
-    make install
-    #make install-winball || true # not needed anymore in v1.7+
-    winball=$(basename $(head config.log | sed -r -n -e 's/.*--prefix=([^ ]+).*/\1/p'))
-    cd ..
-
-    zip -r ../${winball}.zip ${winball}
-    test -f ${winball}/lib/libhwloc.lib || false
-  else
-    cd ..
-  fi
-
-  build32/utils/lstopo/lstopo-no-graphics -v
-  build32/utils/hwloc/hwloc-info --support
-
-  if test x$dotar = x1; then
-    cd ..
-  fi
-
-fi
-
-
-if test x$dobuild64 = x1; then
-
-  echo "*** Switching to MinGW64 ***"
-  set +xe # cannot keep set -e while sourcing /etc/profile
-  MSYSTEM=MINGW64 . /etc/profile || true
-  set -xe
-  cd $oldPWD
-
-  # for MS 'lib' program in dolib.c
-  export PATH=$PATH:$MSLIB_PATH
-
-  mkdir ${basename}/build64 || true
-  cd ${basename}/build64
-
-  if test x$doconf = x1; then
-    winball=hwloc-win64-build-${version}
-    prefix=${PWD}/../${winball}
-    ../configure --prefix=$prefix --enable-static CC="gcc -static-libgcc" $confopts
-  fi
-
-  make
-
-  if test x$docheck = x1; then
-    make check
-  fi
-
-  if test x$doinstall = x1; then
-    make install
-    #make install-winball || true # not needed anymore in v1.7+
-    winball=$(basename $(head config.log | sed -r -n -e 's/.*--prefix=([^ ]+).*/\1/p'))
-    cd ..
-
-    zip -r ../${winball}.zip ${winball}
-    test -f ${winball}/lib/libhwloc.lib || false
-  else
-    cd ..
-  fi
-
-  build64/utils/lstopo/lstopo-no-graphics -v
-  build64/utils/hwloc/hwloc-info --support
-
-  if test x$dotar = x1; then
-    cd ..
-  fi
-
-fi
-
-PATH=$oldPATH
+rm -f $tarball
 
 exit 0
