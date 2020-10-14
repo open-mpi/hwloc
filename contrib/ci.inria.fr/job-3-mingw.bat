56c0b14558b96e571dd196d95c2f4c121029ad2a
Brice Goglin <Brice.Goglin@inria.fr>
2020-10-14 15:27:38 +0200

    ci.inria.fr: modernize MinGW builds
    * Move ci.inria.fr specific config to the slave profile
      so that we don't explicitly rely on a precis msys2 installation.
    * Split 32bits and 64bits builds
      + Check that we're indeed running in the right msys/mingw 32/64 shell.
    * Check that we found MSVC lib.exe early.
    * Remove many options in the job, they were mostly useful
      when debugging the initial script itself.
    
    New node labels mean we are switching from Win7 to Win10
    with a recent MSYS2/MinGW-W64 install.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 262ea5d4e1e0ad1de30a374651055aa9eb998422,
     including mingw parts of 777e69422278bc182d647d3d9faa29d0c240a303)

--- contrib/ci.inria.fr/job-3-mingw.bat
+++ contrib/ci.inria.fr/job-3-mingw.bat
@@ -1,12 +1,25 @@
 REM
-REM  Copyright © 2012-2018 Inria.  All rights reserved.
+REM  Copyright © 2012-2020 Inria.  All rights reserved.
 REM  See COPYING in top-level directory.
 REM
 
 call %JENKINS_CONFIG_DIR%\\ciprofile.bat
 
+REM %MINGW32SH% and %MINGW64SH% should be defined on the slave to something like:
+REM   C:\msys64\usr\bin\env MSYSTEM=MINGW32 HOME=%cd% /usr/bin/bash -li
+REM (either in ciprofile.bat or custom Windows environment variables)
+
+REM bashrc should then define $MSLIB32_PATH and $MSLIB64_PATH pointing to MSVC
+REM lib.exe directory for 32/64 builds, such as:
+REM export MSLIB32_PATH=/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx86/x86/
+REM export MSLIB64_PATH=/c/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.27.29110/bin/Hostx64/x64/
+
 sh -c "chmod 755 job-3-mingw.sh"
-sh -c "./job-3-mingw.sh %1"
+
+if %2 equ 32 set MINGWSH=%MINGW32SH%
+if %2 equ 64 set MINGWSH=%MINGW64SH%
+
+%MINGWSH% -c "./job-3-mingw.sh %1 %2"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b 0
