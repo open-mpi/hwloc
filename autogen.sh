d1ef509505d776a8251bcafaa00e888b106100a1
Brice Goglin <Brice.Goglin@inria.fr>
2020-11-25 16:12:27 +0100

    autogen: fix MacOS X Big Sur libtool.m4 bug by patching configure
    This libtool patch isn't applied upstream yet but it is
    already used by several projects to fix build failures
    on the latest MacOS X (Big Sur).
    
    Upstream patch at https://lists.gnu.org/archive/html/libtool-patches/2020-06/msg00001.html
    applies to libtool.m4 but we have to apply it to configure
    after autoconf used the (buggy) libtool.m4 instead.
    
    See also open-mpi/ompi#8218.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- autogen.sh
+++ autogen.sh
@@ -7,3 +7,11 @@ if test "x$srcdir" != x; then
 fi
 
 autoreconf ${autoreconf_args:-"-ivf"}
+
+echo "Trying to patching configure for MacOS Big Sur libtool.m4 bug"
+if patch -p1 --dry-run < config/libtool-big-sur-fixup.patch; then
+   echo "Patching for real now"
+   patch -p1 < config/libtool-big-sur-fixup.patch
+else
+   echo "WARNING: Couldn't apply Big Sur libtool.m4 bug fix."
+fi
