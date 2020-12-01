e9b67603da2896543f0f189f699c811440465f9c
Brice Goglin <Brice.Goglin@inria.fr>
2020-12-01 10:47:35 +0100

    VERSION: 2.4.0 released, doing 2.4.1rc1 now
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/android/include/hwloc/autogen/config.h
+++ contrib/android/include/hwloc/autogen/config.h
@@ -12,10 +12,10 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.4.0"
+#define HWLOC_VERSION "2.4.1"
 #define HWLOC_VERSION_MAJOR 2
 #define HWLOC_VERSION_MINOR 4
-#define HWLOC_VERSION_RELEASE 0
+#define HWLOC_VERSION_RELEASE 1
 #define HWLOC_VERSION_GREEK ""
 
 #if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
