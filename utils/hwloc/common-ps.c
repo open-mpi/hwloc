open-mpi/hwloc

commit cbb6b960f9c1d5c2cc205e3a3df78454bd551d13

hwloc-ps: don't apply --pid-cmd to processes that will be filtered out
Split the calling of the user-provided command out of the main
common-ps functions and let the caller calls it after filtering
interesting processes.

Otherwise hwloc-ps --pid-cmd takes several seconds on all platforms
since we have to query hundreds of (uninteresting because not bound)
processes.

Thanks to Valentin Hoyet for the report.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

utils/hwloc/common-ps.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009-2019 Inria.  All rights reserved.
+ * Copyright © 2009-2021 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -24,7 +24,7 @@
 
 int hwloc_ps_read_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
 			  struct hwloc_ps_process *proc,
-			  unsigned long flags, const char *pidcmd)
+			  unsigned long flags)
 {
 #ifdef HAVE_DIRENT_H
   hwloc_pid_t realpid;
@@ -104,23 +104,7 @@ int hwloc_ps_read_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocp
 
   free(path);
 
-  proc->string[0] = '\0';
-  if (pidcmd) {
-    char *cmd;
-    FILE *file;
-    cmd = malloc(strlen(pidcmd)+1+5+2+1);
-    sprintf(cmd, "%s %u", pidcmd, (unsigned) proc->pid);
-    file = popen(cmd, "r");
-    if (file) {
-      if (fgets(proc->string, sizeof(proc->string), file)) {
-	end = strchr(proc->string, '\n');
-	if (end)
-	  *end = '\0';
-      }
-      pclose(file);
-    }
-    free(cmd);
-  }
+  proc->string[0] = '\0'; /* might be set later if hwloc_ps_pidcmd is called */
 
   if (flags & HWLOC_PS_FLAG_UID) {
     proc->uid = HWLOC_PS_ALL_UIDS;
@@ -263,6 +247,24 @@ int hwloc_ps_read_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocp
   return -1;
 }
 
+void hwloc_ps_pidcmd(struct hwloc_ps_process *proc, const char *pidcmd)
+{
+  char *cmd;
+  FILE *file;
+  cmd = malloc(strlen(pidcmd)+1+5+2+1);
+  sprintf(cmd, "%s %u", pidcmd, (unsigned) proc->pid);
+  file = popen(cmd, "r");
+  if (file) {
+    if (fgets(proc->string, sizeof(proc->string), file)) {
+      char *end = strchr(proc->string, '\n');
+      if (end)
+        *end = '\0';
+    }
+    pclose(file);
+  }
+  free(cmd);
+}
+
 void hwloc_ps_free_process(struct hwloc_ps_process *proc)
 {
   unsigned i;
@@ -279,7 +281,7 @@ void hwloc_ps_free_process(struct hwloc_ps_process *proc)
 int hwloc_ps_foreach_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
 			     void (*callback)(hwloc_topology_t topology, struct hwloc_ps_process *proc, void *cbdata),
 			     void *cbdata,
-			     unsigned long flags, const char *only_name, long uid, const char *pidcmd)
+			     unsigned long flags, const char *only_name, long uid)
 {
 #ifdef HAVE_DIRENT_H
   DIR *dir;
@@ -304,7 +306,7 @@ int hwloc_ps_foreach_process(hwloc_topology_t topology, hwloc_const_bitmap_t top
     proc.nthreads = 0;
     proc.nboundthreads = 0;
     proc.threads = NULL;
-    if (hwloc_ps_read_process(topology, topocpuset, &proc, flags, pidcmd) < 0)
+    if (hwloc_ps_read_process(topology, topocpuset, &proc, flags) < 0)
       goto next;
     if (only_name && !strstr(proc.name, only_name))
       goto next;

utils/hwloc/common-ps.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009-2019 Inria.  All rights reserved.
+ * Copyright © 2009-2021 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -15,8 +15,8 @@
 
 struct hwloc_ps_process {
   long pid;
-  char string[1024];
   char name[64];
+  char string[1024]; /* optional string, used by hwloc-ps --pid-cmd */
   hwloc_bitmap_t cpuset;
 #define HWLOC_PS_ALL_UIDS ((long)-1)
   long uid;
@@ -38,12 +38,14 @@ struct hwloc_ps_process {
 
 int hwloc_ps_read_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
 			  struct hwloc_ps_process *proc,
-			  unsigned long flags, const char *pidcmd);
+			  unsigned long flags);
 
 int hwloc_ps_foreach_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
 			     void (*callback)(hwloc_topology_t topology, struct hwloc_ps_process *proc, void *cbdata),
 			     void *cbdata,
-			     unsigned long flags, const char *only_name, long only_uid, const char *pidcmd);
+			     unsigned long flags, const char *only_name, long only_uid);
+
+void hwloc_ps_pidcmd(struct hwloc_ps_process *proc, const char *pidcmd);
 
 void hwloc_ps_free_process(struct hwloc_ps_process *proc);
 

utils/hwloc/hwloc-ps.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2009-2019 Inria.  All rights reserved.
+ * Copyright © 2009-2021 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -175,11 +175,16 @@ static void print_process_json(hwloc_topology_t topology,
 
 static void foreach_process_cb(hwloc_topology_t topology,
 			       struct hwloc_ps_process *proc,
-			       void *cbdata __hwloc_attribute_unused)
+			       void *cbdata)
 {
+  const char *pidcmd = cbdata;
+
   /* don't print anything if the process isn't bound and if no threads are bound and if not showing all */
   if (!proc->bound && (!proc->nthreads || !proc->nboundthreads) && !show_all && !only_name)
     return;
+
+  if (pidcmd)
+    hwloc_ps_pidcmd(proc, pidcmd);
 
   if (json_output)
     print_process_json(topology, proc);
@@ -192,7 +197,7 @@ static int run(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
 {
   if (only_pid == NO_ONLY_PID) {
     /* show all */
-    return hwloc_ps_foreach_process(topology, topocpuset, foreach_process_cb, NULL, psflags, only_name, only_uid, pidcmd);
+    return hwloc_ps_foreach_process(topology, topocpuset, foreach_process_cb, pidcmd, psflags, only_name, only_uid);
 
   } else {
     /* show only one */
@@ -205,8 +210,12 @@ static int run(hwloc_topology_t topology, hwloc_const_bitmap_t topocpuset,
     proc.nthreads = 0;
     proc.nboundthreads = 0;
     proc.threads = NULL;
-    ret = hwloc_ps_read_process(topology, topocpuset, &proc, psflags, pidcmd);
+    ret = hwloc_ps_read_process(topology, topocpuset, &proc, psflags);
     if (ret >= 0) {
+
+      if (pidcmd)
+        hwloc_ps_pidcmd(&proc, pidcmd);
+
       if (json_output)
 	print_process_json(topology, &proc);
       else

utils/lstopo/lstopo.c
@@ -160,7 +160,7 @@ static void add_process_objects(hwloc_topology_t topology)
 
   hwloc_ps_foreach_process(topology, root->cpuset,
 			   foreach_process_cb, NULL,
-			   HWLOC_PS_FLAG_THREADS | HWLOC_PS_FLAG_SHORTNAME, NULL, HWLOC_PS_ALL_UIDS, NULL);
+			   HWLOC_PS_FLAG_THREADS | HWLOC_PS_FLAG_SHORTNAME, NULL, HWLOC_PS_ALL_UIDS);
 }
 
 static __hwloc_inline void lstopo_update_factorize_bounds(unsigned min, unsigned *first, unsigned *last)

commit 5fd60f712f4fe52b07ee54e738ee76288a5c7111

utils/common-ps: fix popen/pclose for Windows
Broken in cbb6b960f9c1d5c2cc205e3a3df78454bd551d13

Not strictly needed since hwloc-ps isn't built on Windows
and lstopo --ps does nothing there, but the build is easy to fix.

Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

utils/hwloc/common-ps.c
@@ -247,6 +247,12 @@ int hwloc_ps_read_process(hwloc_topology_t topology, hwloc_const_bitmap_t topocp
   return -1;
 }
 
+#ifdef HWLOC_WIN_SYS
+/* hwloc-ps isn't built for Windows, but lstopo is */
+#define popen _popen
+#define pclose _pclose
+#endif
+
 void hwloc_ps_pidcmd(struct hwloc_ps_process *proc, const char *pidcmd)
 {
   char *cmd;