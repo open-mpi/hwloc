open-mpi/hwloc

commit 3a8c9fb5b1307ff1d30650f810797a391dfb6140

levelzero: add completion queue groups information
Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

hwloc/topology-levelzero.c
@@ -93,6 +93,7 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
 
     for(j=0; j<nbdevices; j++) {
       zes_device_properties_t prop;
+      unsigned nr_cqprops;
       zes_pci_properties_t pci;
       zes_device_handle_t sdvh = dvh[j];
       hwloc_obj_t osdev, parent;
@@ -139,6 +140,27 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
       if (strcmp((const char *) prop.boardNumber, "Unknown"))
         hwloc_obj_add_info(osdev, "LevelZeroBoardNumber", (const char *) prop.boardNumber);
 
+      nr_cqprops = 0;
+      res = zeDeviceGetCommandQueueGroupProperties(dvh[j], &nr_cqprops, NULL);
+      if (res == ZE_RESULT_SUCCESS && nr_cqprops) {
+        ze_command_queue_group_properties_t *cqprops;
+        cqprops = malloc(nr_cqprops * sizeof(*cqprops));
+        if (cqprops) {
+          res = zeDeviceGetCommandQueueGroupProperties(dvh[j], &nr_cqprops, cqprops);
+          if (res == ZE_RESULT_SUCCESS) {
+            char tmp[32];
+            snprintf(tmp, sizeof(tmp), "%u", nr_cqprops);
+            hwloc_obj_add_info(osdev, "LevelZeroCQGroups", tmp);
+            for(k=0; k<nr_cqprops; k++) {
+              char name[32];
+              snprintf(name, sizeof(name), "LevelZeroCQGroup%u", k);
+              snprintf(tmp, sizeof(tmp), "%u*0x%lx", (unsigned) cqprops[k].numQueues, (unsigned long) cqprops[k].flags);
+              hwloc_obj_add_info(osdev, name, tmp);
+            }
+          }
+          free(cqprops);
+        }
+      }
 
       parent = NULL;
       res = zesDevicePciGetProperties(sdvh, &pci);

doc/hwloc.doxy
@@ -1939,6 +1939,12 @@ and SectorSize (in bytes).
 <dd>The index of the Level Zero driver within the list of drivers,
  and the index of the device within the list of devices managed by this driver.
 </dd>
+<dt>LevelZeroCQGroups, LevelZeroCQGroup2 (LevelZero OS devices)</dt>
+<dd>The number of completion queue groups, and the description of the third group
+(as <tt>N*0xX</tt> where <tt>N</tt> is the number of queues in the group,
+ and <tt>0xX</tt> is the hexadecimal bitmask of <tt>ze_command_queue_group_property_flag_t</tt>
+ listing properties of those queues).
+</dd>
 <dt>AMDUUID, AMDSerial (RSMI GPU OS devices)</dt>
 <dd>The UUID and serial number of AMD GPUs.
 </dd>

tests/hwloc/ports/include/levelzero/ze_api.h
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2020 Inria.  All rights reserved.
+ * Copyright © 2020-2021 Inria.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -15,5 +15,13 @@ typedef void * ze_device_handle_t;
 extern ze_result_t zeInit(int);
 extern ze_result_t zeDriverGet(uint32_t *, ze_driver_handle_t *);
 extern ze_result_t zeDeviceGet(ze_driver_handle_t, uint32_t *, ze_device_handle_t *);
+
+typedef struct ze_command_queue_group_properties {
+  unsigned long flags;
+  unsigned numQueues;
+} ze_command_queue_group_properties_t;
+
+extern ze_result_t zeDeviceGetCommandQueueGroupProperties(ze_driver_handle_t, uint32_t *, ze_command_queue_group_properties_t *);
+
 
 #endif /* HWLOC_PORT_L0_ZE_API_H */

commit 11df5ed4972714e8ef2aa5f03a1aec22b1ea4359

levelzero: fix device name numbering
variable k is used as iterator now for CQs, use dedicated variable
for device numbering.

(broken in 3a8c9fb5b1307ff1d30650f810797a391dfb6140)

Signed-off-by: Peyton, Jonathan L <jonathan.l.peyton@intel.com>

hwloc/topology-levelzero.c
@@ -27,7 +27,7 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
   enum hwloc_type_filter_e filter;
   ze_result_t res;
   ze_driver_handle_t *drh;
-  uint32_t nbdrivers, i, k;
+  uint32_t nbdrivers, i, k, zeidx;
   int sysman_maybe_missing = 0; /* 1 if ZES_ENABLE_SYSMAN=1 was NOT set early, 2 if ZES_ENABLE_SYSMAN=0 */
   char *env;
 
@@ -72,7 +72,7 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
     return 0;
   }
 
-  k = 0;
+  zeidx = 0;
   for(i=0; i<nbdrivers; i++) {
     uint32_t nbdevices, j;
     ze_device_handle_t *dvh;
@@ -109,7 +109,7 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
       }
 
       osdev = hwloc_alloc_setup_object(topology, HWLOC_OBJ_OS_DEVICE, HWLOC_UNKNOWN_INDEX);
-      snprintf(buffer, sizeof(buffer), "ze%u", k); // ze0d0 ?
+      snprintf(buffer, sizeof(buffer), "ze%u", zeidx); // ze0d0 ?
       osdev->name = strdup(buffer);
       osdev->depth = HWLOC_TYPE_DEPTH_UNKNOWN;
       osdev->attr->osdev.type = HWLOC_OBJ_OSDEV_COPROC;
@@ -179,7 +179,7 @@ hwloc_levelzero_discover(struct hwloc_backend *backend, struct hwloc_disc_status
         parent = hwloc_get_root_obj(topology);
 
       hwloc_insert_object_by_parent(topology, parent, osdev);
-      k++;
+      zeidx++;
     }
 
     free(dvh);