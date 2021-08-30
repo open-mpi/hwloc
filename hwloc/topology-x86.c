01587e811276100b4c0db4563dceadfa39560d73
Brice Goglin <Brice.Goglin@inria.fr>
2021-08-30 15:11:14 +0200

    x86: fix a comment in AMD quirk
    Broken by mistake in commit e5a61ffb025342274ef6856bf0c619865b99fc4a
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- hwloc/topology-x86.c
+++ hwloc/topology-x86.c
@@ -808,7 +808,7 @@ static void look_proc(struct hwloc_backend *backend, struct procinfo *infos, uns
       } else if (infos->cpufamilynumber == 0x15
 		 && (infos->cpumodelnumber == 0x1 /* Bulldozer */ || infos->cpumodelnumber == 0x2 /* Piledriver */)
 		 && cache->level == 3 && cache->nbthreads_sharing == 6) {
-	/* AMD Bulldozer and Piledriver 12-core processors have same APIC ids as Magny-Cours below,
+	/* AMD Bulldozer and Piledriver 12-core processors have same APIC ids as Magny-Cours above,
 	 * but we can't merge the checks because the original nbthreads_sharing must be exactly 6 here.
 	 */
 	cache->cacheid = (infos->apicid % legacy_max_log_proc) / cache->nbthreads_sharing /* cacheid within the package */
