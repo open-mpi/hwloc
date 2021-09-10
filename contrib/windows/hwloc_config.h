50cfb864f97e319dc7250dc8a9c98b950301167b
Brice Goglin <Brice.Goglin@inria.fr>
2021-09-10 11:04:36 +0200

    contrib/android: new apk hwloc 2.9.2-1-1.5.2
    Remove rc2 greek, update all versions.
    
    Bump versionCode to 270
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/windows/hwloc_config.h
+++ contrib/windows/hwloc_config.h
@@ -11,11 +11,11 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.9.2rc2"
+#define HWLOC_VERSION "2.9.2"
 #define HWLOC_VERSION_MAJOR 2
 #define HWLOC_VERSION_MINOR 9
 #define HWLOC_VERSION_RELEASE 2
-#define HWLOC_VERSION_GREEK "rc2"
+#define HWLOC_VERSION_GREEK ""
 
 #define __hwloc_restrict
 #define __hwloc_inline __inline
