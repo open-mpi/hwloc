be110ff8a8da270e25e8153b8e20398f5191ddbf
Brice Goglin <Brice.Goglin@inria.fr>
2022-06-29 13:46:17 +0200

    VERSION: master is now 2.9
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/android/include/hwloc/autogen/config.h
+++ contrib/android/include/hwloc/autogen/config.h
@@ -1,7 +1,7 @@
 /* include/hwloc/autogen/config.h.  Generated from config.h.in by configure.  */
 /* -*- c -*-
  * Copyright © 2009 CNRS
- * Copyright © 2009-2021 Inria.  All rights reserved.
+ * Copyright © 2009-2022 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -12,9 +12,9 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.8.0a1"
+#define HWLOC_VERSION "2.9.0a1"
 #define HWLOC_VERSION_MAJOR 2
-#define HWLOC_VERSION_MINOR 8
+#define HWLOC_VERSION_MINOR 9
 #define HWLOC_VERSION_RELEASE 0
 #define HWLOC_VERSION_GREEK "a1"
 
