41a079cedf4700a24e4adec9d804e76cd385e668
Brice Goglin <Brice.Goglin@inria.fr>
2023-03-07 18:48:26 +0100

    hwloc-gather-cpuid: add AMD 0x80000026 extended topology leaf
    Likely available in Zen4.
    
    We now cover AMD CPUID of Fam19h Model11h PPR from 2022/10.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- utils/hwloc/hwloc-gather-cpuid.c
+++ utils/hwloc/hwloc-gather-cpuid.c
@@ -504,7 +504,20 @@ static int dump_one_proc(hwloc_topology_t topo, hwloc_obj_t pu, const char *path
     dump_one_cpuid(output, regs, 0x1);
   }
 
-  if (highest_ext_cpuid > 0x80000023) {
+  /* 0x80000026 = Extended CPU Topology on AMD ; Reserved on Intel */
+  if (highest_ext_cpuid >= 0x80000026) {
+    for(i=0; i<256; i++) {
+      regs[0] = 0x80000026; regs[2] = i;
+      dump_one_cpuid(output, regs, 0x5);
+      if (!(regs[1] & 0xffff))
+	/* nothing here, no more levels */
+	break;
+    }
+    if (i == 256)
+      fprintf(output, "# stopped at ecx=256\n");
+  }
+
+  if (highest_ext_cpuid > 0x80000026) {
     static int reported = 0;
     if (!reported)
       fprintf(stderr, "WARNING: Processor supports new extended CPUID leaves upto 0x%x\n", highest_ext_cpuid);
