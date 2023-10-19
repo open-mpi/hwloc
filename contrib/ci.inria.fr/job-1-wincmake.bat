4d0969d1e85adccc530a4f553ff64bdeebe68d05
Brice Goglin <Brice.Goglin@inria.fr>
2023-10-19 16:47:00 +0200

    contrib/ci.inria.fr: shorten the build path in windows cmake
    Our CI uses NMake CMake-generator which doesn't like very long file names.
    It already fails when building some branches with long names.
    
    Use only the last 9 characters of the tarball name
    (that's what our CI currently uses for abbreviated git hashes)
    instead of the full hwloc-<branch>-date.git<hash> tarball name
    as a build directory.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 739a1f9ad0200ade1a11a349373f5cdd37183f10)

--- contrib/ci.inria.fr/job-1-wincmake.bat
+++ contrib/ci.inria.fr/job-1-wincmake.bat
@@ -1,23 +1,30 @@
 REM
-REM  Copyright © 2012-2021 Inria.  All rights reserved.
+REM  Copyright © 2012-2023 Inria.  All rights reserved.
 REM  See COPYING in top-level directory.
 REM
 
 call %JENKINS_CONFIG_DIR%\\ciprofile.bat
 
 set TARBALL=%1
+set BASENAME=%TARBALL:~0,-7%
+REM reduce the build path name to avoid issues with very long file names in cmake/nmake
+set SHORTNAME=%BASENAME:~-9%
 
 echo ###############################
 echo Running on:
 hostname
 ver
 echo Tarball: %TARBALL%
+echo Build short name: %SHORTNAME%
 echo ############################
 
 sh -c "tar xfz %TARBALL%"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-cd %TARBALL:~0,-7%\contrib\windows-cmake
+move %BASENAME% %SHORTNAME%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+cd %SHORTNAME%\contrib\windows-cmake
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 cmake --install-prefix=%cd%/install -DCMAKE_BUILD_TYPE=Release -B build
