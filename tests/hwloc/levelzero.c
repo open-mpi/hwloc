d3a04a70fe8911f93e0d860bdd0603b359fe066c
Brice Goglin <Brice.Goglin@inria.fr>
2024-11-08 13:46:54 +0100

    levelzero.h+tests: update to both ZE and ZES API
    We cannot use the same handle for ZE/Core and ZES/Sysman APIs anymore,
    so split the helpers in levelzero.h into normal and sysman functions,
    and update tests to check both.
    
    Reenable tests/hwloc/levelzero.c and update it to test both ZE and
    ZES APIs.
    
    By the way, cleanup random things and clarify some docs.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- tests/hwloc/levelzero.c
+++ tests/hwloc/levelzero.c
@@ -12,7 +12,7 @@
 #include "hwloc.h"
 #include "hwloc/levelzero.h"
 
-/* check the RSMI helpers */
+/* check the LevelZero helpers */
 
 static int check_levelzero_backend(hwloc_topology_t topology)
 {
@@ -29,31 +29,25 @@ int main(void)
 {
   hwloc_topology_t topology;
   ze_driver_handle_t *drh;
+  zes_driver_handle_t *sdrh;
   uint32_t nbdrivers, i, k;
   ze_result_t res;
   int has_levelzero_backend;
   int err = 0;
-
-  /* SKIP until zesInit() support is finalized in the core and helpers */
-  return 77;
-
-  res = zeInit(0);
-  if (res != ZE_RESULT_SUCCESS) {
-    fprintf(stderr, "Failed to initialize LevelZero in zeInit(): %d\n", (int)res);
-    return 0;
-  }
-
-  res = zesInit(0);
-  if (res != ZE_RESULT_SUCCESS) {
-    fprintf(stderr, "Failed to initialize LevelZero Sysman in zesInit(): %d\n", (int)res);
-    return 0;
-  }
 
   hwloc_topology_init(&topology);
   hwloc_topology_set_io_types_filter(topology, HWLOC_TYPE_FILTER_KEEP_IMPORTANT);
   hwloc_topology_load(topology);
 
   has_levelzero_backend = check_levelzero_backend(topology);
+
+  printf("testing ZE devices\n");
+
+  res = zeInit(0);
+  if (res != ZE_RESULT_SUCCESS) {
+    fprintf(stderr, "Failed to initialize LevelZero in zeInit(): %d\n", (int)res);
+    return 0;
+  }
 
   nbdrivers = 0;
   res = zeDriverGet(&nbdrivers, NULL);
@@ -131,7 +125,97 @@ int main(void)
       }
       hwloc_bitmap_free(set);
     }
+    free(dvh);
   }
+  free(drh);
+
+  printf("testing ZES devices\n");
+
+  res = zesInit(0);
+  if (res != ZE_RESULT_SUCCESS) {
+    fprintf(stderr, "Failed to initialize LevelZero Sysman in zesInit(): %d\n", (int)res);
+    return 0;
+  }
+
+  nbdrivers = 0;
+  res = zesDriverGet(&nbdrivers, NULL);
+  if (res != ZE_RESULT_SUCCESS || !nbdrivers)
+    return 0;
+  sdrh = malloc(nbdrivers * sizeof(*sdrh));
+  if (!sdrh)
+    return 0;
+  res = zesDriverGet(&nbdrivers, sdrh);
+  if (res != ZE_RESULT_SUCCESS) {
+    free(sdrh);
+    return 0;
+  }
+
+  printf("found %u L0 ZES drivers\n", nbdrivers);
+
+  k = 0;
+  for(i=0; i<nbdrivers; i++) {
+    uint32_t nbdevices, j;
+    zes_device_handle_t *sdvh;
+
+    nbdevices = 0;
+    res = zesDeviceGet(sdrh[i], &nbdevices, NULL);
+    if (res != ZE_RESULT_SUCCESS || !nbdevices)
+      continue;
+    sdvh = malloc(nbdevices * sizeof(*sdvh));
+    if (!sdvh)
+      continue;
+    res = zeDeviceGet(sdrh[i], &nbdevices, sdvh);
+    if (res != ZE_RESULT_SUCCESS) {
+      free(sdvh);
+      continue;
+    }
+
+    printf("found %u L0 ZES devices in driver #%u\n", nbdevices, i);
+
+    for (j=0; j<nbdevices; j++, k++) {
+      hwloc_bitmap_t set;
+      hwloc_obj_t osdev, ancestor;
+      const char *value;
+
+      osdev = hwloc_levelzero_get_sysman_device_osdev(topology, sdvh[j]);
+      assert(osdev);
+
+      ancestor = hwloc_get_non_io_ancestor_obj(topology, osdev);
+
+      printf("found OSDev %s\n", osdev->name);
+      err = strncmp(osdev->name, "ze", 2);
+      assert(!err);
+      assert(atoi(osdev->name+2) == (int) k);
+
+      assert(osdev->attr->osdev.types == (HWLOC_OBJ_OSDEV_COPROC|HWLOC_OBJ_OSDEV_GPU));
+
+      assert(has_levelzero_backend);
+
+      value = hwloc_obj_get_info_by_name(osdev, "LevelZeroDriverIndex");
+      assert(value);
+      assert(atoi(value) == (int) i);
+      value = hwloc_obj_get_info_by_name(osdev, "LevelZeroDriverDeviceIndex");
+      assert(value);
+      assert(atoi(value) == (int) j);
+
+      set = hwloc_bitmap_alloc();
+      err = hwloc_levelzero_get_sysman_device_cpuset(topology, sdvh[j], set);
+      if (err < 0) {
+        printf("failed to get cpuset for driver #%u device #%u\n", i, j);
+      } else {
+        char *cpuset_string = NULL;
+        hwloc_bitmap_asprintf(&cpuset_string, set);
+        printf("got cpuset %s for driver #%u device #%u\n", cpuset_string, j, i);
+        free(cpuset_string);
+        if (hwloc_bitmap_isequal(hwloc_topology_get_complete_cpuset(topology), hwloc_topology_get_topology_cpuset(topology)))
+          /* only compare if the topology is complete, otherwise things can be significantly different */
+          assert(hwloc_bitmap_isequal(set, ancestor->cpuset));
+      }
+      hwloc_bitmap_free(set);
+    }
+    free(sdvh);
+  }
+  free(sdrh);
 
   hwloc_topology_destroy(topology);
 
