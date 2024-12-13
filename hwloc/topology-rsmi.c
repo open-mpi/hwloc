aef721f1c348bae01001a0edde2d2a5999d31a74
Brice Goglin <Brice.Goglin@inria.fr>
2024-12-13 09:31:52 +0100

    rsmi: fix the PCI locality of partitioned devices
    rsmi_dev_pci_id_get() returns the GPU "partition ID" inside the PCI BDF function,
    but this virtual function isn't actually exposed to the OS.
    See ROCm/rocm_smi_lib#208 for details.
    
    When hwloc fails to find the corresponding PCI device (usually gets the above bridge instead),
    if the BDF function is > 0, get the RSMI partition ID, compare it with the BDF function,
    and try to get the PCI device with func = 0 instead.
    
    rsmi_dev_partition_id_get() was only added in ROCm 6.2, so configure-check it.
    
    Thanks to Edgar Leon for reporting and debugging the issue.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- hwloc/topology-rsmi.c
+++ hwloc/topology-rsmi.c
@@ -123,6 +123,32 @@ static int get_device_pci_info(uint32_t dv_ind, uint64_t *bdfid)
 }
 
 /*
+ * Get the partition ID of the GPU
+ *
+ * dv_ind  (IN) The device index
+ * partid   (OUT) partition ID of GPU devices
+ */
+static int get_device_partition_id(uint32_t dv_ind, uint32_t *partid)
+{
+#if HAVE_DECL_RSMI_DEV_PARTITION_ID_GET
+  rsmi_status_t rsmi_rc = rsmi_dev_partition_id_get(dv_ind, partid);
+
+  if (rsmi_rc != RSMI_STATUS_SUCCESS) {
+    if (HWLOC_SHOW_ALL_ERRORS()) {
+      const char *status_string;
+      rsmi_rc = rsmi_status_string(rsmi_rc, &status_string);
+      fprintf(stderr, "hwloc/rsmi: GPU(%u): Failed to get partition ID: %s\n", (unsigned)dv_ind, status_string);
+    }
+    return -1;
+  }
+  return 0;
+#else
+  errno = ENOSYS;
+  return -1;
+#endif
+}
+
+/*
  * Get the PCI link speed of the GPU
  *
  * dv_ind    (IN) The device index
@@ -365,6 +391,16 @@ hwloc_rsmi_discover(struct hwloc_backend *backend, struct hwloc_disc_status *dst
       device = ((bdfid & 0xff)>>3) & 0x1f;
       func = bdfid & 0x7;
       parent = hwloc_pci_find_parent_by_busid(topology, domain, bus, device, func);
+      if ((!parent || parent->type != HWLOC_OBJ_PCI_DEVICE) && func > 0) {
+        /* Partitioned MI devices may return the partition ID in a fake BDF func,
+         * hence we would fail to find a pcidev parent.
+         * Try with func=0 instead.
+         */
+        uint32_t partid = 0;
+        get_device_partition_id(i, &partid);
+        if (func == partid)
+          parent = hwloc_pci_find_parent_by_busid(topology, domain, bus, device, 0);
+      }
       if (parent && parent->type == HWLOC_OBJ_PCI_DEVICE)
         get_device_pci_linkspeed(i, &parent->attr->pcidev.linkspeed);
       if (!parent)
