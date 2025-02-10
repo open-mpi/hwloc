b87362d4e28126f2bef1bdffb798b190c25899be
Brice Goglin <Brice.Goglin@inria.fr>
2025-02-10 17:48:16 +0100

    VERSION: prepare for 2.12.0rc1
    Bump soname to 24:0:9 (vs 23:1:8 for 2.11.2)
    
    Reuse AC to 2.72, bump AM to 1.17 and LT to 2.5.4.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/android/include/hwloc/autogen/config.h
+++ contrib/android/include/hwloc/autogen/config.h
@@ -1,7 +1,7 @@
 /* include/hwloc/autogen/config.h.  Generated from config.h.in by configure.  */
 /* -*- c -*-
  * Copyright © 2009 CNRS
- * Copyright © 2009-2024 Inria.  All rights reserved.
+ * Copyright © 2009-2025 Inria.  All rights reserved.
  * Copyright © 2009-2012 Université Bordeaux
  * Copyright © 2009-2011 Cisco Systems, Inc.  All rights reserved.
  * See COPYING in top-level directory.
@@ -12,11 +12,11 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.12.0a1"
+#define HWLOC_VERSION "2.12.0rc1"
 #define HWLOC_VERSION_MAJOR 2
 #define HWLOC_VERSION_MINOR 12
 #define HWLOC_VERSION_RELEASE 0
-#define HWLOC_VERSION_GREEK "a1"
+#define HWLOC_VERSION_GREEK "rc1"
 
 #if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
 # define __hwloc_restrict __restrict
