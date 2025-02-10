233ed1c702c94da41c4d2ba8e2b524e00cd8df6e
Brice Goglin <Brice.Goglin@inria.fr>
2025-02-10 21:48:13 +0100

    VERSION: 2.12.0rc1 released, doing rc2 now
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/windows/hwloc_config.h
+++ contrib/windows/hwloc_config.h
@@ -11,11 +11,11 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.12.0rc1"
+#define HWLOC_VERSION "2.12.0rc2"
 #define HWLOC_VERSION_MAJOR 2
 #define HWLOC_VERSION_MINOR 12
 #define HWLOC_VERSION_RELEASE 0
-#define HWLOC_VERSION_GREEK "rc1"
+#define HWLOC_VERSION_GREEK "rc2"
 
 #define __hwloc_restrict
 #define __hwloc_inline __inline
