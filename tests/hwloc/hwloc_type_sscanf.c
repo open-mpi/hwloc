c1d80cc434be618d27b52c204f77b24e7a67fada
Brice Goglin <Brice.Goglin@inria.fr>
2025-04-17 16:08:37 +0200

    tests/type_sscanf: test a synthetic case with 2 levels of groups
    Refs #715
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- tests/hwloc/hwloc_type_sscanf.c
+++ tests/hwloc/hwloc_type_sscanf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2016-2024 Inria.  All rights reserved.
+ * Copyright © 2016-2025 Inria.  All rights reserved.
  * See COPYING in top-level directory.
  */
 
@@ -247,6 +247,10 @@ int main(void)
   assert(err == -1);
 
   printf("testing the local topology ...\n");
+  check_topo();
+
+  printf("testing synthetic topology group:2 group:2 pu:2 ...\n");
+  putenv((char *) "HWLOC_SYNTHETIC=group:2 group:2 pu:2");
   check_topo();
 
   printf("testing topology 32em64t-2n8c2t-pci-wholeio.xml ...\n");
