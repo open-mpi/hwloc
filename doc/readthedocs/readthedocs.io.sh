2f9da1e3cc457f34bf73ab45601bc465e87d40ec
Brice Goglin <Brice.Goglin@inria.fr>
2025-07-24 11:19:15 +0200

    doc/readthedocs: move the doxygen doc to a subdirectory and redirect to it
    Sphinx overwrites the doxygen index.
    The proper solution (and possibly nicer) is to use things like breathe,
    exhale or doxysphinx to actually integrate doxygen in sphinx,
    but that's a lot of work.
    
    Hence rather move the doxygen generated HTML doc to a subdirectory
    and make a dummy Sphinx index with only a link to the doxygen index.
    
    And we enable a (forced) redirection in the readthedocs config (on the website)
    so that the sphinx index isn't even visible.
    
    Somehow, readthedocs manages to put its own stuff on top of our
    doxygen pages, including the bottom-left menu for switching between versions.
    The only issue I found is that the warning about
    "latest version not being stable" is hidden behind the doxygen title bar.
    
    By the way, we don't use autotools at all on readthedocs,
    hence we have to duplicate the fig2dev command to generate diagram.png.
    Might be better to just prebuild diagram.{png,eps} as we do for lstopo
    outputs since we pretty much never modify diagram.fig.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit bf81a920270bfce3d96d1ad55a18b21d91e5eca2)
    
    doc/readthedocs: add a minimal .readthedocs.yaml
    
    Seems to be required nowadays
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>
    (cherry picked from commit 5253fe9a1307bfd02411cc976f8ee40f67fa662f)
    
    (also includes and replaces commit 345b956fd4ad519039122c64685051f67057488e)
    (also includes and replaces commit a397c428e80bd97425de833341c6fac646409839)

--- doc/readthedocs/readthedocs.io.sh
+++ doc/readthedocs/readthedocs.io.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright © 2018-2021 Inria.  All rights reserved.
+# Copyright © 2018-2025 Inria.  All rights reserved.
 # $COPYRIGHT$
 #
 
@@ -10,11 +10,16 @@ cp ./../doxygen-config.cfg.in ./doxygen-config.cfg
 sed -i 's/@top_srcdir@/..\/../g' ./doxygen-config.cfg
 sed -i 's/@PACKAGE_VERSION@/'$version'/g' ./doxygen-config.cfg
 
-
 echo "@INCLUDE = ./../doxygen.cfg" > readthedocs.io.cfg
-echo "OUTPUT_DIRECTORY = ./build" >> readthedocs.io.cfg
+echo "OUTPUT_DIRECTORY = $READTHEDOCS_OUTPUT/html/doxygen" >> readthedocs.io.cfg
 echo "SHORT_NAMES = NO" >> readthedocs.io.cfg
 echo "GENERATE_TAGFILE =" >> readthedocs.io.cfg
 echo "GENERATE_MAN = NO" >> readthedocs.io.cfg
 echo "GENERATE_LATEX = NO" >> readthedocs.io.cfg
 echo "GENERATE_TREEVIEW = YES" >> readthedocs.io.cfg
+
+echo "#### Contents of readthedocs.io.cfg:"
+cat readthedocs.io.cfg
+echo "####"
+
+mkdir -p $READTHEDOCS_OUTPUT/html/doxygen
