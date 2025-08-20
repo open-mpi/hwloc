f7b47ab7e5aa99e1074ca4a66261d7ff1d65bdac
Brice Goglin <Brice.Goglin@inria.fr>
2025-08-20 15:00:50 +0200

    VERSION: 2.12.2rc1 released, doing rc2 now
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- contrib/android/include/hwloc/autogen/config.h
+++ contrib/android/include/hwloc/autogen/config.h
@@ -12,11 +12,11 @@
 #ifndef HWLOC_CONFIG_H
 #define HWLOC_CONFIG_H
 
-#define HWLOC_VERSION "2.12.2rc1"
+#define HWLOC_VERSION "2.12.2rc2"
 #define HWLOC_VERSION_MAJOR 2
 #define HWLOC_VERSION_MINOR 12
 #define HWLOC_VERSION_RELEASE 2
-#define HWLOC_VERSION_GREEK "rc1"
+#define HWLOC_VERSION_GREEK "rc2"
 
 #if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 95))
 # define __hwloc_restrict __restrict
