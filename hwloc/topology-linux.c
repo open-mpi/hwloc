91d2a091b4bea0d6abcd1990ebea34d0bd551338
Brice Goglin <Brice.Goglin@inria.fr>
2025-10-23 11:52:03 +0200

    linux: get CPURevision info attr on Loongarch too
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- hwloc/topology-linux.c
+++ hwloc/topology-linux.c
@@ -4761,6 +4761,9 @@ hwloc_linux_parse_cpuinfo_loongarch(const char *prefix, const char *value,
   } else if (!strcmp("CPU Family", prefix)) {
     if (value[0])
       hwloc__add_info(infos, "CPUFamily", value);
+  } else if (!strcmp("CPU Revision", prefix)) {
+    if (value[0])
+      hwloc__add_info(infos, "CPURevision", value);
   }
   return 0;
 }
