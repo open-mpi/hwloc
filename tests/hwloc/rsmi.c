09b5d5f1a98fded264a69303739a136f183d0974
Brice Goglin <Brice.Goglin@inria.fr>
2026-05-06 19:47:59 +0200

    rsmi: remove ROCm SMI, just use AMD SMI
    This removes dual-support stuff added in 285db5a1c5043637cd3019df51d1a4ae81759c8b
    including --disable-rsmi-amd and --disable-rsmi-rocm for configure,
    and the HWLOC_RSMI_BACKEND envvar.
    
    Signed-off-by: Brice Goglin <Brice.Goglin@inria.fr>

--- tests/hwloc/rsmi.c
+++ tests/hwloc/rsmi.c
@@ -12,7 +12,10 @@
 #include "hwloc.h"
 #include "private/autogen/config.h"
 
-/* check the RSMI and AMD SMI helpers */
+#include <amd_smi/amdsmi.h>
+#include "hwloc/amdsmi.h"
+
+/* check the AMD SMI helpers */
 
 static int check_rsmi_backend(hwloc_topology_t topology)
 {
@@ -24,83 +27,6 @@ static int check_rsmi_backend(hwloc_topology_t topology)
       return 1;
   return 0;
 }
-
-#ifdef HWLOC_RSMI_USE_ROCM_SMI
-#include <rocm_smi/rocm_smi.h>
-#include "hwloc/rsmi.h"
-
-static int check_rsmi(hwloc_topology_t topology)
-{
-  rsmi_status_t ret;
-  unsigned count, i;
-  int has_rsmi_backend;
-  int err = 0;
-
-  rsmi_init(0);
-
-  ret = rsmi_num_monitor_devices(&count);
-  if (RSMI_STATUS_SUCCESS != ret || !count) {
-    rsmi_shut_down();
-    if (RSMI_STATUS_SUCCESS != ret) {
-      fprintf(stderr, "Failed to get device count\n");
-      return -1;
-    } else {
-      fprintf(stderr, "No GPU available\n");
-      return 0;
-    }
-  }
-  printf("rsmi_num_monitor_devices found %u devices\n", count);
-
-  has_rsmi_backend = check_rsmi_backend(topology);
-
-  for (i=0; i<count; i++) {
-    hwloc_bitmap_t set;
-    hwloc_obj_t osdev, osdev2, ancestor;
-    const char *value;
-
-    osdev = hwloc_rsmi_get_device_osdev(topology, i);
-    assert(osdev);
-    osdev2 = hwloc_rsmi_get_device_osdev_by_index(topology, i);
-    assert(osdev2 == osdev);
-
-    ancestor = hwloc_get_non_io_ancestor_obj(topology, osdev);
-
-    printf("found OSDev %s\n", osdev->name);
-    err = strncmp(osdev->name, "rsmi", 4);
-    assert(!err);
-    assert(atoi(osdev->name+4) == (int) i);
-
-    assert(has_rsmi_backend);
-
-    assert(osdev->attr->osdev.types == (HWLOC_OBJ_OSDEV_COPROC|HWLOC_OBJ_OSDEV_GPU));
-
-    value = hwloc_obj_get_info_by_name(osdev, "GPUModel");
-    printf("found OSDev model %s\n", value);
-
-    set = hwloc_bitmap_alloc();
-    err = hwloc_rsmi_get_device_cpuset(topology, i, set);
-    if (err < 0) {
-      printf("failed to get cpuset for device %u\n", i);
-    } else {
-      char *cpuset_string = NULL;
-      hwloc_bitmap_asprintf(&cpuset_string, set);
-      printf("got cpuset %s for device %u\n", cpuset_string, i);
-      free(cpuset_string);
-      if (hwloc_bitmap_isequal(hwloc_topology_get_complete_cpuset(topology), hwloc_topology_get_topology_cpuset(topology)))
-        /* only compare if the topology is complete, otherwise things can be significantly different */
-        assert(hwloc_bitmap_isequal(set, ancestor->cpuset));
-    }
-    hwloc_bitmap_free(set);
-  }
-
-  rsmi_shut_down();
-  return 0;
-}
-#endif /* HWLOC_RSMI_USE_ROCM_SMI */
-
-#ifdef HWLOC_RSMI_USE_AMD_SMI
-#include <amd_smi/amdsmi.h>
-#include "hwloc/amdsmi.h"
 
 static int check_amdsmi(hwloc_topology_t topology)
 {
@@ -189,7 +115,6 @@ static int check_amdsmi(hwloc_topology_t topology)
   amdsmi_shut_down();
   return 0;
 }
-#endif /* HWLOC_RSMI_USE_AMD_SMI */
 
 int main(void)
 {
@@ -198,13 +123,7 @@ int main(void)
   hwloc_topology_set_io_types_filter(topology, HWLOC_TYPE_FILTER_KEEP_IMPORTANT);
   hwloc_topology_load(topology);
 
-#ifdef HWLOC_RSMI_USE_ROCM_SMI
-  check_rsmi(topology);
-#endif /* HWLOC_RSMI_USE_ROCM_SMI */
-  printf("\n");
-#ifdef HWLOC_RSMI_USE_AMD_SMI
   check_amdsmi(topology);
-#endif /* HWLOC_RSMI_USE_AMD_SMI */
 
   hwloc_topology_destroy(topology);
   return 0;
